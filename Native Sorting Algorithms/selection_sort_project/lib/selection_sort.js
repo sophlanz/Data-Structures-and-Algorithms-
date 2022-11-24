44cb66835354f14809a3853bb211c7f8a3ab0104
Sophia Lanzendorf <sophialanzendorf@gmail.com>
2022-11-23 20:41:20 -0500

    native selection sort

---
+++ Native Sorting Algorithms/selection_sort_project/lib/selection_sort.js
@@ -0,0 +1,28 @@
+function swap(arr, index1, index2) {
+    //declare temp to hold first index before we change
+    let temp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = temp;
+    }
+
+function selectionSort(arr) {
+    //start at left most index and iterate through, this will be our starting min index
+    for(let i=0; i<arr.length;i++) {
+        let minIndex = i;
+        //interate through the right side of the array, compare array[minIndex] with array[j] to see which is smaller, reset the min index if array[j] is smaller.
+        for(let j=i+1; j<arr.length;j++) {
+            //check to see if min is still the smallest, if not, reset it
+            if(arr[minIndex] > arr[j] ) {
+                minIndex = j
+            }
+        }
+        //once we've found the min, after iterating through  swap the first index of the unsorted side (outer loop i) with the newfly found minIndex
+        swap(arr,i,minIndex)
+    }
+    return arr
+}
+
+module.exports = {
+    selectionSort,
+    swap
+};

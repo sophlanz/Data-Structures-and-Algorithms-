cc67cc14e3c59e484abc502e99cbfdc688179763
Sophia Lanzendorf <sophialanzendorf@gmail.com>
2022-11-29 16:19:52 -0500

    merge sort

---
+++ Efficient Sorting Algorithms/merge_sort_project/lib/merge_sort.js
@@ -0,0 +1,40 @@
+function merge(array1, array2) {
+       //merge into ascending order
+    let merged = [];
+    while(array1.length || array2.length) {
+        //if it's length is 0, we'll use infinity because we know it'll always be bigger
+        let ele1 = array1.length ? array1[0] : Infinity;
+        let ele2 = array2.length ? array2[0] : Infinity;
+        let next;
+        //comare the 2 elements
+        if(ele1 > ele2) {
+            //remove and return the first element
+            next = array2.shift();
+        } else {
+            next = array1.shift();
+        }
+        merged.push(next);
+    }
+    return merged
+}
+
+function mergeSort(array) {
+    //return array if length is 0 or 1
+    if(array.length <=1) {
+        return array;
+    }
+    //get mid, left and right
+    let mid = Math.floor(array.length / 2 );
+    let left = array.slice(0,mid);
+    let right = array.slice(mid);
+    //recursive call to sort the sides separately
+    let leftSorted = mergeSort(left);
+    let rightSorted= mergeSort(right);
+    //merge the 2 sorted sides
+    return merge(leftSorted,rightSorted);
+}
+
+module.exports = {
+    merge,
+    mergeSort
+};

c4e319f87d98cfef17fa0b862e85d26626d970ca
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2022-12-14 17:26:04 -0500

    Time: 71 ms (98.15%), Space: 46.8 MB (69.76%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.js
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.js
@@ -4,16 +4,15 @@
  * @return {number}
  */
 var longestOnes = function(nums, k) {
-    let start=0,
-        onesCount=0,
-        maxLen=0;
+let start=0,
+    maxLen=0,
+    onesCount=0;
     for(let end=0;end<nums.length;end++) {
-        //check if it's a one, if so increase one's count
         if(nums[end]===1) {
             onesCount++
         }
-        //check if window size - ones is greater than k
-        if((end-start+1 -onesCount) >k) {
+        if((end-start+1-onesCount)>k) {
+            //shrink window
             if(nums[start]===1) {
                 onesCount--
             }

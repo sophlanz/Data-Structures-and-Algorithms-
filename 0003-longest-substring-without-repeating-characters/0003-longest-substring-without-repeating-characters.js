d80c35906e575d7c5596ee9d3ead91574eb265c0
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2022-12-14 15:40:55 -0500

    Time: 98 ms (90.44%), Space: 48 MB (52.47%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -3,26 +3,16 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-  let start=0,
-      maxLen=0,
-      charFreq={};
-    for(let end=0; end<s.length;end++) {
-        //get right char
-        const rChar = s[end];
-         //if Char  of end is already in the charFreq keep shrinking the window
-        while(charFreq[rChar])
-        {
-            const lChar = s[start]
-            //shrink the window, deleting the starting char
-            delete charFreq[lChar]
-            //move the start
-            start++
-        }
-        //now we've removed it, add the new one
-        charFreq[rChar] = 1
-        //update maxLen
-        maxLen = Math.max(maxLen, end-start+1);   
-    
+let start=0,
+    max=0,
+    charIndexMap={};
+for(let end=0;end<s.length;end++) {
+   let rChar=s[end]
+    if(rChar in charIndexMap) {
+        start = Math.max(start,charIndexMap[rChar]+1)
     }
-    return maxLen
+    charIndexMap[rChar] = end;
+    max=Math.max(max,end-start+1);
+}
+    return max
 };

66e3a2b37b1beff79613f80288c626c56b33c323
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2022-12-14 15:11:43 -0500

    Time: 460 ms (37.25%), Space: 57.9 MB (37.25%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.js
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.js
@@ -3,28 +3,20 @@
  * @return {number}
  */
 var totalFruit = function(fruits) {
-    let start=0,
-        maxLength=0,
-        basket = { };
-    //loop through fruits
-    for(let end=0; end<fruits.length;end++) {
-    //get right fruit, add to basket, keep adding until basket length is more than 2
-        const rFruit = fruits[end];
-        basket[rFruit] = basket[rFruit] + 1 || 1;    
-    //while basket length is longer than 2
-        while(Object.keys(basket).length > 2) {
-    //get the left fruit and remove it from the basket
-    const lFruit = fruits[start];
-    basket[lFruit] --
-    //once that fruit of the basket is decremented to 0, remove it
-    basket[lFruit] === 0 ? delete basket[lFruit] : null;
-    //move the start pointer over
-            start ++
+ let start=0,
+     max=0,
+     basket={};
+    for(let end=0;end<fruits.length;end++) {
+      basket[fruits[end]] = basket[fruits[end]] +1 || 1;
+        while(Object.keys(basket).length >2) {
+            basket[fruits[start]]--
+            if(basket[fruits[start]]===0) {
+                delete basket[fruits[start]]
+            }
+            start++
         }
-    //get maxLength of window
-    maxLength = Math.max(maxLength, end-start +1)
+        max= Math.max(max,end-start+1)
     }
-    //return maxLength
-    return maxLength
+    return max
     
 };

0790f9fa671fdd219382d0726a450e7ba76b7556
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2022-12-15 12:39:51 -0500

    Time: 76 ms (94.86%), Space: 46.4 MB (90.16%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.js
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.js
@@ -7,19 +7,18 @@ var longestOnes = function(nums, k) {
 let start=0,
     maxLen=0,
     onesCount=0;
-    for(let end=0;end<nums.length;end++) {
-        if(nums[end]===1) {
-            onesCount++
-        }
-        if((end-start+1-onesCount)>k) {
-            //shrink window
-            if(nums[start]===1) {
-                onesCount--
-            }
-            start++
-        }
-        maxLen=Math.max(maxLen,end-start+1)
+for(let end=0;end<nums.length;end++) {
+    if(nums[end]===1) {
+        onesCount++
     }
+    if((end-start+1-onesCount)>k){
+        if(nums[start]===1) {
+            onesCount--
+        }
+        start++
+    }
+    maxLen =Math.max(maxLen,end-start+1);
+}
     return maxLen
             
 };

812b3b1641d08e8b24bd2635f28bf8e30753918b
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2022-12-15 08:44:25 -0500

    Time: 89 ms (94.80%), Space: 48.8 MB (35.12%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -5,14 +5,18 @@
 var lengthOfLongestSubstring = function(s) {
    let start=0,
        maxLen=0,
-       idxMap={};
+       map={};
     for(let end=0; end<s.length;end++) {
-        let rChar = s[end];
-        if(rChar in idxMap) {
-            start = Math.max(start, idxMap[rChar]+1);
-        }
-        idxMap[rChar] = end
-        maxLen=Math.max(maxLen, end-start+1)
+   let rChar=s[end];
+        
+            while(rChar in map) {
+                let lChar = s[start];
+                delete map[lChar]
+                start++
+            }
+        
+        map[rChar] = 1;
+        maxLen=Math.max(maxLen,end-start+1)
     }
     return maxLen
 };

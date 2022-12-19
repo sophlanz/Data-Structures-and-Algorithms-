c0f04eeb8e1ca58491581f41ba80dc0152ce159c
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2022-12-19 14:59:58 -0500

    Time: 126 ms (73.26%), Space: 48.3 MB (42.11%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.js
+++ 0567-permutation-in-string/0567-permutation-in-string.js
@@ -8,23 +8,23 @@ let start=0,
     match=0,
     map={};
     for(let i=0;i<s1.length;i++) {
-      map[s1[i]] = map[s1[i]] +1 || 1
+        map[s1[i]] = map[s1[i]] +1 || 1;
     }
     for(let end=0;end<s2.length;end++) {
         let rChar = s2[end];
-        if(rChar in map) {
-            map[rChar]--
-            if(map[rChar]===0) {
-                match++
-            }
-        }
+       if(rChar in map) {
+           map[rChar]--
+           if(map[rChar]===0) {
+               match++
+           }
+       }
         if(Object.keys(map).length === match) {
             return true
         }
-        if(end >= s1.length-1) {
-            let lChar = s2[start]
+        if(end>=s1.length-1 ) {
+            let lChar = s2[start];
             if(lChar in map) {
-                if(map[lChar]=== 0) {
+                if(map[lChar]===0) {
                     match--
                 }
                 map[lChar]++

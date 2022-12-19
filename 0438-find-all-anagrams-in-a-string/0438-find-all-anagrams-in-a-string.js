98d75b9c5641509a13216ea09f787109f360c16f
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2022-12-19 10:55:13 -0500

    Time: 149 ms (77.15%), Space: 49.6 MB (35.33%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.js
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.js
@@ -4,32 +4,33 @@
  * @return {number[]}
  */
 var findAnagrams = function(s, p) {
-    let start=0,
-        match=0,
-        map={};
-    const res=[];
-    for(let i=0; i<p.length;i++) {
-        map[p[i]] = map[p[i]]+1 || 1
-    }
+let start=0,
+    match=0,
+    res=[],
+    map={};
+for(let i=0;i<p.length;i++) {
+   map[p[i]] = map[p[i]] +1 || 1;
+}
     for(let end=0;end<s.length;end++) {
-        let rChar=s[end];
-           
+        let rChar = s[end];
         if(rChar in map) {
+            
             map[rChar]--
-            if(map[rChar]===0){
+            if(map[rChar]===0) {
                 match++
             }
         }
         if(match === Object.keys(map).length) {
-            res.push(start);
+            res.push(start)
         }
-        if(end>=p.length-1) {
-            let rChar = s[start];
-            if(rChar in map) {
-                if(map[rChar]===0) {
-                    match--
+        if(end >=p.length-1 ) {
+            //shrink window
+            let lChar = s[start];
+            if(lChar in map) {
+                if(map[lChar]===0){
+                            match--
                 }
-                map[rChar]++
+                 map[lChar]++
             }
             start++
         }

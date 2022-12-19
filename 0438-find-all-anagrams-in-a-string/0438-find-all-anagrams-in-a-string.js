cfe8b4c3867ea712198f75189bc298c64a464f1e
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2022-12-19 11:08:49 -0500

    Time: 221 ms (45.91%), Space: 49.2 MB (41.53%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.js
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.js
@@ -5,32 +5,31 @@
  */
 var findAnagrams = function(s, p) {
 let start=0,
-    match=0,
     res=[],
+    match=0,
     map={};
-for(let i=0;i<p.length;i++) {
-   map[p[i]] = map[p[i]] +1 || 1;
-}
+    for(let i=0;i<p.length;i++) {
+        map[p[i]] = map[p[i]]+1 ||1;
+        
+    }
     for(let end=0;end<s.length;end++) {
         let rChar = s[end];
         if(rChar in map) {
-            
             map[rChar]--
-            if(map[rChar]===0) {
+            if(map[rChar]=== 0) {
                 match++
             }
         }
         if(match === Object.keys(map).length) {
             res.push(start)
         }
-        if(end >=p.length-1 ) {
-            //shrink window
+        if(end >= p.length-1) {
             let lChar = s[start];
             if(lChar in map) {
-                if(map[lChar]===0){
-                            match--
+                if(map[lChar]=== 0) {
+                    match--
                 }
-                 map[lChar]++
+                map[lChar]++
             }
             start++
         }

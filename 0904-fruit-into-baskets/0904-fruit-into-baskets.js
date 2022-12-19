e5728e9199800bf01adfb883695dfbffcf05161a
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2022-12-19 15:55:54 -0500

    Time: 505 ms (30.69%), Space: 66.1 MB (7.40%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.js
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.js
@@ -3,22 +3,21 @@
  * @return {number}
  */
 var totalFruit = function(fruits) {
-    let start=0,
-        map={},
-        maxLen=0;
-for(let end=0;end<fruits.length;end++){
-    let rChar = fruits[end];
-    map[rChar] = map[rChar] +1 || 1
-    if(Object.keys(map).length>2) {
-        //shrink the widnow
-        let lChar = fruits[start];
-        map[lChar]--
-        if(map[lChar]===0) {
-            delete map[lChar]
+let start=0,
+    map={},
+    maxLen=0;
+    for(let end=0;end<fruits.length;end++) {
+        let rChar = fruits[end];
+        map[rChar] = map[rChar] +1 || 1;
+        if(Object.keys(map).length >2) {
+            let lChar = fruits[start];
+            map[lChar]--
+            if(map[lChar]===0) {
+                delete map[lChar]
+            }
+            start++
         }
-        start++
+        maxLen = Math.max(maxLen, end-start+1)
     }
-    maxLen=Math.max(maxLen,end-start+1);
-}
     return maxLen
 };

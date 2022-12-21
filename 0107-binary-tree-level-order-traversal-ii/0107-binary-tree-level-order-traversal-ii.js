a49e3c81a34fa036d7bdd7145c57f3b658d76c76
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2022-12-21 10:49:18 -0500

    Time: 105 ms (40.69%), Space: 44.3 MB (45.56%) - LeetHub

--- 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.js
+++ 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.js
@@ -11,24 +11,23 @@
  * @return {number[][]}
  */
 var levelOrderBottom = function(root) {
-    let res =[];
-       if(root===null) {
-        return res;
+let res = [];
+    if(root === null) {
+        return res
     }
     let queue = [root];
-    while(queue.length>0) {
+    while(queue.length >0) {
         const levelSize = queue.length;
-        let currLevel=[];
-        for(let i=0;i<levelSize;i++) {
+        let currLevel = [];
+        for(let i=0; i<levelSize;i++) {
             let curr = queue.shift();
-            currLevel.push(curr.val)
-              if(curr.left !== null){
-                  queue.push(curr.left)
-              }
+            currLevel.push(curr.val);
+            if(curr.left !== null) {
+                queue.push(curr.left)
+            }
             if(curr.right !== null) {
                 queue.push(curr.right)
             }
-          
         }
         res.unshift(currLevel)
     }

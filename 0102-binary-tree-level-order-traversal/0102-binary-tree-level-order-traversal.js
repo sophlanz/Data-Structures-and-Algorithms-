56ef385bda820cb6294425583997792f2af7f666
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2022-12-21 09:55:29 -0500

    Time: 116 ms (23.39%), Space: 44.3 MB (51.09%) - LeetHub

---
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function(root) {
+let res=[];
+    if(root === null) {
+        return res
+    }
+    const queue = [root];
+    while(queue.length >0) {
+        const levelSize = queue.length;
+        currLevel = [];
+        for(let i=0;i<levelSize;i++) {
+            const curr = queue.shift();
+            currLevel.push(curr.val)
+            if(curr.left !== null)  {
+                queue.push(curr.left)
+            }
+            if(curr.right !== null) {
+                queue.push(curr.right)
+            }
+        }
+        res.push(currLevel)
+    }
+    return res
+};

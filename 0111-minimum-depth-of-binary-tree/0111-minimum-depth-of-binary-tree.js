79ece82b7e512952462fddeef93bacc6aea9592d
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2022-12-21 11:37:47 -0500

    Time: 261 ms (82.44%), Space: 82.8 MB (92.26%) - LeetHub

---
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var minDepth = function(root) {
+    let res =[];
+    if (root === null) {
+        return res;
+    }
+    let queue = [root];
+         let minDepth =0
+    while(queue.length >0) {
+        const levelSize = queue.length;
+      minDepth ++;
+        for(let i=0;i<levelSize;i++) {
+         
+            let curr = queue.shift();
+            if(curr.left === null && curr.right === null) {
+                return minDepth
+            }
+            if(curr.left !== null) {
+                queue.push(curr.left)
+            }
+            if(curr.right !== null) {
+                queue.push(curr.right)
+            }
+        }
+    }
+};

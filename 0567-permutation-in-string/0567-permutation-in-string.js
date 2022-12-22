95dc6b2a90608bdae33e22b85a79a9d65278df99
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2022-12-22 11:44:39 -0500

    Time: 129 ms (69.39%), Space: 48.6 MB (33.52%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.js
+++ 0567-permutation-in-string/0567-permutation-in-string.js
@@ -5,23 +5,23 @@
  */
 var checkInclusion = function(s1, s2) {
 let start=0,
-    match=0,
-    map={};
+    map={},
+    match=0;
     for(let i=0;i<s1.length;i++) {
         map[s1[i]] = map[s1[i]] +1 || 1;
     }
-    for(let end=0;end<s2.length;end++) {
+    for(let end=0; end<s2.length;end++) {
         let rChar = s2[end];
-       if(rChar in map) {
-           map[rChar]--
-           if(map[rChar]===0) {
-               match++
-           }
-       }
-        if(Object.keys(map).length === match) {
+        if(rChar in map) {
+            map[rChar]--
+            if(map[rChar]===0) {
+                match++
+            }
+        }
+        if(match === Object.keys(map).length) {
             return true
         }
-        if(end>=s1.length-1 ) {
+        if(end-start+1 >= s1.length) {
             let lChar = s2[start];
             if(lChar in map) {
                 if(map[lChar]===0) {
@@ -32,5 +32,5 @@ let start=0,
             start++
         }
     }
-    return false
+    return false;
 };

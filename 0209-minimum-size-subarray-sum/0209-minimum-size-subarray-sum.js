3a4c44b1c129e879d2aa92a8c6a049176edcfdcc
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2022-12-22 11:50:52 -0500

    Time: 58 ms (99.64%), Space: 45.8 MB (92.48%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.js
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.js
@@ -5,18 +5,17 @@
  */
 var minSubArrayLen = function(target, nums) {
 let start=0,
-    sum=0,
-    minLen=Infinity;
+    minLen=Infinity,
+    sum=0;
     for(let end=0;end<nums.length;end++) {
         let rChar = nums[end];
-        sum += rChar
+        sum += rChar;
         while(sum >= target) {
-            minLen=Math.min(minLen,end-start+1);
-            let lChar = nums[start]
-            sum -= lChar
+            minLen = Math.min(minLen,end-start+1);
+            let lChar = nums[start];
+            sum -= lChar;
             start++
         }
-        
     }
-   return minLen === Infinity ? 0 : minLen;
+return    minLen === Infinity ? 0 : minLen;
 };

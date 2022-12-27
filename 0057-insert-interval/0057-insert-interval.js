16ddabd9aea22887208a556142a6fc304ddf5069
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2022-12-27 11:11:20 -0500

    Time: 80 ms (84.83%), Space: 43.9 MB (84.83%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.js
+++ 0057-insert-interval/0057-insert-interval.js
@@ -4,18 +4,18 @@
  * @return {number[][]}
  */
 var insert = function(intervals, newInterval) {
-let merged=[];
+let merged = [];
     let i=0;
-    while(i<intervals.length && intervals[i][1]< newInterval[0]) {
-        merged.push(intervals[i])
+    while(i<intervals.length && intervals[i][1] < newInterval[0]) {
+        merged.push(intervals[i]);
         i++
     }
-    while(i<intervals.length && intervals[i][0]<=newInterval[1]) {
-        newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
-        newInterval[1] = Math.max(newInterval[1],intervals[i][1])
+    while(i<intervals.length && intervals[i][0]<= newInterval[1]) {
+        newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
+        newInterval[1]= Math.max(intervals[i][1], newInterval[1]);
         i++
     }
-    merged.push(newInterval)
+    merged.push(newInterval);
     while(i<intervals.length) {
         merged.push(intervals[i])
         i++

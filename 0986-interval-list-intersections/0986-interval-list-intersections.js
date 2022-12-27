7483a0057cac365ed1c5d139ba2ab7543218f982
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2022-12-27 12:50:50 -0500

    Time: 97 ms (81.14%), Space: 48.3 MB (78.29%) - LeetHub

--- 0986-interval-list-intersections/0986-interval-list-intersections.js
+++ 0986-interval-list-intersections/0986-interval-list-intersections.js
@@ -4,20 +4,21 @@
  * @return {number[][]}
  */
 var intervalIntersection = function(firstList, secondList) {
-let result = [];
+    
+let result =[];
     let i=0;
     let j=0;
-    while( i<firstList.length && j<secondList.length ) {
-        let aOverLapB= firstList[i][0] >= secondList[j][0] && firstList[i][0]<= secondList[j][1] ;
-        let bOverLapA= secondList[j][0] >= firstList[i][0] && secondList[j][0]<= firstList[i][1] ;
-        if(aOverLapB || bOverLapA) {
-            result.push([Math.max(firstList[i][0], secondList[j][0]), Math.min(firstList[i][1], secondList[j][1])]); 
-        }
-        if(firstList[i][1]< secondList[j][1]) {
-            i++
-        } else {
-            j++
-        }
+while(i<firstList.length && j <secondList.length) {
+    let aOverlapB = firstList[i][0] >= secondList[j][0] && firstList[i][0]<= secondList[j][1];
+    let bOverlapA = secondList[j][0] >= firstList[i][0] && secondList[j][0] <= firstList[i][1];
+    if(aOverlapB || bOverlapA) {
+        result.push([Math.max(firstList[i][0],secondList[j][0]),Math.min(secondList[j][1], firstList[i][1])]);
     }
+    if(firstList[i][1]<secondList[j][1]) {
+        i++
+    } else {
+        j++
+    }
+}
     return result
 };

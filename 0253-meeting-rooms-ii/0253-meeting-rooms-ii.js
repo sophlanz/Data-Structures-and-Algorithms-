b5e5454c1ca70bdaa475769c64c0d9e5519e11cd
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2022-12-27 16:12:36 -0500

    Time: 106 ms (69.18%), Space: 45.4 MB (29.50%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.js
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.js
@@ -3,22 +3,17 @@
  * @return {number}
  */
 var minMeetingRooms = function(intervals) {
-	intervals.sort((a, b) => a[0] - b[0]);
-	let minRooms = 0;
-	let minHeap = new MinPriorityQueue();
-    
-for (let i = 0; i < intervals.length; i++) {
-        // remove all the meetings that have ended
-		const interval = intervals[i];
-     while (minHeap.size() > 0 && interval[0] >= minHeap.front().element) {
-      minHeap.dequeue().element;
+const minHeap = new MinPriorityQueue();
+    intervals.sort((a,b)=> a[0]-b[0])
+    let minRooms = 0
+for(let i=0;i<intervals.length;i++) {
+    const interval = intervals[i];
+    if(minHeap.size()>0 && interval[0]>= minHeap.front().element){
+        minHeap.dequeue().element
     }
-        // add the current meeting into min_heap
-    minHeap.enqueue(interval[1]).element;
-    // all active meetings are in the min_heap, so we need rooms for all of them.
-    minRooms = Math.max(minRooms, minHeap.size());
-	}
-	return minRooms;
+    minHeap.enqueue(interval[1]).element
+    minRooms = Math.max(minHeap.size(),minRooms)
+}
+    return minRooms
 };
-
 

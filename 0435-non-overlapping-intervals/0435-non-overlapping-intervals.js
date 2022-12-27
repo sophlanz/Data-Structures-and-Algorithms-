cba96ff8785003705a1926eb302dcf6346a5543d
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2022-12-27 16:56:38 -0500

    Time: 253 ms (92.65%), Space: 75.6 MB (29.11%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.js
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.js
@@ -4,16 +4,17 @@
  */
 var eraseOverlapIntervals = function(intervals) {
 intervals.sort((a,b)=> a[1]-b[1]);
-    let count =0;
     let curr = intervals[0];
+    let count=0;
     for(let i=1;i<intervals.length;i++) {
-        if(intervals[i][0]< curr[1]) {
+        const interval = intervals[i];
+        if(interval[0]< curr[1]) {
             count++
         }else {
             curr=intervals[i]
         }
     }
-    return count;
+    return count
 };
  
     

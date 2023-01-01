681f529f03cb711c546874fefbdf22d8c1d61c26
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-01 15:53:43 -0500

    Time: 79 ms (95.26%), Space: 44.7 MB (84.16%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.js
+++ 0200-number-of-islands/0200-number-of-islands.js
@@ -3,31 +3,26 @@
  * @return {number}
  */
 var numIslands = function(grid) {
- let count = 0;
- let rows = grid.length;
-let cols = grid[0].length;
+let count = 0;
+	
 	function callDFS(grid, i, j) {
-        //return if it is not a valid cell or a water cell
-		if (i < 0 || i >= rows || j < 0 || j >= cols || grid[i][j] == '0') {
+		if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0') {
 			return;
 		}
-		//mark cell as visited by making it a water cell
+		
 		grid[i][j] = '0';
-		//recursively visit all cells
+		
 		callDFS(grid, i + 1, j); // down
 		callDFS(grid, i - 1, j); // up
 		callDFS(grid, i, j + 1); // right
 		callDFS(grid, i, j - 1); // left
-       
 	}
 
-	for (let i = 0; i < rows; i++) {
-		for (let j = 0; j < cols; j++) {
+	for (let i = 0; i < grid.length; i++) {
+		for (let j = 0; j < grid[i].length; j++) {
 			if (grid[i][j] == '1') {
-				count ++;
+				count += 1;
 				callDFS(grid, i, j);
-                console.log(count)
-    
 			}
 		}
 	}

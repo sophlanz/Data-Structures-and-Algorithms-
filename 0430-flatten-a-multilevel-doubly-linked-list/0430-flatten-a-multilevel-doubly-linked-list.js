1f1803f2770de07dfc5a7ce06ed5c8d1f1e280b4
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-03 10:43:09 -0500

    Time: 95 ms (46.34%), Space: 43.4 MB (85.82%) - LeetHub

--- 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.js
+++ 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.js
@@ -20,11 +20,11 @@ var flatten = function(head) {
         if(cur.child) {
             if(cur.next) stack.push(cur.next);
             cur.next = cur.child;
-            cur.next.prev = cur;
-            cur.child = null;
+            cur.next.prev=cur;
+            cur.child = null
         } else if(!cur.next && stack.length !==0) {
             cur.next = stack.pop();
-            cur.next.prev = cur
+            cur.next.prev = cur;
         }
         cur = cur.next;
     }

ddbde158887e589275748844067041f53757dd92
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-04 11:52:28 -0500

    Time: 308 ms (48.94%), Space: 56.8 MB (25.53%) - LeetHub

--- 0582-kill-process/0582-kill-process.js
+++ 0582-kill-process/0582-kill-process.js
@@ -5,23 +5,24 @@
  * @return {number[]}
  */
 var killProcess = function(pid, ppid, kill) {
- const parentMap = {};
+    let parentMap = {};
     for(let i=0;i<pid.length;i++) {
         const parent = ppid[i];
         const child = pid[i];
         parentMap[parent] = parentMap[parent] || [];
         parentMap[parent].push(child);
     };
+    
     let result = [];
     DFStoKill(kill);
     
-     function DFStoKill (killNode) {
+    function DFStoKill(killNode) {
         result.push(killNode);
         if(parentMap[killNode]) {
             for(const child of parentMap[killNode]) {
-                DFStoKill(child)
+                DFStoKill(child);
             }
         }
-    } 
-    return result
+    }
+    return result;
 };

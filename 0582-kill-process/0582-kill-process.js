276d726c8c98310667592d53fca831496c92e57c
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-04 11:48:27 -0500

    Time: 167 ms (85.11%), Space: 53.4 MB (78.72%) - LeetHub

--- 0582-kill-process/0582-kill-process.js
+++ 0582-kill-process/0582-kill-process.js
@@ -5,24 +5,23 @@
  * @return {number[]}
  */
 var killProcess = function(pid, ppid, kill) {
-	// Create Graph: Parent-Map
-    const parentMap = {};
-    for(let idx = 0; idx < pid.length; idx++){
-       const parent = ppid[idx];
-       const child = pid[idx];
-       if(!parentMap[parent]) parentMap[parent] = [];
-       parentMap[parent].push(child);
-    }
-    // Inititate traversal
-    const output = [];
-    dfsToKill(kill);
-    // populate output list with killed nodes
-    function dfsToKill(node){
-        output.push(node);
-        if(parentMap[node]){
-            for(let child of parentMap[node])
-                dfsToKill(child);
+ const parentMap = {};
+    for(let i=0;i<pid.length;i++) {
+        const parent = ppid[i];
+        const child = pid[i];
+        parentMap[parent] = parentMap[parent] || [];
+        parentMap[parent].push(child);
+    };
+    let result = [];
+    DFStoKill(kill);
+    
+     function DFStoKill (killNode) {
+        result.push(killNode);
+        if(parentMap[killNode]) {
+            for(const child of parentMap[killNode]) {
+                DFStoKill(child)
+            }
         }
-    }
-    return output;
+    } 
+    return result
 };

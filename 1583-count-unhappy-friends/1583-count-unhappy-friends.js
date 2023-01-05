dda79afa49cbb9f97beaa575d547a3145f9c2ec3
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-05 09:24:46 -0500

    Time: 130 ms (38.96%), Space: 46.4 MB (66.23%) - LeetHub

--- 1583-count-unhappy-friends/1583-count-unhappy-friends.js
+++ 1583-count-unhappy-friends/1583-count-unhappy-friends.js
@@ -5,20 +5,20 @@
  * @return {number}
  */
 var unhappyFriends = function(n, preferences, pairs) {
-    let preferenceMap = {};
+    let preferenceMap={};
     for(let [i,j] of pairs) {
-        preferenceMap[i] = preferences[i].indexOf(j);
+        preferenceMap[i]= preferences[i].indexOf(j);
         preferenceMap[j] = preferences[j].indexOf(i);
     }
-    let unhappy=0;
+    let unhappy =0;
     for(let person=0;person<n;person++) {
         for(let rank=0;rank<preferenceMap[person];rank++) {
             const partner = preferences[person][rank];
-            if(preferences[partner].indexOf(person)<preferenceMap[partner]) {
+            if(preferences[partner].indexOf(person)< preferenceMap[partner]){
                 unhappy++
                 break;
             }
         }
     }
-    return unhappy
+    return unhappy;
 };

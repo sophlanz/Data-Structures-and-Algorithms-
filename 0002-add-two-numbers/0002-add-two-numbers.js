e5def2cd0209abb6d5e232cd48a2ab60dd7df2b1
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-06 09:14:34 -0500

    Time: 93 ms (98.14%), Space: 46.9 MB (84.11%) - LeetHub

---
+++ 0002-add-two-numbers/0002-add-two-numbers.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+ let dummy = new ListNode(0);
+    let list = dummy;
+    let carry =0;
+    while(l1||l2||carry) {
+        const v1 = l1? l1.val : 0;
+        const v2 = l2? l2.val :0;
+        const node = new ListNode((v1+v2+carry)%10);
+        carry = Math.floor((v1+v2+carry)/10);
+        list.next = node;
+        list = node;
+        l1 = l1? l1.next: null;
+        l2 = l2?l2.next:null
+    }
+    return dummy.next
+};

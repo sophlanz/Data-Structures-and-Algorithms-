50dcd1a1458126f54a5fe09ec8f741262258654e
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-06 12:59:14 -0500

    Time: 193 ms (32.73%), Space: 45.6 MB (52.59%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
@@ -3,13 +3,13 @@
  * @return {number}
  */
 var firstUniqChar = function(s) {
-let map = {};
-    for(const char of s) {
-        map[char] = map[char]+1 ||1
-    }
+    let charMap = {};
+    for (const char of s) {
+        charMap[char] = charMap[char] +1 || 1;
+}
     for(let i=0;i<s.length;i++) {
         const char = s[i];
-        if(map[char]===1) {
+        if(charMap[char]===1) {
             return i
         }
     }

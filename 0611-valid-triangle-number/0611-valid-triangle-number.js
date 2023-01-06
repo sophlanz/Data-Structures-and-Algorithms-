be0614d8b866f8d7cf9ddd642bad04b2ae28d467
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-06 11:49:32 -0500

    Time: 142 ms (77.87%), Space: 44.6 MB (16.39%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.js
+++ 0611-valid-triangle-number/0611-valid-triangle-number.js
@@ -3,14 +3,14 @@
  * @return {number}
  */
 var triangleNumber = function(nums) {
-    nums.sort((a,b)=> a-b);
-    let count=0;
-    for(let k=nums.length-1;k>1 ;k--) {
-        for(let i=0, j=k-1;i<j;) {
-            if(nums[i] +nums[j] > nums[k]) {
+  nums.sort((a,b)=> a-b);
+let count=0;
+    for(let k=nums.length-1;k>1;k--) {
+        for(let i=0,j=k-1;i<j;) {
+            if(nums[i]+nums[j] > nums[k]) {
                 count += j-i;
                 j--
-            }else {
+            } else {
                 i++
             }
         }

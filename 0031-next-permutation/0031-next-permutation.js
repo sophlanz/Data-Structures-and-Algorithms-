eedd7f0a408e228a6ecb9fd364055b8d6f0a3331
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-07 13:20:09 -0500

    Time: 74 ms (79.63%), Space: 42.8 MB (59.51%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.js
+++ 0031-next-permutation/0031-next-permutation.js
@@ -5,22 +5,23 @@
 var nextPermutation = function(nums) {
     //get pivot
     let pivot =-1;
-    for(let i=nums.length-1;i>0;i--) {
+    for(let i=nums.length-1;i>0;i--) { //get first decrease in values
         if(nums[i]>nums[i-1]) {
             pivot = i-1;
-            break;
+            break;                  //once found, break
         }
     }
     if(pivot=== -1) {
         return nums.reverse();
     }
-    for(let j=nums.length-1;j>pivot;j--) {
-        if(nums[j]>nums[pivot]) {
+    for(let j=nums.length-1;j>pivot;j--) {              //get first number greater than pivot, to the right of pivot 
+        if(nums[j]>nums[pivot]) {   
             [nums[j], nums[pivot]]= [nums[pivot], nums[j]]
-            break;
+            break;                                      //once found, break
         }
     };
-    for(let i=pivot+1, j=nums.length-1;i<j;i++,j--) {
+    for(let i=pivot+1, j=nums.length-1;i<j;i++,j--) {   //to the left of pivot. we want to change these numbers from descending order to ascending order.
+                                                        //This allows us to get the next greater number. The swap doesn't change the descending order of                                                                          arr[pivot+1...end].
          [nums[j], nums[i]]= [nums[i], nums[j]]
     }
     return nums

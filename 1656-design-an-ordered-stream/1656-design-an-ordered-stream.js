d0b5ae7c37ef0f8ecfb207f254ec10dbda3d1d6c
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-08 14:04:16 -0500

    Time: 191 ms (81.75%), Space: 54.2 MB (33.94%) - LeetHub

---
+++ 1656-design-an-ordered-stream/1656-design-an-ordered-stream.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number} n
+ */
+var OrderedStream = function(n) {
+    this.list=[];
+    this.pointer=0;
+};
+
+/** 
+ * @param {number} idKey 
+ * @param {string} value
+ * @return {string[]}
+ */
+OrderedStream.prototype.insert = function(idKey, value) {
+    this.list[idKey-1] = value;
+    let res=[];
+    while(this.list[this.pointer]) {
+        res.push(this.list[this.pointer])
+        this.pointer++
+    }
+    return res;
+};
+
+/** 
+ * Your OrderedStream object will be instantiated and called as such:
+ * var obj = new OrderedStream(n)
+ * var param_1 = obj.insert(idKey,value)
+ */

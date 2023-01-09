4375304cdc29cbbdd70b185035f558662596dc9c
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-09 15:04:48 -0500

    Time: 89 ms (88.45%), Space: 51 MB (46.48%) - LeetHub

---
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var removeDuplicates = function(s, k) {
+    let stack = [];
+    for(const char of s) {
+        if(stack.length !== 0 && stack[stack.length-1][0]===char) {
+            stack[stack.length-1][1]++;
+            if(stack[stack.length-1][1]===k ) {
+                stack.pop(); 
+            }
+            
+        }else {
+            stack.push([char,1])
+        }
+    }
+    let result = '';
+    for(const [char,count] of stack) {
+        result += char.repeat(count)
+    }
+    return result
+};

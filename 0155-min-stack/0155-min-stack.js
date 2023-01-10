6f49d64c5c2ebeb734d25d7ed7b16e88410bd321
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-10 09:19:23 -0500

    Time: 117 ms (71.38%), Space: 50 MB (33.05%) - LeetHub

--- 0155-min-stack/0155-min-stack.js
+++ 0155-min-stack/0155-min-stack.js
@@ -1,6 +1,7 @@
 
 var MinStack = function() {
     this.stack=[];
+    this.minStack=[]
 };
 
 /** 
@@ -8,31 +9,31 @@ var MinStack = function() {
  * @return {void}
  */
 MinStack.prototype.push = function(val) {
-    this.stack.push({
-        val:val,
-        min: this.stack.length ? Math.min(this.stack[this.stack.length-1].min, val) : val
-    })
+    this.stack.push(val);
+   this.minStack.push(Math.min(val, this.minStack.length ? this.minStack[this.minStack.length-1] : Infinity))
 };
 
 /**
  * @return {void}
  */
 MinStack.prototype.pop = function() {
+    this.minStack.pop();
     this.stack.pop();
+    
 };
 
 /**
  * @return {number}
  */
 MinStack.prototype.top = function() {
-    return this.stack[this.stack.length-1].val;
+    return this.stack[this.stack.length-1];
 };
 
 /**
  * @return {number}
  */
 MinStack.prototype.getMin = function() {
-    return this.stack[this.stack.length-1].min;
+    return this.minStack[this.minStack.length-1];
 };
 
 /** 

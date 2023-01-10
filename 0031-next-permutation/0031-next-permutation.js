f80d59297b27e289d52b869e6061ce8d4eefc7f3
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-10 10:38:07 -0500

    Time: 71 ms (84.60%), Space: 42.8 MB (53.15%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.js
+++ 0031-next-permutation/0031-next-permutation.js
@@ -3,26 +3,26 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var nextPermutation = function(nums) {
-    //get pivot
-    let pivot =-1;
-    for(let i=nums.length-1;i>0;i--) { //get first decrease in values
+let pivot = -1;
+    for(let i=nums.length-1;i>0;i--) {
         if(nums[i]>nums[i-1]) {
             pivot = i-1;
-            break;                  //once found, break
+            break;
         }
     }
-    if(pivot=== -1) {
+    if(pivot === -1) {
         return nums.reverse();
     }
-    for(let j=nums.length-1;j>pivot;j--) {              //get first number greater than pivot, to the right of pivot 
-        if(nums[j]>nums[pivot]) {   
-            [nums[j], nums[pivot]]= [nums[pivot], nums[j]]
-            break;                                      //once found, break
+    
+    for(let i=nums.length-1;i>pivot;i--) {
+        if(nums[i]>nums[pivot]) {
+            [nums[i],nums[pivot]]=[nums[pivot],nums[i]];
+            break;
         }
-    };
-    for(let i=pivot+1, j=nums.length-1;i<j;i++,j--) {   //to the left of pivot. we want to change these numbers from descending order to ascending order.
-                                                        //This allows us to get the next greater number. The swap doesn't change the descending order of                                                                          arr[pivot+1...end].
-         [nums[j], nums[i]]= [nums[i], nums[j]]
     }
-    return nums
+    for(let i=pivot + 1, j=nums.length-1;i<j;i++,j--) {
+        [nums[i],nums[j]] = [nums[j],nums[i]];
+    
+    } 
+    return nums;
 };

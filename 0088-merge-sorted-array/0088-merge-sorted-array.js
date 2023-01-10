f15fc9639f48512f40e2bafd58de913c678f30cd
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-10 11:41:02 -0500

    Time: 65 ms (82.12%), Space: 41.8 MB (81.09%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -6,14 +6,14 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-   for(let i=nums1.length-1,a=m-1,b=n-1;b>=0;i--) {
-       if(nums1[a] > nums2[b] ) {
-           nums1[i] = nums1[a];
-           a--
-       }else {
-           nums1[i] = nums2[b];
-           b--
-       }
-   }
+    for(let i=nums1.length-1,a=m-1,b=n-1;b>=0;i--) {
+        if(nums1[a]>nums2[b]) {
+            nums1[i] = nums1[a];
+            a--;
+        }else {
+            nums1[i] = nums2[b];
+            b--;
+        }
+    }
     return nums1;
 };

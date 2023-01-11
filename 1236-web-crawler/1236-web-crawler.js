6d60fdfc6daa4a4c060cabf3524ef061cff4120a
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-11 18:05:52 -0500

    Time: 109 ms (79.07%), Space: 51 MB (44.19%) - LeetHub

---
+++ 1236-web-crawler/1236-web-crawler.js
@@ -0,0 +1,34 @@
+/**
+ * // This is the HtmlParser's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * function HtmlParser() {
+ *
+ *		@param {string} url
+ *     	@return {string[]}
+ *     	this.getUrls = function(url) {
+ *      	...
+ *     	};
+ * };
+ */
+
+/**
+ * @param {string} startUrl
+ * @param {HtmlParser} htmlParser
+ * @return {string[]}
+*/
+var crawl = function(startUrl, htmlParser) {
+    const getHostName = (url) => url.split('/')[2];
+    const hostName= getHostName(startUrl)
+    let queue=[startUrl]
+    let seen = new Set([startUrl])
+    while(queue.length) {
+        const currUrl = queue.shift();
+        for(const url of htmlParser.getUrls(currUrl)) {
+            if(!seen.has(url) && getHostName(url).includes(hostName)) {
+                seen.add(url);
+                queue.push(url)
+            }
+        }
+    }
+    return [...seen.values()]
+};

88d4651a6a3055c374727e201e34fb9c36aafc5f
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-13 13:10:05 -0500

    Time: 388 ms (8.27%), Space: 55.9 MB (7.15%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -3,11 +3,19 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
- let arr = String(x).split('');
-    while(arr.length >1) {
-        if(arr.pop() !== arr.shift()) {
-            return false
-        }
+
+ let arr = Array.from(String(x));
+    let l=0;
+    let r=arr.length-1
+ 
+    while(l<=r) {
+        console.log(arr[l])
+       if(arr[l]=== arr[r]) {
+           l++
+           r--
+       } else {
+           return false
+       }
     }
     return true;
     };

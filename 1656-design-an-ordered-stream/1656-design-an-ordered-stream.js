0e986e82766a826a4d429c99b8e5ccc93c8c7b46
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-16 18:52:18 -0500

    Time: 197 ms (71.90%), Space: 54.3 MB (33.99%) - LeetHub

--- 1656-design-an-ordered-stream/1656-design-an-ordered-stream.js
+++ 1656-design-an-ordered-stream/1656-design-an-ordered-stream.js
@@ -2,8 +2,8 @@
  * @param {number} n
  */
 var OrderedStream = function(n) {
-    this.ptr = 0;
-    this.stream=[];
+    this.stream = [];
+    this.pointer=0;
 };
 
 /** 
@@ -12,11 +12,11 @@ var OrderedStream = function(n) {
  * @return {string[]}
  */
 OrderedStream.prototype.insert = function(idKey, value) {
-    this.stream[idKey-1] = value;
+this.stream[idKey-1] = value;
     let result = [];
-    while(this.stream[this.ptr]) {
-        result.push(this.stream[this.ptr])
-        this.ptr++
+    while(this.stream[this.pointer]) {
+        result.push(this.stream[this.pointer]);
+        this.pointer++
     }
     return result;
 };

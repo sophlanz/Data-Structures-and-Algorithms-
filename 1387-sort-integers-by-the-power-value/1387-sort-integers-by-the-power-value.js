04212e485220d29631606338c2029e0d36145481
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-16 10:34:17 -0500

    Time: 113 ms (87.35%), Space: 47.5 MB (77.11%) - LeetHub

--- 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.js
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.js
@@ -5,19 +5,19 @@
  * @return {number}
  */
 var getKth = function(lo, hi, k) {
-    let res =[];
+    let result = [];
     let memo = new Map();
     memo.set(1,0);
-    const getSteps = (num) => {
+    const getPower = (num) => {
         if(memo.has(num)) return memo.get(num);
-        const steps = num % 2 ? getSteps(num*3+1) +1 : getSteps(num/2) +1;
-        memo.set(num, steps)
+        const steps = num % 2 ? getPower(num*3+1) +1 : getPower(num/2) +1;
+        memo.set(num,steps);
         return memo.get(num);
     }
     while(lo<=hi) {
-        res.push([lo,getSteps(lo)])
+        result.push([lo,getPower(lo)])
         lo++
     }
-    res.sort((a,b)=> a[1]-b[1]);
-    return res[k-1][0];
+    result.sort((a,b)=> a[1]-b[1]);
+    return result[k-1][0];
 };

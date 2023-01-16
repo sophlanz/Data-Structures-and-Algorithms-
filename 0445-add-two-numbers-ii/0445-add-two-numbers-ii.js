0ee2c62a988ac7d64dd842c4d70580dd39cdde60
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-16 09:56:37 -0500

    Time: 94 ms (97.93%), Space: 46.9 MB (95.02%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.js
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.js
@@ -11,28 +11,25 @@
  * @return {ListNode}
  */
 var addTwoNumbers = function(l1, l2) {
-const s1=[];
-const s2=[];
+ let stack1=[],
+     stack2=[];
     while(l1) {
-        s1.push(l1.val);
-        l1=l1.next;
+        stack1.push(l1.val);
+        l1 = l1.next;
     }
     while(l2) {
-        s2.push(l2.val);
+        stack2.push(l2.val);
         l2=l2.next;
     }
-    let list = null;
     let carry=0;
-    while(s1.length || s2.length || carry) {
-        const v1 = s1.length ? s1.pop() : 0;
-        const v2 = s2.length? s2.pop() :0;
+    let list = null;
+    while(stack1.length || stack2.length ||carry ) {
+        const v1 = stack1.length ? stack1.pop() : 0;
+        const v2= stack2.length ? stack2.pop() :0;
         const node = new ListNode((v1+v2+carry)%10);
-        carry = Math.floor((v1+v2+carry)/10)
-        
-            node.next=list;
-       
+        carry = Math.floor((v1+v2+carry) /10);
+        node.next=list;
         list=node;
     }
     return list
- 
 };

ff1680703765e764b3b3e8386681599cfc5a27cf
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-16 16:26:17 -0500

    Time: 1104 ms (28.15%), Space: 49.4 MB (41.48%) - LeetHub

--- 1169-invalid-transactions/1169-invalid-transactions.js
+++ 1169-invalid-transactions/1169-invalid-transactions.js
@@ -3,22 +3,21 @@
  * @return {string[]}
  */
 var invalidTransactions = function(transactions) {
-    const invalid = Array.from(transactions.length).fill(false);
+    let invalid = Array.from(transactions.length).fill(false);
     for(let i=0;i<transactions.length;i++) {
         const [name,time,price,city] = transactions[i].split(',');
         if(price > 1000) invalid[i] = true;
         for(let j=i+1;j<transactions.length;j++) {
-            const [name2, time2, price2, city2] = transactions[j].split(',');
-            if(Math.abs(time-time2)<=60 && name === name2 && city !== city2) {
+            const [name2,time2,price2,city2] = transactions[j].split(',');
+            if(name === name2 && city !== city2 && Math.abs(time-time2) <= 60) {
                 invalid[i] = true;
                 invalid[j] = true;
             }
         }
+                                                             
     }
-    return invalid.reduce((acc,val,idx)=> {
-        if(val) {
-            acc.push(transactions[idx])
-        }
-        return acc;
+ return invalid.reduce((acc,val,index)=> {
+        if(val) acc.push(transactions[index])
+     return acc;
     },[])
 };

fc951e512b67d8d9cf735adfa0c6c1df0ff65f28
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-16 18:14:44 -0500

    Time: 62 ms (82.68%), Space: 42.1 MB (33.24%) - LeetHub

--- 0394-decode-string/0394-decode-string.js
+++ 0394-decode-string/0394-decode-string.js
@@ -3,23 +3,23 @@
  * @return {string}
  */
 var decodeString = function(s) {
-    let str='',
-        stack=[],
-        num=0;
+    let stack=[],
+        num=0,
+        str='';
     for(let i=0;i<s.length;i++) {
-        if(s[i] === "[") {
+        if(s[i] === '[') {
             stack.push(str);
             stack.push(num);
-            str=''
+            str='';
             num=0
-        }else if (s[i] === "]") {
-            let currNum=stack.pop();
+        }else if (s[i]===']') {
+            let currNum = stack.pop();
             let prevStr = stack.pop();
             str = prevStr + str.repeat(currNum);
-        }else if(!isNaN(s[i])) {
+        }else if (!isNaN(s[i])) {
             num = num*10 + Number(s[i]);
         }else {
-            str += s[i];
+            str+= s[i];
         }
     }
     return str;

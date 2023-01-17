f98d1fdaf5dc4bbe855f7256e3dce7f5796b19df
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-17 17:12:01 -0500

    Time: 126 ms (87.64%), Space: 48.7 MB (62.92%) - LeetHub

--- 0935-knight-dialer/0935-knight-dialer.js
+++ 0935-knight-dialer/0935-knight-dialer.js
@@ -8,27 +8,27 @@ var knightDialer = function(n) {
         [4,6],
         [8,6],
         [7,9],
-        [4,8],
-        [9,3,0],
+        [8,4],
+        [0,9,3],
         [],
-        [0,7,1],
+        [0,1,7],
         [2,6],
         [1,3],
-        [2,4] 
+        [2,4]
     ];
     let dp = new Array(10).fill(1);
     for(let i=2;i<=n;i++) {
         let newDp = new Array(10).fill(0);
         for(let digit=0;digit<=9;digit++) {
             for(const nextKey of nextKeys[digit]) {
-                newDp[digit] = (newDp[digit] + dp[nextKey]) % MOD
+                newDp[digit] = (newDp[digit] + dp[nextKey]) %MOD;
             }
         }
         dp = newDp;
     }
-    let res=0;
+    let result = 0;
     for(const count of dp) {
-        res= (res+count) % MOD
+        result = (result+count)%MOD;
     }
-    return res;
+    return result;
 };

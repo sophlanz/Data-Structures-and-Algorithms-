5f6f0885e5611c30e951feb1b255914e8908dbc9
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-17 14:51:52 -0500

    Time: 180 ms (61.02%), Space: 48.1 MB (28.25%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.js
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.js
@@ -4,14 +4,14 @@
  * @return {number}
  */
 var minSteps = function(s, t) {
-let charFreqMap = {};
+   let charFreqMap = {};
     for(let i=0;i<s.length;i++) {
-        charFreqMap[s[i]] ? charFreqMap[s[i]]++ : charFreqMap[s[i]] = 1;
-        charFreqMap[t[i]] ? charFreqMap[t[i]]-- : charFreqMap[t[i]] = -1;
+        charFreqMap[s[i]] ? charFreqMap[s[i]]++ : charFreqMap[s[i]] =1;
+        charFreqMap[t[i]] ? charFreqMap[t[i]]-- : charFreqMap[t[i]] =-1;
     }
-    let count=0
+    let count=0;
     for(let char in charFreqMap) {
-        if (charFreqMap[char] >0) count+= charFreqMap[char]
+        if(charFreqMap[char]<0) count += Math.abs(charFreqMap[char]);
     }
     return count;
 };

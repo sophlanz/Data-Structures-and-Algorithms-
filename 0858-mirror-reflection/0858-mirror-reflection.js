2c09891ca32631276416ccf6ff3c8de384ef56a9
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-18 13:48:07 -0500

    Time: 67 ms (62.22%), Space: 42 MB (28.89%) - LeetHub

--- 0858-mirror-reflection/0858-mirror-reflection.js
+++ 0858-mirror-reflection/0858-mirror-reflection.js
@@ -4,15 +4,16 @@
  * @return {number}
  */
 var mirrorReflection = function(p, q) {
-    while(p%2 === 0 && q%2 ===0) {
-        p = p/2;
-        q=q/2;
-    }
-    if(p%2===0) {
+
+    while(p%2 === 0 && q%2 === 0) {
+        p=p/2;
+        q=q/2
+    };
+    if(p%2 == 0) {
         return 2
-    }
-    if(q%2===0) {
+    }else if (q%2 == 0) {
         return 0
+    }else {
+        return 1;
     }
-    return 1
 };

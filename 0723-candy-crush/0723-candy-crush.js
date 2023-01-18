a6a3ea946cad45d7bbe838c8e46fda6c4649a338
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-18 10:48:30 -0500

    Time: 85 ms (81.73%), Space: 44.4 MB (90.38%) - LeetHub

--- 0723-candy-crush/0723-candy-crush.js
+++ 0723-candy-crush/0723-candy-crush.js
@@ -3,27 +3,27 @@
  * @return {number[][]}
  */
 var candyCrush = function(board) {
-    let rows = board.length;
-    let cols = board[0].length;
+    const rows = board.length;
+    const cols = board[0].length;
     let found=true;
     while(found) {
-        found = false;
-        for(let i=0;i<rows;i++) {
-            for(let j=0;j<cols;j++) {
-                let val = Math.abs(board[i][j]);
-                if(val === 0) continue;
-                if(i<rows-2 && val===Math.abs(board[i+1][j]) && val===Math.abs(board[i+2][j])) {
-                    found=true;
-                    board[i][j] = board[i+1][j] = board[i+2][j] =-val;
-                }
-                if(j<rows-2 && val===Math.abs(board[i][j+1]) && val === Math.abs(board[i][j+2])) {
-                    found=true;
-                    board[i][j] = board[i][j+1] = board[i][j+2] =-val; 
-                }
+        found=false;
+    for(let i=0;i<rows;i++) {
+        for(let j=0;j<cols;j++){
+            let val = Math.abs(board[i][j]);
+            if(val === 0) continue;
+            if(i<rows-2 && val === Math.abs(board[i+1][j]) && val === Math.abs(board[i+2][j])) {
+                found=true
+                board[i][j] = board[i+1][j] = board[i+2][j] = -val;
+            }
+            if(j<rows-2 && val === Math.abs(board[i][j+1]) && val === Math.abs(board[i][j+2])) {
+                found=true;
+                board[i][j] = board[i][j+1] = board[i][j+2] = -val;
             }
         }
+    }
         if(found) {
-            for(let j=0;j<cols;j++) {
+            for(let j =0; j<cols;j++) {
                 let idx = rows-1;
                 for(let i=idx;i>=0;i--) {
                     if(board[i][j] >0) {
@@ -36,6 +36,6 @@ var candyCrush = function(board) {
                 }
             }
         }
-    }
-    return board
+}
+    return board;
 };

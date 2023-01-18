0bada788e3a96764fbdc31e708d9620f25693a15
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-18 16:11:30 -0500

    Time: 106 ms (63.87%), Space: 50.7 MB (48.87%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.js
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.js
@@ -3,16 +3,16 @@
  * @return {number[][]}
  */
 var allPathsSourceTarget = function(graph) {
-let paths = [];
-    const DFS = (index,path) => {
-        if(path[path.length-1] === graph.length-1) {
-            paths.push(path);
-            return;
-        }
-        for(let i=0;i<graph[index].length;i++) {
-            DFS(graph[index][i], [...path,graph[index][i]])
-        }
+    let paths=[];
+const DFS = (index,path) => {
+    if(path[path.length-1] === graph.length-1) {
+        paths.push(path);
+        return;
+    };
+    for(let i=0;i<graph[index].length;i++) {
+        DFS(graph[index][i], [...path,graph[index][i]])
     }
-    DFS(0,[0]);
+}
+DFS(0,[0]);
     return paths;
 };

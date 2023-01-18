7a4d0cf4e595b7eb023a834e448343b06ab3ea22
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-18 10:29:19 -0500

    Time: 69 ms (85.71%), Space: 43.5 MB (69.64%) - LeetHub

--- 0311-sparse-matrix-multiplication/0311-sparse-matrix-multiplication.js
+++ 0311-sparse-matrix-multiplication/0311-sparse-matrix-multiplication.js
@@ -4,19 +4,18 @@
  * @return {number[][]}
  */
 var multiply = function(mat1, mat2) {
-    if(mat1.length ===0||
-      mat2.length ===0 ||
+    if(mat1.length === 0 ||
+      mat2.length === 0 ||
       mat1[0].length !== mat2.length) return [];
     const rows = mat1.length;
     const cols = mat1[0].length;
     const colsB = mat2[0].length;
     let res = new Array(rows).fill(0).map(()=> new Array(colsB).fill(0));
-    
     for(let i=0;i<rows;i++) {
         for(let j=0;j<cols;j++) {
-            if(mat1[i][j]===0) continue;
-            for(let k=0;k<colsB;k++) {
-                res[i][k] += mat1[i][j] * mat2[j][k]
+            if(mat1[i][j] === 0) continue;
+            for(let k =0;k<colsB;k++) {
+                res[i][k] += mat1[i][j] * mat2[j][k];
             }
         }
     }

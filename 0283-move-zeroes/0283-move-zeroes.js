975742bd2517bdd0b68d9a920b43f300ae0b9b7c
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-18 16:44:47 -0500

    Time: 100 ms (68.57%), Space: 47 MB (32.68%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -3,10 +3,10 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-let idxNonZero = 0;
+    let idxNonZero=0
     for(let i=0;i<nums.length;i++) {
         if(nums[i] !== 0) {
-            [nums[i], nums[idxNonZero]] = [nums[idxNonZero],nums[i]];
+            [nums[i], nums[idxNonZero]] = [nums[idxNonZero], nums[i]];
             idxNonZero++
         }
     }

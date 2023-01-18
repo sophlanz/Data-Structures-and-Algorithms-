100d68aa47e876c800066281e381e0fb5ee875cd
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-18 12:34:55 -0500

    Time: 270 ms (20.08%), Space: 47.2 MB (28.69%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.js
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.js
@@ -4,15 +4,14 @@
  * @return {number}
  */
 var findTheWinner = function(n, k) {
-    let queue=[];
-while(n >0) {
-    queue.unshift(n)
-    n--
-}
+    let queue= [];
+ for(let i=1;i<=n;i++) {
+     queue.push(i)
+ };
     while(queue.length !==1) {
-        let x=k
-        while(x >1){
-           queue.push(queue.shift())
+        let x = k;
+        while(x>1) {
+            queue.push(queue.shift())
             x--
         }
         queue.shift();

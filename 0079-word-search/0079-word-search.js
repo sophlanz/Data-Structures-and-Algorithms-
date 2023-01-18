2c405d5b6024049b104f999659645ecb337cdcc7
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-18 12:31:22 -0500

    Time: 670 ms (70.03%), Space: 42.7 MB (69.67%) - LeetHub

--- 0079-word-search/0079-word-search.js
+++ 0079-word-search/0079-word-search.js
@@ -4,28 +4,27 @@
  * @return {boolean}
  */
 var exist = function(board, word) {
-    const rows = board.length;
-    const cols = board[0].length;
+    let rows = board.length;
+    let cols = board[0].length;
     const DFS = (i,j,idx) => {
-        if(word.length === idx) return true;
-        if(i<0 || j<0 || i>= rows || j>= cols || board[i][j] !== word[idx]) return false;
-        board[i][j] = 0;
-        if(DFS(i+1,j,idx+1)||
+        if(idx === word.length) return true
+        if(i<0 || j<0 || i>= rows || j>=cols || board[i][j] !== word[idx]) return false;
+        board[i][j] =0;
+        if(DFS(i+1,j,idx+1) ||
           DFS(i-1,j,idx+1) ||
            DFS(i,j+1,idx+1) ||
-           DFS(i,j-1,idx+1))  return true;
-           else {
-           board[i][j] = word[idx];
-           }
-           
-    };
-
+           DFS(i,j-1,idx+1)
+          )return true
+        else {
+            board[i][j] = word[idx];
+        }
+    }
     for(let i=0;i<rows;i++) {
         for(let j=0;j<cols;j++) {
-            if(board[i][j] === word[0] && DFS(i,j,0)) {
-                return true
+            if(board[i][j] === word[0] && DFS(i,j,0)){
+                return true;
             }
         }
     }
-return false;
+    return false;
 };

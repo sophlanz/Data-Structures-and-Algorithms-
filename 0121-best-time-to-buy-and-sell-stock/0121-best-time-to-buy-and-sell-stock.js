59fd2d0167288a3ccd295ffb4c944a6d0b1c3ba1
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-19 16:28:30 -0500

    Time: 97 ms (59.67%), Space: 51.7 MB (66.72%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,20 +3,16 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-        let l=0;
-        let r=1
-        let max=0
-        while(r<prices.length) {
-            //check if profitable
-            if(prices[l]<prices[r]) {
-                //update max profit if it's more
-                max=Math.max(max,prices[r]-prices[l])
-            }else {
-                //not profitable
-                l=r
-            }
-            //only update right pointer, leave left pointer where it is since we know l leads to a profit
-            r+=1
+let l=0,
+    r=1,
+    max=0;
+    while(r<prices.length) {
+        if(prices[l]<prices[r]) {
+            max = Math.max(max,prices[r]-prices[l]);
+        }else {
+            l=r
         }
-        return max
+        r++
+    }
+    return max
 };

aa94a9c9d44b802a99c228d8e4890f9e33a5231e
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-20 10:05:13 -0500

    Time: 82 ms (51.65%), Space: 42.5 MB (73.67%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.js
+++ 0031-next-permutation/0031-next-permutation.js
@@ -3,23 +3,24 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var nextPermutation = function(nums) {
-let pivot = -1;
+    let pivot = -1;
     for(let i=nums.length-1;i>=0;i--) {
         if(nums[i] > nums[i-1]) {
             pivot = i-1;
-            break
-        }
-    };
-    for(let i=nums.length-1;i>pivot;i--) {
-        if(nums[i] > nums[pivot]) {
-            [nums[i],nums[pivot]] = [nums[pivot], nums[i]];
             break;
         }
     }
-    
+    if(pivot == -1) {
+        return nums.reverse();
+    }
+    for(let i=nums.length-1;i>pivot;i--) {
+        if(nums[i] > nums[pivot]) {
+            [nums[i],nums[pivot]] = [nums[pivot],nums[i]];
+            break;
+        }
+    }
     for(let i=pivot+1,j=nums.length-1;i<j;i++,j--) {
-        [nums[i], nums[j]] = [nums[j],nums[i]];
+        [nums[i],nums[j]] = [nums[j],nums[i]];
     }
     return nums;
-                        
 };

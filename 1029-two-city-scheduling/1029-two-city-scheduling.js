9c8fee32a79703ff8d91083a901baa89784777a4
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-20 09:38:22 -0500

    Time: 58 ms (93.93%), Space: 42 MB (74.09%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.js
+++ 1029-two-city-scheduling/1029-two-city-scheduling.js
@@ -3,6 +3,6 @@
  * @return {number}
  */
 var twoCitySchedCost = function(costs) {
-    costs.sort((a,b)=> (a[1]-a[0])-(b[1]-b[0]));
-   return costs.reduce((total,cost,idx)=> (idx<costs.length/2 ? total + cost[1] : total + cost[0]),0)
+    costs.sort((a,b)=> (a[1]-a[0]) - (b[1]-b[0]));
+    return costs.reduce((total,price,idx)=> (idx< costs.length/2 ? total+ price[1] : total + price[0]),0)
 };

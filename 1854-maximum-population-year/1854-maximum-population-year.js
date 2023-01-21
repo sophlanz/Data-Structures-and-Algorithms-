b1429bb735020a2eb786430c300bd33fc5ae5ded
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-21 12:17:41 -0500

    Time: 106 ms (26.12%), Space: 44.1 MB (50.00%) - LeetHub

--- 1854-maximum-population-year/1854-maximum-population-year.js
+++ 1854-maximum-population-year/1854-maximum-population-year.js
@@ -3,16 +3,16 @@
  * @return {number}
  */
 var maximumPopulation = function(logs) {
-    let year =0;
     let arr = new Array(101).fill(0);
-    for(const [birth,death] of logs) {
-        arr[birth-1950]++;
-        arr[death-1950]--;
-    }
+    let year=0;
+    for(const[birth,death]of logs) {
+        arr[birth-1950]++
+        arr[death-1950]--
+    };
     for(let i=1;i<arr.length;i++) {
         arr[i] += arr[i-1];
-        if(arr[i]> arr[year]) {
-            year=i
+        if(arr[i] > arr[year]) {
+            year=i;
         }
     }
     return year+1950

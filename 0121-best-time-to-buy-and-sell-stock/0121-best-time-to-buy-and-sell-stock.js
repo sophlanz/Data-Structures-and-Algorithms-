71208902d02728984048acf3b389fc045d0d7003
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-21 11:37:03 -0500

    Time: 94 ms (65.59%), Space: 52 MB (28.38%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,17 +3,17 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-  let  profit =0,
-        l=0,
-      r=1;
-    while(r<=prices.length-1) {
-       if(prices[l]<prices[r]) {
-           profit= Math.max(profit,prices[r]-prices[l])
-       }else {
-           l=r
-       }
+ let profit = 0,
+     l=0,
+     r=1;
+    while(r<prices.length) {
+        if(prices[l]<prices[r]) {
+            profit = Math.max(profit, prices[r]-prices[l])
+        }else {
+            l=r
+        }
         r++
     }
-    return profit;
+    return profit
     
 };

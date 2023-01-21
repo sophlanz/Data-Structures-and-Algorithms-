7cd359f8fda98225f17b54bb4f26868413d68f08
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-21 13:00:47 -0500

    Time: 1951 ms (11.36%), Space: 52.5 MB (13.64%) - LeetHub

--- 0835-image-overlap/0835-image-overlap.js
+++ 0835-image-overlap/0835-image-overlap.js
@@ -4,31 +4,28 @@
  * @return {number}
  */
 var largestOverlap = function(img1, img2) {
-         const rows = img1.length;
-        const cols = img1[0].length;
-        let s1=[];
-        let s2=[];
-        for(let i=0;i<rows;i++){
-            for(let j=0;j<cols;j++){
-                if(img1[i][j]==1){
-                   s1.push([i,j]);
-                }
-                if(img2[i][j]==1){
-                    s2.push([i,j]);
-                }
+    const rows = img1.length;
+    const cols = img1[0].length;
+    let vals1=[];
+    let vals2=[]
+    for(let i=0;i<rows;i++) {
+        for(let j=0;j<cols;j++) {
+            if(img1[i][j] === 1) {
+                vals1.push([i,j])
+            }
+            if(img2[i][j]===1) {
+                vals2.push([i,j])
             }
         }
-        let ans=0;
-        let map = {};
-
-       for(const v1 of s1) {
-           for(const v2 of s2) {
-               let shift = ([v2[0] - v1[0] , v2[1]-v1[1]]);
-               map[shift] ? map[shift]++ : map[shift] = 1;
-               ans = Math.max(ans,map[shift])
-           }
-       }
-        return ans;
-    
+    }
+    let map ={};
+    let max=0;
+    for(const [i1,j1] of vals1) {
+        for(const [i2,j2] of vals2) {
+            let diff = [i2-i1,j2-j1];
+            map[diff] ? map[diff]++ : map[diff] =1;
+            max = Math.max(max, map[diff]);
+        }
+    }
+    return max;
 };
- 

8b3b6bda8c78c89138e7622470893e7f002e74cc
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-21 13:19:46 -0500

    Time: 3129 ms (6.82%), Space: 52.6 MB (13.64%) - LeetHub

--- 0835-image-overlap/0835-image-overlap.js
+++ 0835-image-overlap/0835-image-overlap.js
@@ -7,23 +7,23 @@ var largestOverlap = function(img1, img2) {
     const rows = img1.length;
     const cols = img1[0].length;
     let vals1=[];
-    let vals2=[]
+    let vals2=[];
     for(let i=0;i<rows;i++) {
         for(let j=0;j<cols;j++) {
             if(img1[i][j] === 1) {
                 vals1.push([i,j])
             }
-            if(img2[i][j]===1) {
-                vals2.push([i,j])
+            if(img2[i][j] === 1) {
+                vals2.push([i,j]);
             }
         }
     }
     let map ={};
-    let max=0;
-    for(const [i1,j1] of vals1) {
-        for(const [i2,j2] of vals2) {
-            let diff = [i2-i1,j2-j1];
-            map[diff] ? map[diff]++ : map[diff] =1;
+    let max=0
+    for(const v1 of vals1) {
+        for(const v2 of vals2) {
+            let diff = [v2[0]-v1[0], v2[1]-v1[1]]
+            map[diff] ? map[diff]++ : map[diff]=1;
             max = Math.max(max, map[diff]);
         }
     }

5d1aadde2aaa2f292f0acf9298f361079c126558
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-21 11:14:11 -0500

    Time: 80 ms (53.77%), Space: 44.7 MB (8.49%) - LeetHub

--- 0967-numbers-with-same-consecutive-differences/0967-numbers-with-same-consecutive-differences.js
+++ 0967-numbers-with-same-consecutive-differences/0967-numbers-with-same-consecutive-differences.js
@@ -4,17 +4,17 @@
  * @return {number[]}
  */
 var numsSameConsecDiff = function(n, k) {
-let dp = new Set([1,2,3,4,5,6,7,8,9]);
+    let dp = new Set([1,2,3,4,5,6,7,8,9]);
     for(let i=2;i<=n;i++) {
         let newDp = new Set();
         for(let prevVal of dp) {
-            let lastDigit = prevVal % 10;
-            let plusK = lastDigit +k;
-            let minusK = lastDigit -k;
-            if(plusK<10) newDp.add(prevVal *10 +plusK);
-            if(minusK >=0) newDp.add(prevVal *10 +minusK );
+            let lastDigit = prevVal%10;
+            let plusK = lastDigit+k;
+            let minusK = lastDigit-k;
+            if(plusK<10)newDp.add((prevVal*10)+plusK);
+            if(minusK>=0)newDp.add((prevVal*10)+minusK)
         }
-        dp = newDp
+        dp = newDp;
     }
     return [...dp]
 };

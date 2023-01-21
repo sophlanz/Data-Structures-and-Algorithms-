29887237163fcb07ca03400199f44ea5d19b44a7
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-21 08:47:52 -0500

    Time: 64 ms (88.44%), Space: 42.8 MB (76.63%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+ let l=0,
+     r=nums.length-1;
+    while(l<=r) {
+        const sum = nums[l] + nums[r];
+        if(sum === target) {
+            return [l+1,r+1];
+        }else if (sum> target) {
+            r--
+        }else {
+            l++
+        }
+    }
+};

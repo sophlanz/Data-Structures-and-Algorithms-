983c1714cfbb7c0191ce7daa3678448eab6ac1c8
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-21 18:12:31 -0500

    Time: 89 ms (42.73%), Space: 43.8 MB (49.96%) - LeetHub

--- 0139-word-break/0139-word-break.js
+++ 0139-word-break/0139-word-break.js
@@ -5,16 +5,16 @@
  */
 var wordBreak = function(s, wordDict) {
     let arr=[];
-    arr[0] = true;
-   
-   for(let i=0;i<=s.length;i++) {
-       if(arr[i] === true)
-       for(const word of wordDict) {
-           if(s.slice(i, i+ word.length) === word) {
-               arr[i+word.length]=true;
-           }
-       }
-   }
-    console.log(arr)
-    return arr[s.length] ? true: false
+   arr[0] = true;
+    for(let i=0;i<=s.length;i++) {
+        if(arr[i]===true) {
+            for(const word of wordDict) {
+                if(s.slice(i,i+word.length ) === word) {
+                    arr[i+word.length] = true;
+                }
+            }
+        
+        }
+    }
+    return arr[s.length] ? true : false;
 };

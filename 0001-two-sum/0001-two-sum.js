066faaa66f3e8f91ce486cff9ecc717eb4b515d9
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-21 08:41:39 -0500

    Time: 78 ms (79.36%), Space: 42.4 MB (61.19%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+ let idxMap={};
+    for(let i=0;i<nums.length;i++) {
+        if((target - nums[i]) in idxMap) {
+            return [idxMap[target-nums[i]], i]
+        }else {
+            idxMap[nums[i]] = i;
+        }
+    }
+    return [];
+};

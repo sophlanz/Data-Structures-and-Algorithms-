855cf9aa000f987e32fe0228db705da4064ed84a
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-22 17:21:13 -0500

    Time: 112 ms (49.26%), Space: 49 MB (55.79%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.js
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.js
@@ -3,25 +3,25 @@
  * @return {string}
  */
 var minRemoveToMakeValid = function(s) {
-    s=s.split('');
-    let balance=0,
-        idxStack=[];
-    for(let i=0;i<s.length;i++){
-        if(s[i] === "(") {
-            idxStack.push(i);
+  let res=  s.split('');
+    let balance=0;
+    let stack = [];
+    for(let i=0;i<s.length;i++) {
+        if(s[i] === '(') {
+            stack.push(i);
             balance++
-        }else if(s[i] === ")") {
-            if(balance >=1) {
-                idxStack.pop();
+        }else if(s[i]===")") {
+            if(balance>=1){
+                stack.pop();
                 balance--
             }else {
-                idxStack.push(i);
+                stack.push(i);
             }
+            
         }
     }
-    
-    for(const idx of idxStack) {
-        s[idx] = "";
+    for(const idx of stack) {
+        res[idx] = "";
     }
-        return s.join('');
+    return res.join('')
 };

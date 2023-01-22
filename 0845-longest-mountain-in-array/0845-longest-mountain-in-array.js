8232a2ef2cea7b50d2befb171c34432b1c11abaa
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-22 14:23:18 -0500

    Time: 119 ms (35.62%), Space: 44.5 MB (34.25%) - LeetHub

--- 0845-longest-mountain-in-array/0845-longest-mountain-in-array.js
+++ 0845-longest-mountain-in-array/0845-longest-mountain-in-array.js
@@ -3,35 +3,31 @@
  * @return {number}
  */
 var longestMountain = function(arr) {
-    const n = arr.length;
-    
-    if (n < 3) return 0;
-    
-    let maxLen = 0;
-    let start = 0;
-    
-    while (start < n - 2) {
-       let end = start;
+  let l=0,
+      len = arr.length,
+      
+      maxLen=0;
+    let r;
+     let peak;
+    while(l<=len-3) {
+        r = l;
+      
+             while(arr[r]<arr[r+1]) {
+                r++
+            }
+            peak=r
+      
+      
+             while(arr[r] > arr[r+1]) {
+                 r++
+             }
+        if(arr[l] < arr[peak] && arr[r] < arr[peak])  maxLen = Math.max(maxLen, r-l+1);
+       
+         l = Math.max(r, l+1)  
+         
+            
         
-        // check for ascending
-        while (end < n - 1 && arr[end] < arr[end + 1]) {
-            end++;
-        }
-        
-        const peak = arr[end]; // mark the apex 
-        
-        // check for descending
-        while (end < n - 1 && arr[end] > arr[end + 1]) {
-            end++;
-        }
-        
-        if (arr[start] < peak && peak > arr[end]) {
-            maxLen = Math.max(maxLen, end - start + 1);
-        }
-        
-        start = Math.max(end, start + 1); // if we made no advancement with end, then we would want to at least move one space
     }
-   
     return maxLen;
     
 };

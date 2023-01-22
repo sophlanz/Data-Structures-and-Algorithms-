ee5f5ac92a532ce6d9497e4fc1eb9541566c7029
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-22 17:04:55 -0500

    Time: 80 ms (84.20%), Space: 48 MB (85.33%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.js
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.js
@@ -14,11 +14,14 @@
  */
 var connect = function(root) {
     if(!root || !root.left) return root;
-        root.left.next = root.right;
-    if(root.next) {
-        root.right.next = root.next.left;
-    }
-    connect(root.left);
-    connect(root.right);
+
+      if(root.right)  root.left.next = root.right;
+    
+    if(root.next)  root.right.next = root.next.left;
+    
+     connect(root.left);
+     connect(root.right)
+   
+   
     return root;
 };

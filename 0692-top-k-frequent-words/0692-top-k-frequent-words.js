bdf309fff0ebc31c85df36cca9ac3a604778f22a
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-22 09:05:44 -0500

    Time: 99 ms (50.84%), Space: 45 MB (71.07%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.js
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.js
@@ -6,12 +6,10 @@
 var topKFrequent = function(words, k) {
     words.sort();
     let freqMap={};
-    for(const word of words) {
-        if(!freqMap[word]) freqMap[word] =1;
-        else {
-            freqMap[word]++
-        }
-    }
-    let arr = Object.keys(freqMap).sort((a,b)=> freqMap[b]-freqMap[a]);
-    return arr.splice(0,k);
-};
+    for(let i=0;i<words.length;i++) {
+        const word = words[i];
+        freqMap[word] ? freqMap[word]++ : freqMap[word] =1;
+    };
+    const values = Object.keys(freqMap).sort((a,b)=> freqMap[b]-freqMap[a]);
+    return values.splice(0,k)
+}

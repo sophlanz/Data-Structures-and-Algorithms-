a809db11ea6217f6dca46940d5d78f2664cb8b68
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-22 16:59:41 -0500

    Time: 135 ms (27.66%), Space: 48.1 MB (20.21%) - LeetHub

--- 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.ts
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.ts
@@ -15,29 +15,28 @@
  */
 
 function connect(root: Node | null): Node | null {
- const  findNext = (root) => { // get parent node
-        while (root.next != null) { // scan all next parent nodes until we find the first left or right child
-            root = root.next;
-            if (root.left != null) return root.left;
-            if (root.right != null) return root.right;
+    if(!root || root===null) return null;
+    const getNextNode = (node) => {
+        while(node.next !=null) {
+            node = node.next;
+            if(node.left != null ) return node.left;
+            if(node.right != null) return node.right;
         }
         return null;
     }
-   if (root == null) return null;
-        
-        if (root.left != null) { // update left next
-            if (root.right != null) root.left.next = root.right; // if right child exists - simple connect left.next to right
-            else root.left.next = findNext(root); // if not - scan parent next node until we find the first left or right child
+  if(root.left != null) {
+      if(root.right != null) {
+          root.left.next = root.right
+      }else {
+          root.left.next = getNextNode(root);
+      }
+  }
+    if(root.right !=null) {
+        if(root.next) {
+            root.right.next = getNextNode(root);
         }
-        if (root.right != null) { // update right next
-            root.right.next = findNext(root);
-        }
-        
-        connect(root.right); // update the right nodes first
-        connect(root.left);
-    
-   
-        return root;
+    }
+    connect(root.right);
+    connect(root.left);
+    return root
 };
-  
-

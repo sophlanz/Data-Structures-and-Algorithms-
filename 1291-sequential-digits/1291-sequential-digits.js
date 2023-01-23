0adfb8cca1dfeea7d24d6c3815d5f5235108a3d1
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-23 08:56:18 -0500

    Time: 51 ms (100.00%), Space: 41.9 MB (75.68%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.js
+++ 1291-sequential-digits/1291-sequential-digits.js
@@ -4,16 +4,14 @@
  * @return {number[]}
  */
 var sequentialDigits = function(low, high) {
-    let digits = '123456789'
-    const maxWindow = high.toString().length;
-    const minWindow = low.toString().length;
+    let digits ="123456789";
+    let minWindow = low.toString().length;
+    let maxWindow = high.toString().length;
     let res=[]
-    for(let window = minWindow;window<=maxWindow;window++) {
-        for(let i=0;i+window<=digits.length;i++) {
-            let num = parseInt(digits.substring(i,i+window));
-            if(num <= high && num >= low) {
-                res.push(num);
-            }
+    for(let window=minWindow;window<=maxWindow;window++) {
+        for(let i=0;window+i<=digits.length;i++) {
+            let num = parseInt(digits.slice(i,window+i));
+            if(num <= high && num>= low)res.push(num);
         }
     }
     return res;

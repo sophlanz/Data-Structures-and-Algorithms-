f7c7dfce6f04cb9b644a4decb344697fbfee1f38
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-23 08:45:24 -0500

    Time: 231 ms (36.11%), Space: 62.9 MB (8.33%) - LeetHub

--- 1506-find-root-of-n-ary-tree/1506-find-root-of-n-ary-tree.js
+++ 1506-find-root-of-n-ary-tree/1506-find-root-of-n-ary-tree.js
@@ -11,22 +11,13 @@
  * @return {Node}
  */
 var findRoot = function(tree) {
-     const set = new Set()
-    
-    for (const node of tree) {
-       
-        for (const child of node.children) {
-            set.add(child.val)
-         
+    let set = new Set([]);
+    for(const node of tree) {
+        for(const child of node.children) {
+            set.add(child.val);
         }
     }
-  
-    let root = null
-    
-    for (const node of tree) {
-        if (!set.has(node.val)) root = node
-        
+    for(const node of tree) {
+        if(!set.has(node.val)) return node;
     }
-    
-    return root
 };

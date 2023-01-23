2b7d4f140a173b75e5a8ecad10edb57922f091ad
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-23 07:54:03 -0500

    Time: 260 ms (58.24%), Space: 66.5 MB (63.19%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.js
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.js
@@ -3,15 +3,15 @@
  * @return {number[]}
  */
 var arrayRankTransform = function(arr) {
- let clone = [...arr]
- let rankMap={};
-    let rank = 1;
-    clone.sort((a,b)=> a-b).map((a,b)=> {
-        if(!rankMap[a]){
+    let clone = [...arr];
+    let rankMap={};
+    let rank=1
+    clone.sort((a,b)=> a-b).map((a)=> {
+        if(!rankMap[a]) {
             rankMap[a] = rank;
             rank++
-        } 
-    })
-    return arr.map((a)=> rankMap[a]);
-};
+        }
+    });
+   return arr.map((a)=> rankMap[a]);
     
+};

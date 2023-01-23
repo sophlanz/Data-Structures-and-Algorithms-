454d5aeb1648229cdc8239c1166bb66e943f592c
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-23 10:59:45 -0500

    Time: 76 ms (72.00%), Space: 43.6 MB (98.67%) - LeetHub

--- 0845-longest-mountain-in-array/0845-longest-mountain-in-array.js
+++ 0845-longest-mountain-in-array/0845-longest-mountain-in-array.js
@@ -3,32 +3,21 @@
  * @return {number}
  */
 var longestMountain = function(arr) {
-  let l=0,
-      len = arr.length,
-      
-      maxLen=0;
+    let l=0;
     let r;
-     let peak;
-    while(l<=len-3) {
-        r = l;
-      
-             while(arr[r]<arr[r+1]) {
-                r++
-                  peak=r
-            }
-           
-      
-      
-             while(arr[r] > arr[r+1]) {
-                 r++
-             }
-        if(arr[l] < arr[peak] && arr[r] < arr[peak])  maxLen = Math.max(maxLen, r-l+1);
-       
-         l = Math.max(r, l+1)  
-         
-            
-        
+    let p;
+    let maxLen=0;
+    while(l<=arr.length-3) {
+        p=l;
+        while(arr[p] < arr[p+1]) {
+            p++
+        }
+       r=p
+        while(arr[r] > arr[r+1]) {
+            r++
+        }
+        if(arr[l] < arr[p] && arr[p] > arr[r]) maxLen = Math.max(maxLen, r-l+1)
+        l=Math.max(r, l+1);
     }
     return maxLen;
-    
 };

148ed9a9b920637651cd5ce35a41688bb00547bb
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-23 09:27:58 -0500

    Time: 143 ms (52.76%), Space: 52.1 MB (32.28%) - LeetHub

--- 0888-fair-candy-swap/0888-fair-candy-swap.js
+++ 0888-fair-candy-swap/0888-fair-candy-swap.js
@@ -6,13 +6,16 @@
 var fairCandySwap = function(A, B) {
       const sumA = A.reduce((acc, cur) => acc + cur);
   const sumB = B.reduce((acc, cur) => acc + cur);
-  const diff = (sumA - sumB) >> 1;
+ 
+ 
+    const half = ((sumA+sumB)/2);
+    let diff= sumA-half
   const setA = new Set(A);
   for (const candy of B) {
     if (setA.has(candy + diff)) return [candy + diff, candy];
   }
     
-    
+
     
     
 };

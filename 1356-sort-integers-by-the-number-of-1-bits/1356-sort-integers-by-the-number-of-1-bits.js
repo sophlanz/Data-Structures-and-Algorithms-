b5f426b9ca17b153fca9b52af3e949768c52278c
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-24 08:49:04 -0500

    Time: 186 ms (23.14%), Space: 48.5 MB (13.16%) - LeetHub

---
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} arr
+ * @return {number[]}
+ */
+var sortByBits = function(arr) {
+ const numOfBits = (num) => {
+     let bits = num.toString(2).match(/1/g);
+     return bits ? bits.length : 0;
+ }
+ return arr.sort((a,b)=> numOfBits(a) - numOfBits(b) || a-b)
+};

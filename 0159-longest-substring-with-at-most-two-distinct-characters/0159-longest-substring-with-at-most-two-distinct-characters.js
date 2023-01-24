bce6b07e96248d9a052b97feeeb8b89bfb9767ab
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-24 08:57:46 -0500

    Time: 432 ms (41.26%), Space: 49.2 MB (44.06%) - LeetHub

--- 0159-longest-substring-with-at-most-two-distinct-characters/0159-longest-substring-with-at-most-two-distinct-characters.js
+++ 0159-longest-substring-with-at-most-two-distinct-characters/0159-longest-substring-with-at-most-two-distinct-characters.js
@@ -3,21 +3,19 @@
  * @return {number}
  */
 var lengthOfLongestSubstringTwoDistinct = function(s) {
-   let start=0,
-       freqMap={},
-       maxLen=0;
+    let start = 0,
+        freqMap={},
+        maxLen=0;
     for(let end=0;end<s.length;end++) {
-        const rChar = s[end];
-        freqMap[rChar] ? freqMap[rChar]++ : freqMap[rChar]=1;
-        while(Object.keys(freqMap).length > 2) {
-            let lChar = s[start];
-            freqMap[lChar]--;
-            if(freqMap[lChar] == 0) {
-                delete freqMap[lChar]
-            }
+        const char = s[end];
+        freqMap[char] ? freqMap[char]++ : freqMap[char] =1;
+        while(Object.keys(freqMap).length >2) {
+            let lchar = s[start];
+            freqMap[lchar]--;
+            if(freqMap[lchar] === 0) delete freqMap[lchar];
             start++
         }
-        maxLen = Math.max(maxLen, end-start+1);
+        maxLen= Math.max(maxLen,end-start+1 )
     }
-    return maxLen;
+    return maxLen
 };

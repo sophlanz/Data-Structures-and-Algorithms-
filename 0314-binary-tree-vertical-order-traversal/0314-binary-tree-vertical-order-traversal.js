332d7f7508d1338da9e70d8b911f1f51d0c73c08
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-25 16:38:35 -0500

    Time: 64 ms (92.14%), Space: 43.9 MB (70.00%) - LeetHub

--- 0314-binary-tree-vertical-order-traversal/0314-binary-tree-vertical-order-traversal.js
+++ 0314-binary-tree-vertical-order-traversal/0314-binary-tree-vertical-order-traversal.js
@@ -12,13 +12,13 @@
  */
 var verticalOrder = function(root) {
     if(!root) return [];
-    let queue = [[0,root]];
-    let idxMap = {}  
+    let queue = [[0,root]],
+    idxMap={};
     while(queue.length) {
-        const [idx, node] = queue.shift();
-        idxMap[idx] ? idxMap[idx].push(node.val) : idxMap[idx] = [node.val];
-        node.left ? queue.push([idx-1, node.left]) : null;
-        node.right ? queue.push([idx+1, node.right]):null;
-    };
-  return  Object.keys(idxMap).sort((a,b)=> a-b).map((a)=> idxMap[a]);
+        let [i,node] = queue.shift();
+        idxMap[i] ? idxMap[i].push(node.val) : idxMap[i] = [node.val];
+        node.left ? queue.push([i-1,node.left]) : null;
+        node.right ? queue.push([i+1,node.right]) : null;
+    }
+    return Object.keys(idxMap).sort((a,b)=> a-b).map((a)=> idxMap[a])
 };

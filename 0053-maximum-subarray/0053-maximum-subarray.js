830fa6fffb72f8c1091bc4e745502055aed491c9
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-27 15:09:34 -0500

    Time: 134 ms (23.27%), Space: 50.1 MB (75.62%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    let maxSum = nums[0];
+    let currSum= nums[0];
+    for(let i=1;i<nums.length;i++) {
+        currSum = Math.max(currSum + nums[i], nums[i] );
+        maxSum = Math.max(maxSum, currSum);
+    }
+    return maxSum
+};
+ 

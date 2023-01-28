3a5241aba6931ba1c19691401912a8e029ce8a44
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-28 12:40:02 -0500

    Time: 67 ms (68.16%), Space: 41.8 MB (78.39%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -4,25 +4,24 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    let l=0,
-    r=nums.length-1;
+  let l=0,
+      r=nums.length-1;
     while(l<=r) {
         let mid = Math.floor((l+r)/2);
         if(nums[mid]===target) return mid;
-        if(nums[l]<=nums[mid]) {
-            if(target<nums[l] || target> nums[mid] ) {
-                l=mid+1
+        if(nums[l] <=nums[mid]) {
+            if(target<nums[l] || target>nums[mid]) {
+                l=mid+1;
             }else {
                 r=mid-1
             }
         }else {
-            if(target >nums[r] || target<nums[mid]) {
-                r = mid-1
+            if(target<nums[mid] || target > nums[r]) {
+                r=mid-1
             }else {
                 l=mid+1
             }
         }
     }
     return -1;
-    
 };

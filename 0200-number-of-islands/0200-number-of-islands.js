943240d87afa2763c1eceff535ff825dd60f88c2
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-30 10:32:07 -0500

    Time: 90 ms (69.90%), Space: 44.6 MB (84.52%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.js
+++ 0200-number-of-islands/0200-number-of-islands.js
@@ -3,17 +3,17 @@
  * @return {number}
  */
 var numIslands = function(grid) {
-    const rows = grid.length;
+        const rows = grid.length;
     const cols = grid[0].length;
-    const DFS = (i,j) => {
-        if(i<0 || j<0 || i>= rows || j>= cols|| grid[i][j] === "0") return;
-        grid[i][j] = "0";
-        DFS(i+1,j);
-        DFS(i-1,j);
-        DFS(i,j+1);
-        DFS(i,j-1);
-    }
     let count=0
+    const DFS = (i,j) => {
+        if(i<0 || j<0 || i>=rows || j>=cols || grid[i][j] === "0") return;
+        grid[i][j] = "0";
+        DFS(i+1,j)
+        DFS(i-1,j)
+        DFS(i,j+1)
+        DFS(i,j-1)
+    }
     for(let i=0;i<rows;i++) {
         for(let j=0;j<cols;j++) {
             if(grid[i][j] === "1") {

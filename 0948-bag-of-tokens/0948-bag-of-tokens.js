837591ebe1dae04d2d7f10ea897e3b53be1f4b02
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-01-29 22:30:11 -0500

    Time: 71 ms (78.18%), Space: 43.4 MB (69.09%) - LeetHub

--- 0948-bag-of-tokens/0948-bag-of-tokens.js
+++ 0948-bag-of-tokens/0948-bag-of-tokens.js
@@ -5,23 +5,23 @@
  */
 var bagOfTokensScore = function(tokens, power) {
     tokens.sort((a,b)=> a-b)
-  let l=0,
-      r=tokens.length-1,
-      maxScore=0,
-      score=0;
+    let l=0,
+        r=tokens.length-1,
+        score=0,
+        maxScore=0;
     while(l<=r) {
         if(power >= tokens[l]) {
-            power -= tokens[l];
             score++
+            power -= tokens[l]
             l++
-        }else if(score >= 1) {
+        }else if ( score >=1) {
+            score--;
             power += tokens[r];
-            score--
             r--
         }else {
             break;
         }
-        maxScore = Math.max(maxScore, score);
+        maxScore = Math.max(score, maxScore);
     }
-    return maxScore
-};
+    return maxScore;
+}

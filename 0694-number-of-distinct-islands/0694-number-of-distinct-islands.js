505b35aee6909214481de15d99c9cf679c2450a0
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-02-01 11:41:48 -0500

    Time: 92 ms (78.79%), Space: 48.5 MB (51.51%) - LeetHub

--- 0694-number-of-distinct-islands/0694-number-of-distinct-islands.js
+++ 0694-number-of-distinct-islands/0694-number-of-distinct-islands.js
@@ -6,11 +6,10 @@ var numDistinctIslands = function(grid) {
     const rows = grid.length, cols = grid[0].length;
     const seen = new Set();    
     let counter = 0;
-    
     for(let i = 0; i < rows; i++) {
         for(let j = 0; j < cols; j++) {
             if(grid[i][j]===1) {
-                const pathSign = callDFS(i, j);
+                const pathSign = DFS(i, j);
                 if(!seen.has(pathSign)) {
                     seen.add(pathSign)
                     counter++;
@@ -20,17 +19,19 @@ var numDistinctIslands = function(grid) {
     }
     return counter;
     
-    function callDFS(r, c) {
-        if(r < 0 || c < 0 || r >= rows || c >= cols || !grid[r][c]) return '';
-        grid[r][c] = 0;
+    function DFS(i, j) {
+        if(i < 0 || j < 0 || i >= rows || j >= cols || grid[i][j]===0) return '';
+        grid[i][j] = 0;
         
         const dir = [[0, 1, 'R'], [0, -1, 'L'], [1, 0, 'D'], [-1, 0, 'U']];
         let str = ''
         
-        for(let [dr, dc, sign] of dir) {
+        for(let [r, c, sign] of dir) {
             str += sign
-            str += callDFS(r+dr, c+dc);
+         
+            str += DFS(i+r, j+c);
         }
+  
         return str;
     }
 };

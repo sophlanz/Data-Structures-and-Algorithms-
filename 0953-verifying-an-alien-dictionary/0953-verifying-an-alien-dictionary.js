533649b7dcca895925165f96bb43ae9701df9fef
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-02-02 15:35:05 -0500

    Time: 76 ms (44.21%), Space: 43.7 MB (47.89%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.js
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.js
@@ -4,29 +4,18 @@
  * @return {boolean}
  */
 var isAlienSorted = function(words, alphabet) {
-  const map = {};
-
-    for (let i = 0; i < alphabet.length; i++) {
-        map[alphabet[i]] = i + 1;
+  
+let map = {};
+    for(let i=0;i<alphabet.length;i++){
+        map[alphabet[i]] = i+1;
     }
-
-    for (let i = 0; i < words.length - 1; i++) {
-        for (let j = 0; j < words[i].length; j++) {
-            if(j >= words[i + 1].length) {
-                return false;
-            }
-
-            if(words[i][j] === words[i + 1][j]) {
-                continue;
-            }
-
-            if(map[words[i][j]] > map[words[i + 1][j]]) {
-                return false;
-            }
-
-            break;
+    for(let i=0;i<words.length-1;i++) {
+        for(let j=0;j<words[i].length;j++) {
+            if(j>= words[i+1].length) return false;
+            if(words[i][j] === words[i+1][j])continue;
+            if(map[words[i][j]] > map[words[i+1][j]]) return false;
+           break;
         }
     }
-
     return true;
 };

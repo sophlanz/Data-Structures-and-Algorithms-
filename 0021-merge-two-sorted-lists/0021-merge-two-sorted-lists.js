ba961b82f5ea8ee0aac104d1996ff4d6a9426049
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-02-06 10:52:54 -0500

    Time: 66 ms (90.81%), Space: 44.3 MB (47.01%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -11,15 +11,12 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(list1, list2) {
-if(list1=== null || list2===null) return (list1? list1:list2);
-    console.log({list1})
-    console.log({list2})
-    if(list1.val<=list2.val) {
-        list1.next = mergeTwoLists(list1.next,list2);
-        return list1
+    if(!list1 || !list2) return (list1? list1 : list2);
+    if(list1.val <= list2.val) {
+        list1.next = mergeTwoLists(list1.next,list2)
+        return list1;
     }else {
-        list2.next=mergeTwoLists(list1,list2.next);
-        return list2
+        list2.next = mergeTwoLists(list1,list2.next);
+        return list2;
     }
-    
 };

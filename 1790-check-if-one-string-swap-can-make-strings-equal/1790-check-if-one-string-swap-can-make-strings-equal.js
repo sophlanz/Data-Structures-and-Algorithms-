d5f201006a907a5bb1ed7cf2b027d668b869e2f8
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-02-06 14:52:05 -0500

    Time: 62 ms (79.02%), Space: 42.3 MB (41.52%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.js
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.js
@@ -4,12 +4,11 @@
  * @return {boolean}
  */
 var areAlmostEqual = function(s1, s2) {
-let diff = [];
+    let diff = [];
     for(let i=0;i<s1.length;i++) {
-        if(s1[i] !== s2[i]) diff.push(i);
+        if(s1[i] !== s2[i]) diff.push(i)
     }
-    if (diff.length >2)return false;
-    const [a,b] =diff
-    return s1[a] === s2[b] && s1[b] === s2[a]
- 
+    if(diff.length >2) return false
+    const [a,b] = diff;
+    return s1[a] === s2[b] && s1[b] === s2[a];
 };

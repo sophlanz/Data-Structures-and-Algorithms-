ecef301e0c1c3c0988526c5cf4e7750e248b3eb2
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-02-06 16:46:14 -0500

    Time: 60 ms (89.54%), Space: 41.9 MB (77.04%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.js
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.js
@@ -3,18 +3,12 @@
  * @return {number}
  */
 var maxDepth = function(s) {
-    let count=0,
-        maxDepth = 0;
-    
-   for(let i=0;i<s.length;i++) {
-       const char = s[i];
-       if(char === '(') {
-           count++
-        maxDepth = Math.max(maxDepth,count);
-       }else if (char === ")") {
-           count--;
-       }
-    
-   }
-    return maxDepth;
+   let count=0;
+    let max=0;
+    for(let i=0;i<s.length;i++) {
+        if(s[i] === "(")count++
+        else if(s[i]===")")count--
+        max = Math.max(max,count);
+    }
+    return max;
 };

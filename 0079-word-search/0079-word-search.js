6693ce23d69fdec8db44927f573d7385947791f2
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-02-06 16:42:21 -0500

    Time: 600 ms (80.06%), Space: 42.5 MB (73.03%) - LeetHub

--- 0079-word-search/0079-word-search.js
+++ 0079-word-search/0079-word-search.js
@@ -4,15 +4,15 @@
  * @return {boolean}
  */
 var exist = function(board, word) {
-   const rows = board.length;
+const rows = board.length;
     const cols = board[0].length;
-    const DFS = (i,j,idx)=> {
-        if(word.length=== idx)  return true
-        if(i< 0 || j<0 || i>= rows || j>= cols || board[i][j] !== word[idx]) return false;
+    const DFS = (i,j,idx) => {
+        if(idx === word.length) return true;
+        if(i<0 || j<0 || i>= rows || j>= cols || board[i][j] !== word[idx]) return false;
         board[i][j] = "#"
         if(DFS(i+1,j,idx+1)||
            DFS(i-1,j,idx+1)||
-           DFS(i,j+1,idx+1)||
+           DFS(i,j+1,idx+1) ||
            DFS(i,j-1,idx+1)
           ) return true
         else {
@@ -24,6 +24,5 @@ var exist = function(board, word) {
             if(board[i][j] === word[0] && DFS(i,j,0)) return true
         }
     }
-    return false
-  
+    return false;
 };

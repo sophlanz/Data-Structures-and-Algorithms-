ce8ea1cc71b3c4ea7ac2c73c309905c2249ab15f
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-02-07 14:47:46 -0500

    Time: 122 ms (94.87%), Space: 51.3 MB (100.00%) - LeetHub

--- 1229-meeting-scheduler/1229-meeting-scheduler.js
+++ 1229-meeting-scheduler/1229-meeting-scheduler.js
@@ -9,10 +9,10 @@ var minAvailableDuration = function(slots1, slots2, duration) {
     slots2.sort((a,b)=>a[0]-b[0]);
     let p1=0,p2=0;
     while(p1<slots1.length && p2<slots2.length) {
-        let start = Math.max(slots1[p1][0], slots2[p2][0]);
+        let start= Math.max(slots1[p1][0], slots2[p2][0]);
         let end = Math.min(slots1[p1][1], slots2[p2][1]);
-        if(end-start >= duration) return [start, duration+start]
-        if(slots1[p1][1]<slots2[p2][1]) p1++
+        if(end-start >= duration) return [start, start+duration];
+        if(slots1[p1][1]<slots2[p2][1])p1++
         else p2++
     }
     return [];

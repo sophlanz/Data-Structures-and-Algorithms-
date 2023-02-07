3e2cb2e074ce6aa2e5e4e4e5a9fdb04d46a1361e
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-02-07 09:14:03 -0500

    Time: 68 ms (87.70%), Space: 42.3 MB (81.84%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.js
+++ 0263-ugly-number/0263-ugly-number.js
@@ -3,16 +3,16 @@
  * @return {boolean}
  */
 var isUgly = function(n) {
-    if(n === 0) return false;
-    if(n === 1) return true;
-    if(n%2 === 0) {
+    if(n===1) return true
+    if(n===0) return false;
+    if(n%2===0) {
         return isUgly(n/2)
     }
-    if(n%3 === 0) {
-       return isUgly (n/3)
+    if(n%3===0) {
+   return isUgly(n/3)
     }
-    if(n%5 === 0) {
-       return isUgly(n/5)
+    if(n%5==0) {
+        return isUgly(n/5)
     }
     return false
 };

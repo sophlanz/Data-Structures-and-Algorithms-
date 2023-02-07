240aca2bb8d9ad0302a1743ffb6a26df35d9d6e3
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-02-07 15:58:22 -0500

    Time: 96 ms (76.15%), Space: 49.9 MB (48.46%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.js
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.js
@@ -3,15 +3,15 @@
  * @return {number[]}
  */
 var lexicalOrder = function(n) {
-let res=[];
+    let res=[];
     const DFS = (num) => {
-        if(num*10>n) return;
-        for(let i=num*10;i<=n && i<num*10+10;i++) {
+        if(num*10>n) return 
+        for(let i=num*10;i<=n && i< num*10+10;i++) {
             res.push(i);
             DFS(i);
         }
     }
-    for(let i=1;i<=9 && i<=n;i++){
+    for (let i=1;i<=n && i<=9;i++) {
         res.push(i);
         DFS(i)
     }

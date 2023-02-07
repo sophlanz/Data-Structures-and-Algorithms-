58e60ef6664285f0ac428f5f3001bd9b00d190ed
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-02-07 09:11:20 -0500

    Time: 60 ms (86.90%), Space: 41.5 MB (95.24%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.js
+++ 1328-break-a-palindrome/1328-break-a-palindrome.js
@@ -4,16 +4,13 @@
  */
 var breakPalindrome = function(palindrome) {
 let res = palindrome.split('');
+    if(res.length ===1) return '';
     for(let i=0;i<Math.floor(res.length/2);i++) {
-        if(res[i] !=="a") {
-            res[i] = "a";
+        if(res[i]!=="a") {
+            res[i]="a";
             return res.join('');
         }
     }
-    if(res.length ===1) {
-        return ''
-    }else {
-         res[res.length-1] = 'b';
-        return res.join('')
-    }
+    res[res.length-1]="b";
+    return res.join('');
 };

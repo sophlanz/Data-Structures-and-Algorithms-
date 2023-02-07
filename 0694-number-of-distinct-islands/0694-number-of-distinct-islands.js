20a472907b989d98dc4caf46a716f62398f7ca01
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-02-07 16:47:59 -0500

    Time: 101 ms (63.64%), Space: 48.3 MB (64.94%) - LeetHub

--- 0694-number-of-distinct-islands/0694-number-of-distinct-islands.js
+++ 0694-number-of-distinct-islands/0694-number-of-distinct-islands.js
@@ -5,25 +5,26 @@
 var numDistinctIslands = function(grid) {
     const rows = grid.length;
     const cols = grid[0].length;
-    let seen = new Set();
+    const seen = new Set();
     const DFS = (i,j) => {
-        if(i<0 || j<0 || i>= rows || j>= cols || grid[i][j] === 0) return "";
+        if(i<0 || j<0 || i>= rows || j>= cols || grid[i][j] === 0) return;
         grid[i][j] = 0;
         let str=""
-        const key = [[1,0,"D"],[-1,0,"U"],[0,1,"R"],[0,-1,"L"]];
-        for(let [r,c,move] of key) {
-            str+= move;
-            str+=DFS(i+r,j+c)
+        let dict = [[1,0,"D"], [-1,0,"U"],[0,1,"R"],[0,-1,"L"]];
+        for(const [r,c,move] of dict) {
+           str += move;
+            str+= DFS(i+r, j+c);
         }
         return str;
+        
     }
     for(let i=0;i<rows;i++) {
         for(let j=0;j<cols;j++) {
-            if(grid[i][j] ===1) {
-                const path = DFS(i,j)
-                if(!seen.has(path)) seen.add(path);
+            if(grid[i][j]===1) {
+               let pattern= DFS(i,j)
+               if(!seen.has(pattern)) seen.add(pattern);
             }
         }
     }
-    return seen.size
+    return seen.size;
 };

eb5a3cc9abbf1cc94545844b0ef791caca3a3658
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-02-07 14:28:13 -0500

    Time: 133 ms (83.75%), Space: 51.8 MB (82.50%) - LeetHub

--- 1229-meeting-scheduler/1229-meeting-scheduler.js
+++ 1229-meeting-scheduler/1229-meeting-scheduler.js
@@ -5,17 +5,15 @@
  * @return {number[]}
  */
 var minAvailableDuration = function(slots1, slots2, duration) {
- slots1.sort((a,b)=> a[0]-b[0]);
-    slots2.sort((a,b)=> a[0]-b[0]);
-    let p1=0,
-        p2=0;
-while(p1<slots1.length && p2<slots2.length) {
-    let start = Math.max(slots1[p1][0], slots2[p2][0]);
-    let end = Math.min(slots1[p1][1], slots2[p2][1]);
-    if(end-start >= duration) return [start, start+duration];
-    if(slots1[p1][1]< slots2[p2][1]) p1++
-    else p2++
-}
+    slots1.sort((a,b)=> a[0]-b[0]);
+    slots2.sort((a,b)=>a[0]-b[0]);
+    let p1=0,p2=0;
+    while(p1<slots1.length && p2<slots2.length) {
+        let start = Math.max(slots1[p1][0], slots2[p2][0]);
+        let end = Math.min(slots1[p1][1], slots2[p2][1]);
+        if(end-start >= duration) return [start, duration+start]
+        if(slots1[p1][1]<slots2[p2][1]) p1++
+        else p2++
+    }
     return [];
 };
-   

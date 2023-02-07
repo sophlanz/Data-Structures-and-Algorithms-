e7930726bd453b2209b7b8d94a9695b22582f237
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-02-07 13:55:21 -0500

    Time: 163 ms (75.00%), Space: 49.6 MB (66.67%) - LeetHub

--- 1756-design-most-recently-used-queue/1756-design-most-recently-used-queue.js
+++ 1756-design-most-recently-used-queue/1756-design-most-recently-used-queue.js
@@ -2,21 +2,25 @@
  * @param {number} n
  */
 var MRUQueue = function(n) {
-   this.queue =[];
+    this.queue = [];
     while(n>0) {
         this.queue.unshift(n);
         n--
     }
 };
+
 /** 
  * @param {number} k
  * @return {number}
  */
 MRUQueue.prototype.fetch = function(k) {
-   const val = this.queue.splice(k-1, 1);
+    let val = this.queue[k-1];
+    this.queue.splice(k-1,1);
     this.queue.push(val)
-    return val;
+    return val
+    
 };
+
 /** 
  * Your MRUQueue object will be instantiated and called as such:
  * var obj = new MRUQueue(n)

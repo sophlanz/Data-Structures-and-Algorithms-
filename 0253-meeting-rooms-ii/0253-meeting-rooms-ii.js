0f3b147c86bb2bef67967509b70ff902a1e7a32f
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-02-07 12:38:05 -0500

    Time: 106 ms (34.40%), Space: 45.4 MB (31.99%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.js
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.js
@@ -3,17 +3,17 @@
  * @return {number}
  */
 var minMeetingRooms = function(intervals) {
-    intervals.sort((a,b)=> a[0]-b[0]);
+   intervals.sort((a,b)=> a[0]-b[0]);
     let minHeap = new MinPriorityQueue();
     let minRooms = 0
     for(let i=0;i<intervals.length;i++) {
         const interval = intervals[i]
-        if(minHeap.size()>0 && minHeap.front().element<=interval[0]) {
+        if(minHeap.size() !==0 && minHeap.front().element <=interval[0] ) {
             minHeap.dequeue().element;
         }
-        minHeap.enqueue(interval[1]).element;
-        console.log(minHeap)
-      minRooms=  Math.max(minRooms, minHeap.size());
+        minHeap.enqueue(interval[1]).element
+        minRooms = Math.max(minRooms,minHeap.size());
     }
     return minRooms
+    
 };

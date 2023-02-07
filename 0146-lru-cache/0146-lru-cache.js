ca41db0560c3de2b91ab18fa75b9abf02c81ecd4
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-02-07 16:52:50 -0500

    Time: 638 ms (59.87%), Space: 95.8 MB (63.78%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.js
+++ 0146-lru-cache/0146-lru-cache.js
@@ -11,11 +11,12 @@ var LRUCache = function(capacity) {
  * @return {number}
  */
 LRUCache.prototype.get = function(key) {
-    if(!this.cache.has(key)) return -1
+    if(!this.cache.has(key))return -1;
     let val = this.cache.get(key);
     this.cache.delete(key);
     this.cache.set(key,val);
-    return val
+    return val;
+    
 };
 
 /** 
@@ -28,7 +29,7 @@ LRUCache.prototype.put = function(key, value) {
     this.cache.set(key,value);
     if(this.cache.size > this.capacity) {
         const LRU = this.cache.keys().next().value;
-        this.cache.delete(LRU)
+        this.cache.delete(LRU);
     }
 };
 

b3b45422cf5301e330201c1678000622ecec4057
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-02-07 16:37:02 -0500

    Time: 451 ms (33.11%), Space: 49.1 MB (50.99%) - LeetHub

--- 0159-longest-substring-with-at-most-two-distinct-characters/0159-longest-substring-with-at-most-two-distinct-characters.js
+++ 0159-longest-substring-with-at-most-two-distinct-characters/0159-longest-substring-with-at-most-two-distinct-characters.js
@@ -3,19 +3,20 @@
  * @return {number}
  */
 var lengthOfLongestSubstringTwoDistinct = function(s) {
-    let start = 0,
-        freqMap={},
-        maxLen=0;
+  let freqMap = {};
+    let start=0
+    let maxLen=0;
     for(let end=0;end<s.length;end++) {
-        const char = s[end];
-        freqMap[char] ? freqMap[char]++ : freqMap[char] =1;
+        let char = s[end];
+        freqMap[char] ? freqMap[char]++ : freqMap[char]=1;
         while(Object.keys(freqMap).length >2) {
-            let lchar = s[start];
-            freqMap[lchar]--;
-            if(freqMap[lchar] === 0) delete freqMap[lchar];
+            let lChar = s[start];
+            freqMap[lChar]--;
+            if(freqMap[lChar]===0) delete freqMap[lChar];
             start++
         }
-        maxLen= Math.max(maxLen,end-start+1 )
+        maxLen = Math.max(maxLen, end-start+1)
     }
-    return maxLen
+    return maxLen;
+
 };

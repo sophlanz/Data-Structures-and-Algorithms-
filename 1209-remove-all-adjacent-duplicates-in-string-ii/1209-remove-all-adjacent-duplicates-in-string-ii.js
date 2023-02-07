274d3d2f6583141270df8a0af07cfa52b86f69f4
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-02-07 09:31:42 -0500

    Time: 92 ms (68.79%), Space: 50.5 MB (69.46%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.js
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.js
@@ -4,22 +4,20 @@
  * @return {string}
  */
 var removeDuplicates = function(s, k) {
-    let stack = [];
-    for( let i=0;i<s.length;i++) {
-        const char = s[i];
-       if(stack.length && stack[stack.length-1][0] === char) {
-           stack[stack.length-1][1]++
-           if(stack[stack.length-1][1]=== k) {
-               stack.pop();
-           }
-       }else {
-             stack.push([char, 1]);
-       }
-      
+
+ let stack=[];
+    for(const char of s) {
+        if(stack.length !== 0 && stack[stack.length-1][0]===char) {
+            stack[stack.length-1][1]++
+             if(stack[stack.length-1][1]===k) stack.pop();
+        }else {
+            stack.push([char,1]);
+        }
+       
     }
-    let res=''
-    for (const [char,count] of stack) {
-        res += char.repeat(count);
+    let res=""
+    for(const [char,count] of stack ) {
+        res += char.repeat(count)
     }
     return res;
 };

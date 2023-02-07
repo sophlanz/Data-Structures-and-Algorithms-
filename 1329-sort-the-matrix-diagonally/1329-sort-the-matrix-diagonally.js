61287dbe916c45d35c71104b2b56a75020022bb1
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-02-07 18:04:16 -0500

    Time: 78 ms (86.76%), Space: 44.9 MB (61.03%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.js
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.js
@@ -3,25 +3,23 @@
  * @return {number[][]}
  */
 var diagonalSort = function(mat) {
-   
- let map = {};
-    const rows = mat.length;
+let vectorMap = {};
+    const rows= mat.length;
     const cols = mat[0].length;
     for(let i=0;i<rows;i++) {
         for(let j=0;j<cols;j++) {
             let vector = i-j;
-            if(!map[vector]) map[vector]=[mat[i][j]]
-            else map[vector].push(mat[i][j]);
+            vectorMap[vector] ? vectorMap[vector].push(mat[i][j]) : vectorMap[vector] = [mat[i][j]];
         }
     }
-   for(const val in map) {
-       map[val].sort((a,b)=> a-b);
-   }
+    for(const val in vectorMap) {
+        vectorMap[val].sort((a,b)=> a-b);
+    }
     for(let i=0;i<rows;i++) {
         for(let j=0;j<cols;j++) {
             let vector = i-j;
-            mat[i][j] = map[vector].shift();
+            mat[i][j] = vectorMap[vector].shift();
         }
     }
-    return mat
+    return mat;
 };

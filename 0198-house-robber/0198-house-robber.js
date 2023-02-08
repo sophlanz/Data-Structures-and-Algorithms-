e9b15193f3b5bee6438de81880341c90bef02612
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-02-08 14:19:47 -0500

    Time: 63 ms (66.96%), Space: 41.8 MB (66.03%) - LeetHub

--- 0198-house-robber/0198-house-robber.js
+++ 0198-house-robber/0198-house-robber.js
@@ -3,11 +3,12 @@
  * @return {number}
  */
 var rob = function(nums) {
-    if(nums.length===1) return nums[0];
-    if(nums.length ===0) return 0;
-    let totals = [nums[0],Math.max(nums[0],nums[1])];
+   
+   if(nums.length===1) return nums[0];
+    if(!nums.length) return 0;
+    let totals = [nums[0], Math.max(nums[0],nums[1])];
     for(let i=2;i<nums.length;i++) {
-        totals[i] = Math.max(totals[i-1], totals[i-2] + nums[i]);
+        totals[i]=Math.max(nums[i]+totals[i-2], totals[i-1])
     }
-    return totals[totals.length-1]
+    return totals[nums.length-1]
 };

28154672a89804237878ee9db86005d0405b15d4
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-02-07 20:55:09 -0500

    Time: 289 ms (64.35%), Space: 67.8 MB (58.25%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.js
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.js
@@ -3,27 +3,28 @@
  * @return {string[][]}
  */
 var partition = function(s) {
-    let res=[]
-    const isPalindrome = (str) => {
-        let left=0,right=str.length-1;
-        while(left<right) {
-            if(str[left]!==str[right])return false
-            else {
-                left++
-                right--
-            }
+  let res=[];
+    const isPalindrome = (str)=> {
+        let l=0;
+       let r=str.length-1;
+        while(l<r){
+            if(str[l]===str[r]){
+                l++
+                r--
+            }else return false;
         }
         return true;
+        
     }
-    const DFS = (arr,str) => {
-        if(!str)res.push(arr);
-        for(let i=1;i<=str.length;i++) {
-            let subString = str.slice(0,i);
-            if(isPalindrome(subString)) {
-                DFS([...arr,subString],str.slice(i))
+    const DFS = (arr,str)=> {
+        if(!str.length)res.push(arr);
+        for(let i=1;i<=str.length;i++){
+            let subStr = str.slice(0,i);
+            if(isPalindrome(subStr)){
+                DFS([...arr,subStr], str.slice(i))
             }
         }
     }
     DFS([],s);
-    return res
+    return res;
 };

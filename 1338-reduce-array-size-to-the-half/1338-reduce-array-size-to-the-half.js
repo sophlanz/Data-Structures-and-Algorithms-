6ffc5093a6e811bc85a1249b99500c35f523d714
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-02-07 19:38:15 -0500

    Time: 127 ms (92.55%), Space: 58.1 MB (82.61%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.js
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.js
@@ -3,20 +3,19 @@
  * @return {number}
  */
 var minSetSize = function(arr) {
-     let freqMap = new Map();
-     for(const val of arr) {
-         if(!freqMap.get(val)) freqMap.set(val,1)
-         else freqMap.set(val,freqMap.get(val)+1)
-     }
- const countPairs=[...freqMap].sort((a,b)=> b[1]-a[1]);
-    let target = arr.length/2
-  let idx=0;
-    let count=0;
-    let sum=0;
+let freqMap = {};
+    for(const val of arr) {
+        freqMap[val]? freqMap[val]++ :freqMap[val]=1;
+    }
+    const freqIntegers = Object.values(freqMap).sort((a,b)=> b-a);
+    let target = Math.floor(arr.length/2);
+    let sum = 0;
+    let idx=0;
+    let count=0
     while(sum<target) {
-        sum+=countPairs[idx][1];
-        idx++
+        sum +=  freqIntegers[idx];
         count++
+        idx++
     }
     return count;
 };

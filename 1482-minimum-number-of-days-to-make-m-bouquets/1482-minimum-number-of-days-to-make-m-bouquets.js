431b016357345095c897d94fff3394586b3e824a
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-02-07 21:23:16 -0500

    Time: 108 ms (82.28%), Space: 52.7 MB (29.11%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.js
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.js
@@ -5,23 +5,26 @@
  * @return {number}
  */
 var minDays = function(bloomDay, m, k) {
- let left = Math.min(...bloomDay), right=Math.max(...bloomDay);
-    if(m*k > bloomDay.length) return -1
-    while(left<right) {
-        let mid = Math.floor((right+left)/2)
+if(m*k >bloomDay.length) return -1;
+    let l = Math.min(...bloomDay);
+    let r = Math.max(...bloomDay);
+    
+    while(l<r){
+        let mid = Math.floor((l+r)/2);
         let flowers=0;
-        let bouqs=0;
-        for(const val of bloomDay) {
-            if(val>mid) flowers=0;
-            else flowers++
-            if(flowers==k) {
-                bouqs++
-                flowers=0
+        let boquets=0;
+        for(const val of bloomDay){
+            if(val>mid) flowers=0
+            else {
+                flowers++
+            }
+            if(flowers===k){
+                boquets++;
+                flowers=0;
+            }
         }
-           
-    } 
-         if(bouqs>=m) right=mid;
-            else left=mid+1
-}
-    return left;
+        if(boquets >= m) r=mid
+        else l=mid+1
+    }
+    return l
 }

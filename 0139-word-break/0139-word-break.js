f7383cdfb2d59f62067cd7afe1f1eb1aafec4357
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-02-08 12:50:27 -0500

    Time: 67 ms (86.76%), Space: 44.2 MB (22.09%) - LeetHub

--- 0139-word-break/0139-word-break.js
+++ 0139-word-break/0139-word-break.js
@@ -4,17 +4,17 @@
  * @return {boolean}
  */
 var wordBreak = function(s, wordDict) {
-   let valid = new Array(s.length+1).fill(false);
-    valid[0] = true;
+    let valid = new Array(s.length+1).fill(false);
+    valid[0]=true;
     for(let i=0;i<s.length;i++) {
-        if(valid[i]===true) {
+        if(valid[i]==true) {
             for(const word of wordDict) {
-                let length = word.length;
-                let possibleString = s.slice(i,i+length)
-                if(word === possibleString) valid[i+length] = true;
+                let length = word.length
+                let possibleString = s.slice(i,i+length);
+                if(possibleString === word) valid[i+length]=true;
+                    
             }
-        }
+        } 
     }
     return valid[s.length]
-    
 };

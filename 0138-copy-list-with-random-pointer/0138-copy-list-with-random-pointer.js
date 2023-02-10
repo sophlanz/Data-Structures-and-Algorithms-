e81e627e07a13810f0b613b98c40ee5dab76c15f
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-02-10 09:29:37 -0500

    Time: 75 ms (34.99%), Space: 43.8 MB (67.20%) - LeetHub

---
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.js
@@ -0,0 +1,31 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, next, random) {
+ *    this.val = val;
+ *    this.next = next;
+ *    this.random = random;
+ * };
+ */
+
+/**
+ * @param {Node} head
+ * @return {Node}
+ */
+var copyRandomList = function(head) {
+    let curr = head;
+    let copyMap = new Map()
+    while(curr) {
+        const copy = new Node(curr.val);
+       copyMap.set(curr,copy);
+        curr = curr.next
+    }
+    curr=head;
+    while(curr) {
+        const copy = copyMap.get(curr);
+        copy.next = copyMap.get(curr.next)||null;
+        copy.random = copyMap.get(curr.random) || null;
+        
+        curr = curr.next;
+    }
+    return copyMap.get(head)
+};

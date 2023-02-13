34788faaf4f379b9ee77dee91bca1705537982cd
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-02-13 16:31:54 -0500

    Time: 59 ms (88.10%), Space: 42.3 MB (18.65%) - LeetHub

---
+++ 0413-arithmetic-slices/0413-arithmetic-slices.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var numberOfArithmeticSlices = function(nums) {
+   
+  	let sum = 0,
+		dp = Array(nums.length).fill(0);
+	for (let i = 2; i <= dp.length - 1; i++) {
+		if (nums[i] - nums[i - 1] === nums[i - 1] - nums[i - 2]) {
+           
+			dp[i] = 1 + dp[i - 1];
+            sum+= dp[i]
+            
+		}
+	}
+
+	return  sum;
+    
+};

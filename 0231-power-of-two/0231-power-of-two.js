3b488921df8c2026d957c0bd9b2a539434d31392
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-02-26 17:48:01 -0500

    Time: 66 ms (93.08%), Space: 43.7 MB (22.81%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function(n) {
+     if(n === 1){
+        return true;
+    }else if(n <= 0){
+        return false;
+    }
+    
+    if(n%2 !== 0){
+        return false;
+    }else{
+        return isPowerOfTwo(Math.floor(n/2));    
+    }
+};

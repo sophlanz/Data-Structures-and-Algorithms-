62db508f7f2b5f5aca669de18582768f453d9991
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-03-15 11:40:26 -0400

    Time: 418 ms (87.13%), Space: 87.3 MB (23.16%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.js
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.js
@@ -10,8 +10,8 @@ var TimeMap = function() {
  * @return {void}
  */
 TimeMap.prototype.set = function(key, value, timestamp) {
-      if (!this.map.has(key)) this.map.set(key, []);
-    this.map.get(key).push([timestamp, value]);
+    if(!this.map.has(key)) this.map.set(key,[]);
+    this.map.get(key).push([timestamp,value]);
 };
 
 /** 
@@ -20,23 +20,23 @@ TimeMap.prototype.set = function(key, value, timestamp) {
  * @return {string}
  */
 TimeMap.prototype.get = function(key, timestamp) {
-     if (!this.map.has(key)) return '';
-    const items = this.map.get(key);
-    
-    let left = 0, right = items.length - 1;
-    while (left <= right) {
-        const mid = Math.floor((left + right) / 2);
-        if (items[mid][0] <= timestamp) {
-            if (mid === items.length - 1 || items[mid + 1][0] > timestamp) {
-                return items[mid][1];
-            } else {
-                left = mid + 1;
+    if(!this.map.has(key)) return '';
+    let items = this.map.get(key);
+    let l=0,r=items.length-1;
+    while(l<=r) {
+        let mid = Math.floor((l+r)/2);
+        if(items[mid][0] <= timestamp){
+            if(mid === items.length-1 || items[mid+1][0] >timestamp) {
+                return items[mid][1]
+            }else {
+                l=mid+1
             }
         } else {
-            right = mid - 1;
+            r=mid-1
         }
     }
-    return '';
+    return ''
+    
 };
 
 /** 

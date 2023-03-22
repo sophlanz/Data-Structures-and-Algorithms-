130d7473bb8d4fe90c6ba14810d225bc1f306312
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-03-22 13:02:54 -0400

    Time: 71 ms (37.91%), Space: 42.1 MB (56.53%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPow = function(x, n) {
+    if(n===0) return  1;
+
+// 2^4 =>  2*2*2*2 => 4 * 4 => 4^2 =>  x*x , n/2
+    if(n%2===0) return myPow(x*x,n/2)
+   // 2^-2 = 1/2^2 => 1/x , n*-1
+    if(n<0) return myPow(1/x, n*-1)
+   //2^3 => 2*2*2 => 4*2 =>x*x, n-1
+    //2^5 => 2*2*2*2*2 => 4*4*2=> 4^2 * 2 =>  x*x, x*x, n-1/2
+    else return myPow(x*x, (n-1)/2)*x
+
+};

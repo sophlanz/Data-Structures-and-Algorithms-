b6169693ae7d5434a56bb95715db564ec95b395a
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-03-28 12:56:07 -0400

    Time: 85 ms (93.60%), Space: 52.7 MB (40.00%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.js
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.js
@@ -5,11 +5,11 @@
  */
 var minCost = function(colors, neededTime) {
     let time=0;
-   for(let i=1;i<neededTime.length;i++) {
-       if(colors[i]===colors[i-1]) {
-           time += Math.min(neededTime[i], neededTime[i-1]);
-           neededTime[i] = Math.max(neededTime[i], neededTime[i-1]);
-       }
-   }
-    return time;
+    for(let i=1;i<colors.length;i++) {
+        if(colors[i]=== colors[i-1]) {
+        time += Math.min(neededTime[i], neededTime[i-1])
+        neededTime[i]= Math.max(neededTime[i],neededTime[i-1]);
+        }
+    }
+    return time
 };

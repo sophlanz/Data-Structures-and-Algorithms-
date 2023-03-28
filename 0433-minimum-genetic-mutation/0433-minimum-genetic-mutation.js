881e44e7364b3efaa13dc6430b1fbe20a99690ea
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-03-28 14:09:55 -0400

    Time: 50 ms (95.90%), Space: 41.9 MB (59.02%) - LeetHub

--- 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.js
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.js
@@ -5,23 +5,25 @@
  * @return {number}
  */
 var minMutation = function(start, end, bank) {
-  let banks = new Set(bank);
-    if(!banks.has(end)) return -1;
-    let queue = [[start,0]];
-    let dicts = ["A", "C",  "G", "T"];
-    while(queue.length) {
-        const [gene,count] = queue.shift();
-        if(gene === end) return count;
-        for(let i=0;i<gene.length;i++) {
-            for(let j=0;j<4;j++) {
-                if(gene[i] === dicts[j]) continue;
-                let possibleGene = gene.slice(0,i)+ dicts[j] + gene.slice(i+1)
-                if(banks.has(possibleGene)) {
-                    queue.push([possibleGene, count+1]);
-                    banks.delete(possibleGene);
-                }
+   let banks = new Set(bank);
+    if(!banks.has(end))return -1;
+    const dict = ['A', 'C', 'T', 'G'];
+    let queue = [[start,0]]
+    while(queue.length ) {
+    const [gene,count] = queue.shift();
+     
+        if(gene === end) return count
+        for(let i=0;i<start.length;i++) {
+        for(const char of dict) {
+            if(gene[i]===char) continue;
+            let potentialRep = gene.slice(0,i)+char+gene.slice(i+1);
+            if(banks.has(potentialRep)){
+                queue.push([potentialRep,count+1])
+                banks.delete(potentialRep)
             }
+        } 
         }
-    }
-    return -1;
+    
+}
+    return -1
 };

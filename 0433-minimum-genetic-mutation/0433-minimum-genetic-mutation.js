43b0092ce21d5a9fa6998bdab38da454d8645f4b
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-03-29 12:27:46 -0400

    Time: 60 ms (57.26%), Space: 42.1 MB (40.32%) - LeetHub

--- 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.js
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.js
@@ -5,21 +5,20 @@
  * @return {number}
  */
 var minMutation = function(startGene, endGene, bank) {
+    let dict = ["A","G", "C", "T"];
     let banks = new Set(bank);
-    if(!banks.has(endGene)) return -1;
     let queue = [[startGene,0]];
-    const dict = ["A", "C", "G", "T"]
+    if(!banks.has(endGene)) return -1;
     while(queue.length) {
-        const [gene,count] = queue.shift();
+        const[gene, count] = queue.shift();
         if(gene === endGene) return count;
-        console.log(gene)
         for(let i=0;i<startGene.length;i++) {
             for(const char of dict) {
-                if(char===gene[i]) continue;
-                let possibleRep = gene.slice(0,i) + char + gene.slice(i+1);
-                if(banks.has(possibleRep)){
-                    queue.push([possibleRep,count+1]);
-                    banks.delete(possibleRep);
+                if(char === gene) continue;
+                let possible = gene.slice(0,i) + char+ gene.slice(i+1);
+                if(banks.has(possible)){
+                    queue.push([possible,count+1])
+                    banks.delete(possible);
                 }
             }
         }

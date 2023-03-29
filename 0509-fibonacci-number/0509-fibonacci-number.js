3c5be2306b11295d089575043ef0f3dcb4797288
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-03-29 13:00:58 -0400

    Time: 691 ms (5.07%), Space: 48 MB (6.00%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.js
+++ 0509-fibonacci-number/0509-fibonacci-number.js
@@ -2,10 +2,10 @@
  * @param {number} n
  * @return {number}
  */
-let memo = {};
-memo[1] =1;
-memo[0]=0
+
 var fib = function(n) {
-    if(n in memo) return memo[n];
+  let memo={};
+    if(n<=1) return n;
+    if(memo[n]) return memo[n];
     return memo[n] = fib(n-1) + fib(n-2);
 };

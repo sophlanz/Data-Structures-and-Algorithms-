8c943fa736b637e0d304321fe499c590a445301a
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-03-30 15:49:28 -0400

    Time: 2106 ms (5.39%), Space: 49.7 MB (23.95%) - LeetHub

--- 1169-invalid-transactions/1169-invalid-transactions.js
+++ 1169-invalid-transactions/1169-invalid-transactions.js
@@ -3,22 +3,27 @@
  * @return {string[]}
  */
 var invalidTransactions = function(transactions) {
-   let invalid = new Array(transactions.length).fill(false);
+    const invalid = new Array(transactions.length).fill(false);
     for(let i=0;i<transactions.length;i++) {
-        const [name,time,price,city] = transactions[i].split(',');
-        if(price >1000) invalid[i]=true;
-        for(let j=i+1;j<transactions.length;j++) {
-            const [name2,time2,price2,city2] = transactions[j].split(',');
-            if(city !== city2 && Math.abs(time2-time) <=60 && name===name2) {
-                invalid[j] = true;
+        const [name,time,amount,city] = transactions[i].split(',');
+        if(amount > 1000) {
+            invalid[i] = true;
+        }
+        for(let j=0;j<transactions.length;j++) {
+            const [name2,time2,amount2,city2] = transactions[j].split(',');
+            if(name === name2 && Math.abs(time2-time)<=60 && city !== city2) {
                 invalid[i] = true;
+                invalid[j] = true;
+            
             }
         }
     }
     let res=[];
-    let idx=0
-    for(let invalidTransaction of invalid) {
-        if(invalidTransaction) res.push(transactions[idx])
+    let idx=0;
+    for(const val of invalid) {
+        if(val === true) {
+            res.push(transactions[idx]);
+        }
         idx++
     }
     return res;

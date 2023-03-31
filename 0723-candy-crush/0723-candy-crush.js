bc310e32b5df4c28c8fea3b50434f78d4af735ec
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-03-31 16:49:40 -0400

    Time: 85 ms (62.50%), Space: 45 MB (72.50%) - LeetHub

--- 0723-candy-crush/0723-candy-crush.js
+++ 0723-candy-crush/0723-candy-crush.js
@@ -3,22 +3,22 @@
  * @return {number[][]}
  */
 var candyCrush = function(board) {
-    let found = true;
+    let found=true;
     const rows = board.length;
     const cols = board[0].length;
     while(found) {
-        found=false;
+        found= false;
         for(let i=0;i<rows;i++) {
             for(let j=0;j<cols;j++) {
-                let val = Math.abs(board[i][j]);
-                if(val === 0) continue;
-                if(i<rows-2 && val === Math.abs(board[i+1][j]) && val === Math.abs(board[i+2][j])){
+                let value = Math.abs(board[i][j])
+                if(value === 0) continue;
+                if(i+2 < rows && Math.abs(board[i+1][j]) === value && Math.abs(board[i+2][j]) === value) {
                     found=true;
-                    board[i][j]=board[i+1][j] = board[i+2][j] = -val;
+                    board[i][j] = board[i+1][j] = board[i+2][j] = -value;
                 }
-                if(j<cols-2 && val === Math.abs(board[i][j+1]) && val === Math.abs(board[i][j+2])) {
-                    found = true;
-                    board[i][j]= board[i][j+1] = board[i][j+2] = -val;
+                if(j+2 < cols && Math.abs(board[i][j+1])===value && Math.abs(board[i][j+2])===value ) {
+                    found=true;
+                    board[i][j] = board[i][j+1] = board[i][j+2] = -value;
                 }
             }
         }
@@ -26,16 +26,18 @@ var candyCrush = function(board) {
             for(let j=0;j<cols;j++) {
                 let idx = rows-1;
                 for(let i=idx;i>=0;i--) {
-                    if(board[i][j] >0){
-                        board[idx][j] = board[i][j];
+                    if(board[i][j] >0) {
+                          board[idx][j] = board[i][j];
                         idx--
                     }
                 }
                 for(let i=idx;i>=0;i--) {
                     board[i][j] = 0;
                 }
+            
             }
         }
     }
-    return board;
+    return board
+    
 };

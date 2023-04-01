4d03cbe97a45ee63dfcbad97763bf78a73de6d07
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-04-01 15:22:41 -0400

    Time: 49 ms (97.03%), Space: 42 MB (33.66%) - LeetHub

--- 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.js
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.js
@@ -4,14 +4,14 @@
  * @return {number}
  */
 var kthGrammar = function(n, k) {
-    if(n===1) return 0;
+ if(n===1) return 0;
     const flip = (num)=> {
-        return num===0 ? 1 : 0;
+        return num === 0 ? 1 : 0;
     }
     if(k%2===0) {
-        return flip(kthGrammar(n-1, k/2))
-    }else {
-        return kthGrammar(n-1,(k+1)/2)
+        return flip(kthGrammar(n-1,k/2))
+    }else{
+        return kthGrammar(n-1, (k+1)/2)
     }
 };
 

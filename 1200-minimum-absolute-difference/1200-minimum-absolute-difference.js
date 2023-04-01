206edf3399f165e5300dcf1f3577dc7447d438d5
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-04-01 17:03:34 -0400

    Time: 159 ms (74.58%), Space: 55.6 MB (20.00%) - LeetHub

--- 1200-minimum-absolute-difference/1200-minimum-absolute-difference.js
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.js
@@ -3,14 +3,16 @@
  * @return {number[][]}
  */
 var minimumAbsDifference = function(arr) {
-   arr.sort((a,b)=>a-b);
-    let diff = Infinity;
+  arr.sort((a,b)=> a-b);
+    let minDiff=Infinity;
     for(let i=1;i<arr.length;i++) {
-        diff = Math.min(diff, arr[i]-arr[i-1])
+        const diff = arr[i]- arr[i-1];
+        minDiff = Math.min(minDiff,diff);
     }
-    let res = [];
+    let res=[];
     for(let i=1;i<arr.length;i++) {
-        if(arr[i]-arr[i-1]===diff) res.push([arr[i-1],arr[i]])
+        const diff = arr[i]-arr[i-1];
+        if(diff === minDiff) res.push([arr[i-1], arr[i]]);
     }
     return res;
 };

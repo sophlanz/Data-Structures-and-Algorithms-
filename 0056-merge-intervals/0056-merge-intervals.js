57fcf5565f0ff457a376ebc10c57e813263d93f4
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-04-01 14:48:44 -0400

    Time: 108 ms (51.14%), Space: 50.6 MB (22.39%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.js
+++ 0056-merge-intervals/0056-merge-intervals.js
@@ -4,19 +4,19 @@
  */
 var merge = function(intervals) {
     intervals.sort((a,b)=> a[0]-b[0]);
+    let merged=[];
     let start = intervals[0][0];
     let end = intervals[0][1];
-    let res=[];
-    for(let i=0;i<intervals.length;i++) {
-        let interval = intervals[i];
-        if(end >= interval[0]) {
-            end = Math.max(end,interval[1]);
-        }else {
-            res.push([start,end]);
-            start =interval[0];
+    for(let i=1;i<intervals.length;i++) {
+        const interval = intervals[i];
+        if(interval[0]<=end) {
+            end = Math.max(end, interval[1]);
+        }else{
+            merged.push([start,end]);
+            start = interval[0];
             end=interval[1];
         }
     }
-    res.push([start,end]);
-    return res;
+    merged.push([start,end]);
+    return merged;
 };

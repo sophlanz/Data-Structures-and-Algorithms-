f763f922efd1e6dbcc25b5f7849853c6630676df
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-04-02 10:54:39 -0400

    Time: 77 ms (83.77%), Space: 52.1 MB (68.06%) - LeetHub

--- 0134-gas-station/0134-gas-station.js
+++ 0134-gas-station/0134-gas-station.js
@@ -4,15 +4,16 @@
  * @return {number}
  */
 var canCompleteCircuit = function(gas, cost) {
-    let tank=0,start=0,total=0;
+let start=0,tank=0,total=0;
     for(let i=0;i<gas.length;i++) {
-        let consumption = gas[i]-cost[i];
-        tank += consumption;
-        if(tank<0) {
-            tank=0;
-            start=i+1;
+        let consume = gas[i]-cost[i];
+         tank += consume
+        if(tank < 0) {
+            start= i +1
+            tank=0
         }
-        total += consumption;
+        total+= consume
     }
-    return total >=0 ? start:-1;
+    return total >=0 ? start: -1;
+  
 };

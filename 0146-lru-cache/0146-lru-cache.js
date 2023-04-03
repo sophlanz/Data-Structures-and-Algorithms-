a4e9aa2dcb0362f34d3058f412896d3d52d99a98
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-04-03 17:17:19 -0400

    Time: 570 ms (98.13%), Space: 98.5 MB (37.97%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.js
+++ 0146-lru-cache/0146-lru-cache.js
@@ -1,37 +1,92 @@
 /**
  * @param {number} capacity
  */
+var Node = function(key, value) {
+    this.key = key;
+    this.val = value;
+    this.prev = null;
+    this.next = null;
+}
+var DoublyLinkedList = function() {
+    this.head = new Node;
+    this.tail = new Node;
+    this.head.next = this.tail;
+    this.tail.prev = this.head;
+}
+/**
+ * Insert a new node right after head
+ * @param {Node} node
+ */
+DoublyLinkedList.prototype.insertHead = function(node) {
+    node.prev = this.head;
+    node.next = this.head.next;
+    this.head.next.prev = node;
+    this.head.next = node;
+}
+DoublyLinkedList.prototype.removeNode = function(node) {
+    // console.log("trying to remove node:", node.key)
+    let prev = node.prev;
+    let next = node.next;
+    prev.next = next;
+    next.prev = prev;
+}
+/**
+ * Move a node to the head
+ * @param {Node} node
+ */
+DoublyLinkedList.prototype.moveToHead = function(node){
+    this.removeNode(node);
+    this.insertHead(node);
+}
+/**
+ * Remove the tail element and return its key
+ * @return {String}
+ */
+DoublyLinkedList.prototype.removeTail = function() {
+    let tail = this.tail.prev;
+    this.removeNode(tail);
+    return tail.key;
+}
+/**
+ * @param {number} capacity
+ */
 var LRUCache = function(capacity) {
-    this.cache = new Map();
-    this.capacity=capacity;
+    this.capacity = capacity;
+    this.hash = new Map();
+    this.dll = new DoublyLinkedList();
 };
-
 /** 
  * @param {number} key
  * @return {number}
  */
 LRUCache.prototype.get = function(key) {
-    if(!this.cache.has(key))return -1;
-    const value = this.cache.get(key);
-    this.cache.delete(key);
-    this.cache.set(key,value);
-    return value
+    if(!this.hash.has(key))return -1;
+    let node = this.hash.get(key);
+    this.dll.moveToHead(node);
+    return node.val;
 };
-
 /** 
  * @param {number} key 
  * @param {number} value
  * @return {void}
  */
 LRUCache.prototype.put = function(key, value) {
-    if(this.cache.has(key))this.cache.delete(key);
-    this.cache.set(key,value);
-    if(this.cache.size>this.capacity) {
-        const LRU = this.cache.keys().next().value;
-        this.cache.delete(LRU);
+    let node = this.hash.get(key);
+    if (node==null) { // new node
+        let newNode = new Node(key, value);
+        this.hash.set(key, newNode);
+        this.dll.insertHead(newNode);
+        
+        if (this.hash.size > this.capacity) {
+            let tailKey = this.dll.removeTail();
+            this.hash.delete(tailKey);
+           
+        }
+    } else { // existed node, update its value and move to head;
+        node.val = value;
+        this.dll.moveToHead(node);
     }
 };
-
 /** 
  * Your LRUCache object will be instantiated and called as such:
  * var obj = new LRUCache(capacity)

fdeb4403acd2f78156974bbcd08c70c88eda11d1
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-04-03 10:02:34 -0400

    Time: 53 ms (92.08%), Space: 41.8 MB (97.77%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
@@ -4,32 +4,39 @@
  * @return {number[]}
  */
 var searchRange = function(nums, target) {
-            //target<=mid search left  if=== mid, record index , search left and right
-            //else search right
-            //while nums[mid+1]  === target and nums[mid-1]
-             //sort indexes and return
-    let l=0,r=nums.length-1;
-    let indexes=[];
-
+  let l=0,r=nums.length;
+    //get start
     while(l<=r) {
-        let mid = Math.floor((l+r)/2);
-        if(nums[mid]===target) {
-            //get start
-           let start=mid;
-            while(nums[start-1]===target) {
-                start--
-            }
-            let end=mid
-            while(nums[end+1]===target){
-                end++
-            }
-            return[start,end]
-        }else if(nums[mid]<target) {
-            l=mid+1
+        let mid= Math.floor((l+r)/2);
+        if(nums[mid]>=target) {
+            r=mid-1
         }else{
-            r=mid-1;
+            l=mid+1
         }
     }
-   return [-1,-1]
-    
+    let start=l
+    if(nums[start] !== target) return [-1,-1];
+    l=0,r=nums.length-1
+    while(l<=r) {
+        let mid = Math.floor((l+r)/2);
+        if(nums[mid]<=target) {
+            l=mid+1
+        }else{
+            r=mid-1
+        }
+        
+    }
+    let end = r;
+    return [start,end]
 };
+//nums = [5,7,7,8,8,10]
+//low=3
+//high=2
+//mid=3
+//start=low=3
+//nums[mid] >= target ? move higher left
+//low=5
+//high=4
+//mid=5
+//end=high=4
+//nums[mid]<= target ? move lower right

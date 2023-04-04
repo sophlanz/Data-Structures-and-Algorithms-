912b03050262b29c201f7107f30b08fe8ad21020
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-04-04 13:24:19 -0400

    Time: 182 ms (22.02%), Space: 49.2 MB (18.45%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.js
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.js
@@ -5,13 +5,13 @@
  */
 var minSteps = function(s, t) {
     let freqMap={};
-    for(let i=0;i<s.length;i++) {
-      freqMap[s[i]]  ? freqMap[s[i]]++ :  freqMap[s[i]] =  1;
-      freqMap[t[i]] ? freqMap[t[i]]-- :    freqMap[t[i]] =-1;
+    for(let i=0;i<s.length;i++){
+        freqMap[s[i]] ? freqMap[s[i]]++ : freqMap[s[i]]=1;
+        freqMap[t[i]] ? freqMap[t[i]]-- : freqMap[t[i]]=-1;
     }
     let count=0
-    for(const val in freqMap) {
-        if(freqMap[val]>0) count += freqMap[val];
+    for(const char in freqMap){
+        if(freqMap[char]>0)count+= freqMap[char]
     }
-    return count;
+    return count
 };

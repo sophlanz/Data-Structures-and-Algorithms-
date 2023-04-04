7133d1e19a950c2e9d35346a1f4bf36f0c7f1a2f
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-04-04 16:11:31 -0400

    Time: 93 ms (91.03%), Space: 47.4 MB (75.34%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.js
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.js
@@ -11,27 +11,27 @@
  * @return {ListNode}
  */
 var addTwoNumbers = function(l1, l2) {
-    let s1=[], s2=[];
-    while(l1) {
-        s1.push(l1.val);
+    let s1=[];
+    let s2=[];
+    while(l1){
+        s1.push(l1.val)
         l1=l1.next;
     }
-    while(l2) {
+    while(l2){
         s2.push(l2.val);
         l2=l2.next;
     }
-    let carry =0;
-    let list = null
+    let carry=0;
+    let list=null
     while(s1.length || s2.length || carry) {
-        const v1 = s1.length ? s1.pop() : 0;
-        const v2 = s2.length ? s2.pop() : 0;
-        let sum = (v1+v2+carry);
-        let digit = (sum%10);
-         carry = Math.floor(sum/10);
-        let node = new ListNode(digit);
+        let v1= s1.length ? s1.pop() : 0;
+        let v2= s2.length ? s2.pop() : 0;
+        let node = new ListNode((v1+v2+carry)%10);
+        carry=Math.floor((v1+v2+carry)/10)
         node.next = list;
-        list = node;
+        list=node;
     }
     return list
-
 };
+//Time complexity O(m+n)length of lists
+//Space compleity O(m+n) length of lists stored in stack

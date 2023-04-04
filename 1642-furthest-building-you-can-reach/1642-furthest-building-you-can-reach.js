1cd7cc9e96421c57f3d7b86708ac1874262ba71e
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-04-04 15:53:46 -0400

    Time: 194 ms (29.69%), Space: 58.6 MB (46.88%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.js
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.js
@@ -5,18 +5,17 @@
  * @return {number}
  */
 var furthestBuilding = function(heights, bricks, ladders) {
-    let minHeap = new MinPriorityQueue();
-    for(let i=1;i<heights.length;i++) {
-        const diff = heights[i]-heights[i-1];
-        if(diff >0){
+   let minHeap = new MinPriorityQueue();
+    for(let i=1;i<heights.length;i++){
+        let diff = heights[i]- heights[i-1];
+        if(diff > 0 ){
             minHeap.enqueue(diff).element;
-            if(minHeap.size()> ladders) {
-                let smallestDiff = minHeap.dequeue().element;
-                bricks -= smallestDiff;
-                if(bricks < 0) {
-                    return i-1
-                }
+            if(minHeap.size()>ladders){
+               let forBricks= minHeap.dequeue().element
+               bricks -= forBricks;
+                if(bricks<0) return i-1;
             }
+            
         }
     }
     return heights.length-1

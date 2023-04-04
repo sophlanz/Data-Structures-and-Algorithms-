0c3233b1f92273580f389e48fd66127e8bf681b1
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-04-04 16:20:07 -0400

    Time: 85 ms (48.68%), Space: 47.9 MB (38.16%) - LeetHub

--- 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.ts
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.ts
@@ -15,24 +15,25 @@
  */
 
 function connect(root: Node | null): Node | null {
- 
-    const findNext = (node) => {
-        while(node.next) {
-            node = node.next
+    const getNext = (node)=> {
+        while(node.next){
+            node = node.next;
             if(node.left) return node.left;
-            if(node.right) return node.right
+            if(node.right)return node.right
         }
         return null
-    };
-   if(!root) return null
-    if(root.left) {
-        if(root.right) root.left.next = root.right
-        else root.left.next = findNext(root);
     }
-    if(root.right) {
-        root.right.next = findNext(root);
+    if(!root) return null;
+    if(root.left){
+        if(root.right)root.left.next = root.right;
+        else {
+            root.left.next = getNext(root);
+        }
     }
-    if(root.right)connect(root.right);
-    if(root.left)connect(root.left);
+    if(root.right){
+        root.right.next = getNext(root);
+    }
+    connect(root.right);
+    connect(root.left);
     return root;
 };

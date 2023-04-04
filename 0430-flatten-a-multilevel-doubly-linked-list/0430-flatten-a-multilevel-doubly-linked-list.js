a57a407d19113ad85c6d52da96affa02a463e0f2
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-04-04 10:28:54 -0400

    Time: 72 ms (16.76%), Space: 43.3 MB (77.93%) - LeetHub

--- 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.js
+++ 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.js
@@ -13,20 +13,22 @@
  * @return {Node}
  */
 var flatten = function(head) {
- let curr = head;
-    while(curr) {
-        if(curr.child) {
+   let curr=head;
+    while(curr){
+        if(curr.child){
             let end = curr.child;
-            while(end.next) {
-                end = end.next
+            while(end.next){
+                end=end.next;
             }
             end.next = curr.next;
-            if(end.next) end.next.prev = end;
+            if(end.next)end.next.prev = end;
             curr.next = curr.child;
             curr.next.prev = curr;
-            curr.child = null
+            curr.child=null;
         }
         curr = curr.next;
-    } 
+    }
     return head;
 };
+//space O(D)max depth of child nodes, stack
+//Time O(N)

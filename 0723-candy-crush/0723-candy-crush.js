1acd8bb9ba9a1078d82e8facf507c13a1afe2d0a
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-04-04 13:42:36 -0400

    Time: 74 ms (86.67%), Space: 44.9 MB (63.33%) - LeetHub

--- 0723-candy-crush/0723-candy-crush.js
+++ 0723-candy-crush/0723-candy-crush.js
@@ -3,41 +3,39 @@
  * @return {number[][]}
  */
 var candyCrush = function(board) {
-    let found=true;
-    const rows = board.length;
+    const rows =board.length;
     const cols = board[0].length;
-    while(found) {
-        found= false;
-        for(let i=0;i<rows;i++) {
-            for(let j=0;j<cols;j++) {
-                let value = Math.abs(board[i][j])
-                if(value === 0) continue;
-                if(i+2 < rows && Math.abs(board[i+1][j]) === value && Math.abs(board[i+2][j]) === value) {
-                    found=true;
-                    board[i][j] = board[i+1][j] = board[i+2][j] = -value;
+let found=true;
+    while(found){
+        found=false
+        for(let i=0;i<rows;i++){
+            for(let j=0;j<cols;j++){
+                let val = Math.abs(board[i][j]);
+                if(val===0)continue;
+                if(i+2<rows && Math.abs(board[i+1][j]) === val && Math.abs(board[i+2][j])===val) {
+                    found=true
+                    board[i][j] = board[i+1][j] = board[i+2][j] = -val;
                 }
-                if(j+2 < cols && Math.abs(board[i][j+1])===value && Math.abs(board[i][j+2])===value ) {
-                    found=true;
-                    board[i][j] = board[i][j+1] = board[i][j+2] = -value;
+                if(j+2<cols && Math.abs(board[i][j+1]) === val && Math.abs(board[i][j+2])===val) {
+                    found=true
+                    board[i][j] = board[i][j+1] = board[i][j+2] = -val;
                 }
             }
         }
-        if(found) {
-            for(let j=0;j<cols;j++) {
-                let idx = rows-1;
-                for(let i=idx;i>=0;i--) {
-                    if(board[i][j] >0) {
-                          board[idx][j] = board[i][j];
+        if(found){
+            for(let j=0;j<cols;j++){
+                let idx=rows-1;
+                for(let i=idx;i>=0;i--){
+                    if(board[i][j]>0){
+                        board[idx][j] = board[i][j]
                         idx--
                     }
                 }
-                for(let i=idx;i>=0;i--) {
+                for(let i=idx;i>=0;i--){
                     board[i][j] = 0;
                 }
-            
             }
         }
     }
     return board
-    
 };

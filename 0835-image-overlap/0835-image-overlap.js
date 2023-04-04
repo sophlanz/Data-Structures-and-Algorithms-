d517d1b571f03250aec5890eab9fcd6969306fc4
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-04-04 16:30:30 -0400

    Time: 1995 ms (24.14%), Space: 52.1 MB (10.34%) - LeetHub

--- 0835-image-overlap/0835-image-overlap.js
+++ 0835-image-overlap/0835-image-overlap.js
@@ -4,23 +4,23 @@
  * @return {number}
  */
 var largestOverlap = function(img1, img2) {
+    const rows=img1.length;
+    const cols=img1[0].length
     let v1=[];
     let v2=[];
-    const rows = img1.length;
-    const cols = img2.length;
-    for(let i=0;i<rows;i++) {
-        for(let j=0;j<cols;j++) {
-            if(img1[i][j]===1) v1.push([i,j]);
-            if(img2[i][j]===1)v2.push([i,j]);
+    for(let i=0;i<rows;i++){
+        for(let j=0;j<cols;j++){
+            if(img1[i][j] !==0) v1.push([i,j]);
+            if(img2[i][j] !==0)v2.push([i,j]);
         }
     }
     let diffMap={};
-    let maxOverlap=0;
-    for(const val1 of v1) {
-        for(const val2 of v2) {
-            const diff = [val1[0]-val2[0], val1[1]-val2[1]]
-            diffMap[diff] ? diffMap[diff]++ : diffMap[diff] =1;
-            maxOverlap = Math.max(maxOverlap, diffMap[diff])
+    let maxOverlap =0;
+    for(const [i1,j1] of v1){
+        for(const [i2,j2] of v2){
+            let diff = [i2-i1,j2-j1];
+            diffMap[diff] ? diffMap[diff]++ : diffMap[diff]=1;
+            maxOverlap = Math.max(maxOverlap, diffMap[diff]);
         }
     }
     return maxOverlap

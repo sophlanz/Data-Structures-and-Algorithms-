238685f5bb76f971e663ac6234d82c3d5e5ef498
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-04-04 16:52:18 -0400

    Time: 555 ms (99.35%), Space: 100.7 MB (21.20%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.js
+++ 0146-lru-cache/0146-lru-cache.js
@@ -1,92 +1,80 @@
 /**
  * @param {number} capacity
  */
-var Node = function(key, value) {
-    this.key = key;
-    this.val = value;
-    this.prev = null;
-    this.next = null;
+//MRU head, LRU tail
+class Node {
+    constructor(key,val,prev){
+        this.val=val;
+        this.key=key;
+        this.prev=this.next=null;
+    }
 }
-var DoublyLinkedList = function() {
-    this.head = new Node;
-    this.tail = new Node;
+var DoublyLinkedList = function(){
+    this.tail = new Node();
+    this.head = new Node();
     this.head.next = this.tail;
     this.tail.prev = this.head;
 }
-/**
- * Insert a new node right after head
- * @param {Node} node
- */
-DoublyLinkedList.prototype.insertHead = function(node) {
-    node.prev = this.head;
+DoublyLinkedList.prototype.insertHead = function(node){
     node.next = this.head.next;
+    node.prev = this.head;
     this.head.next.prev = node;
     this.head.next = node;
 }
-DoublyLinkedList.prototype.removeNode = function(node) {
-    // console.log("trying to remove node:", node.key)
-    let prev = node.prev;
+DoublyLinkedList.prototype.removeNode = function(node){
     let next = node.next;
-    prev.next = next;
+    let prev = node.prev;
     next.prev = prev;
+    prev.next = next;
 }
-/**
- * Move a node to the head
- * @param {Node} node
- */
 DoublyLinkedList.prototype.moveToHead = function(node){
-    this.removeNode(node);
+   this.removeNode(node);
     this.insertHead(node);
 }
-/**
- * Remove the tail element and return its key
- * @return {String}
- */
-DoublyLinkedList.prototype.removeTail = function() {
+DoublyLinkedList.prototype.removeTail = function(){
     let tail = this.tail.prev;
     this.removeNode(tail);
-    return tail.key;
+    return tail.key
 }
-/**
- * @param {number} capacity
- */
 var LRUCache = function(capacity) {
-    this.capacity = capacity;
-    this.hash = new Map();
+    this.cache = new Map();
     this.dll = new DoublyLinkedList();
+    this.capacity = capacity;
 };
+
 /** 
  * @param {number} key
  * @return {number}
  */
 LRUCache.prototype.get = function(key) {
-    if(!this.hash.has(key))return -1;
-    let node = this.hash.get(key);
+if(!this.cache.has(key))return -1
+    let node = this.cache.get(key);
     this.dll.moveToHead(node);
     return node.val;
 };
+
 /** 
  * @param {number} key 
  * @param {number} value
  * @return {void}
  */
 LRUCache.prototype.put = function(key, value) {
-    let node = this.hash.get(key);
-    if (node==null) { // new node
-        let newNode = new Node(key, value);
-        this.hash.set(key, newNode);
-        this.dll.insertHead(newNode);
-        
-        if (this.hash.size > this.capacity) {
-            let tailKey = this.dll.removeTail();
-            this.hash.delete(tailKey);
-           
+    if(!this.cache.has(key)){
+        let node = new Node(key,value);
+        this.dll.insertHead(node);
+        this.cache.set(key,node)
+        if(this.cache.size > this.capacity){
+            const LRU = this.dll.removeTail();
+            this.cache.delete(LRU);
         }
-    } else { // existed node, update its value and move to head;
+    }else{
+        let node = this.cache.get(key);
         node.val = value;
+        this.cache.set(key,node)
         this.dll.moveToHead(node);
     }
 };
+
 /** 
  * Your LRUCache object will be instantiated and called as such:
  * var obj = new LRUCache(capacity)

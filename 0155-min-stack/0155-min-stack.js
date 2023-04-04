782058f63169a3e5f26bddce4c13802a6f71c620
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-04-04 16:59:55 -0400

    Time: 113 ms (37.70%), Space: 49.8 MB (57.62%) - LeetHub

--- 0155-min-stack/0155-min-stack.js
+++ 0155-min-stack/0155-min-stack.js
@@ -1,9 +1,6 @@
-
 var MinStack = function() {
     this.stack =[];
-
 };
-
 /** 
  * @param {number} val
  * @return {void}
@@ -20,23 +17,19 @@ MinStack.prototype.push = function(val) {
  */
 MinStack.prototype.pop = function() {
     this.stack.pop();
-  
 };
-
 /**
  * @return {number}
  */
 MinStack.prototype.top = function() {
  return this.stack[this.stack.length-1].val
 };
-
 /**
  * @return {number}
  */
 MinStack.prototype.getMin = function() {
     return this.stack[this.stack.length-1].min
 };
-
 /** 
  * Your MinStack object will be instantiated and called as such:
  * var obj = new MinStack()

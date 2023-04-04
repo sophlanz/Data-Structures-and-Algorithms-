6a74fc04192642e97d93de595b8abf70748ae34a
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-04-04 13:13:58 -0400

    Time: 76 ms (42.86%), Space: 44.2 MB (48.98%) - LeetHub

--- 0311-sparse-matrix-multiplication/0311-sparse-matrix-multiplication.js
+++ 0311-sparse-matrix-multiplication/0311-sparse-matrix-multiplication.js
@@ -4,18 +4,22 @@
  * @return {number[][]}
  */
 var multiply = function(mat1, mat2) {
-    if(!mat1.length || !mat2.length || mat1[0].length !== mat2.length) return [];
+if(!mat1.length || !mat2.length || mat1[0].length !== mat2.length) return [];
     const rows = mat1.length;
     const cols = mat1[0].length;
     const colsB = mat2[0].length;
-    let res = new Array(rows).fill(0).map(()=>new Array(colsB).fill(0));
-    for(let i=0;i<rows;i++) {
-        for(let j=0;j<cols;j++) {
-            if(mat1[i][j] === 0)continue
-            for(let k=0;k<colsB;k++) {
-                res[i][k] += mat1[i][j] * mat2[j][k]
+    let res = new Array(rows).fill(0).map(()=> new Array(colsB).fill(0));
+    for(let i=0;i<rows;i++){
+        for(let j=0;j<cols;j++){
+            if(mat1[i][j]===0)continue;
+            for(let k=0;k<colsB;k++){
+                res[i][k] += mat1[i][j] * mat2[j][k];
             }
         }
     }
     return res;
 };
+//board m x k and k x n k must be the same on both
+//create board of m x n
+// row of 1st X col of 2nd
+//ith row of mat 2 and jth column mat2

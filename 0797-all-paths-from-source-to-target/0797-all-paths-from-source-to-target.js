bbd6b93791b57b38c67d36a617a4f148effbffe3
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-04-04 13:58:26 -0400

    Time: 105 ms (51.23%), Space: 51.2 MB (41.73%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.js
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.js
@@ -3,15 +3,16 @@
  * @return {number[][]}
  */
 var allPathsSourceTarget = function(graph) {
-    let paths=[];
-    const DFS = (path,index)=> {
-        if(path[path.length-1]===graph.length-1) {
-            paths.push(path);
-        }
-       for(let i=0;i<graph[index].length;i++){
-           DFS([...path,graph[index][i]], graph[index][i])
-       }
-    }
-    DFS([0],0)
+  let stack=[[0]]
+  let paths=[];
+  while(stack.length) {
+      let path = stack.pop();
+      if(path[path.length-1]===graph.length-1)paths.push(path);
+      for(const node of graph[path[path.length-1]]){
+          stack.push([...path,node])
+      }
+  }
     return paths
 };
+//time complexityO(2^n), possible paths from source to target
+//space complexity O(n^2)

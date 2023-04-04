5def7df9d1baa24223ed0a0952c22517211d04c9
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-04-04 14:00:58 -0400

    Time: 104 ms (55.11%), Space: 51 MB (47.18%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.js
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.js
@@ -3,16 +3,19 @@
  * @return {number[][]}
  */
 var allPathsSourceTarget = function(graph) {
-  let stack=[[0]]
-  let paths=[];
-  while(stack.length) {
-      let path = stack.pop();
-      if(path[path.length-1]===graph.length-1)paths.push(path);
-      for(const node of graph[path[path.length-1]]){
-          stack.push([...path,node])
-      }
-  }
+ let stack = [[0]];
+    let paths=[]
+    while(stack.length){
+        let path = stack.pop();
+        if(path[path.length-1]===graph.length-1){
+            paths.push(path)
+        }
+        for(const node of graph[path[path.length-1]]){
+            stack.push([...path,node]);
+        }
+    }
     return paths
-};
-//time complexityO(2^n), possible paths from source to target
-//space complexity O(n^2)
+    };
+//time complexityO(2^n), possible paths from source to target, # of recursive calls
+//space complexity O(n^2), less than the amount of recursive calls
+//space with stack is O(2^n)

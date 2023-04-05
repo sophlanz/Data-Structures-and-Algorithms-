a54bdd0f45a0e7dd2a4d9b243d457c2e8d066f2b
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-04-05 08:15:57 -0400

    Time: 66 ms (27.77%), Space: 42.4 MB (15.25%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.js
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.js
@@ -10,11 +10,11 @@
  * @return {ListNode}
  */
 var middleNode = function(head) {
-  let slow = head;
+    let slow = head;
     let fast = head;
-    while(fast && fast.next) {
+    while(fast && fast.next){
         slow = slow.next;
         fast = fast.next.next;
     }
-    return slow;
+    return slow
 };

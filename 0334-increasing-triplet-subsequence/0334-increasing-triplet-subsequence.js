a83ad83d3955abb395e7b44b46373a0ce2d02725
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-04-05 10:03:30 -0400

    Time: 98 ms (28.40%), Space: 64.6 MB (8.80%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.js
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.js
@@ -3,13 +3,12 @@
  * @return {boolean}
  */
 var increasingTriplet = function(nums) {
-   let small = Math.max(...nums), mid = Math.max(...nums);
-        for (const n of nums) {
-            if (n <= small) small = n;  // update small if n is smaller than both
-            else if (n <= mid) { mid = n; } // update big only if greater than small but smaller than big
-            else return true; // return if you find a number bigger than both
-        }
-        return false;
- 
+    let small = Math.max(...nums);
+    let mid = Math.max(...nums);
+    for(const n of nums){
+        if (n<=small) small=n;
+        else if(n<=mid) {mid =n}
+        else return true
+    }
+    return false
 };
-

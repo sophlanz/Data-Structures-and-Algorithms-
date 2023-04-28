f24d2dccebf32deb2b8b3e95fb1b15b72099405e
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-04-28 12:53:48 -0400

    Time: 51 ms (97.20%), Space: 44.2 MB (37.34%) - LeetHub

--- 0078-subsets/0078-subsets.js
+++ 0078-subsets/0078-subsets.js
@@ -3,23 +3,17 @@
  * @return {number[][]}
  */
 var subsets = function(nums) {
-    const subsets = [];
-  // start by adding the empty subset
-  subsets.push([]);
-  for (let i = 0; i < nums.length; i++) {
-    let currentNumber = nums[i];
-    // we will take all existing subsets and insert the current number in them to create 
-    // new subsets
-    const n = subsets.length;
-    for (j = 0; j < n; j++) {
-      // create a new subset from existing subset and insert the current element to it
-      const set1 = [...subsets[j]];
-      set1.push(currentNumber);
-      subsets.push(set1);
+let subsets=[];
+    subsets.push([]);
+    for(let i=0;i<nums.length;i++){
+        let currNum=nums[i];
+        let length = subsets.length
+        for(let j=0;j<length;j++){
+            const subset=subsets[j].slice(0);
+            subset.push(currNum);
+            subsets.push(subset);
+        }
     }
-  }
-
-  return subsets;
- 
-    
+    return subsets
 };
+ 

ab96b331f2cdcb15ebd50a6d03c1bd6a96cc8cf6
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-04-28 13:02:52 -0400

    Time: 61 ms (70.36%), Space: 44.8 MB (7.41%) - LeetHub

--- 0078-subsets/0078-subsets.js
+++ 0078-subsets/0078-subsets.js
@@ -3,17 +3,13 @@
  * @return {number[][]}
  */
 var subsets = function(nums) {
-let subsets=[];
-    subsets.push([]);
-    for(let i=0;i<nums.length;i++){
-        let currNum=nums[i];
-        let length = subsets.length
-        for(let j=0;j<length;j++){
-            const subset=subsets[j].slice(0);
-            subset.push(currNum);
-            subsets.push(subset);
-        }
-    }
-    return subsets
+  let subsets=[]
+  const DFS=(subset,index)=> {
+      subsets.push(subset);
+      for(let i=index;i<nums.length;i++){
+          DFS([...subset,nums[i]], i+1)
+      }
+  }
+  DFS([],0)
+  return subsets
 };
- 

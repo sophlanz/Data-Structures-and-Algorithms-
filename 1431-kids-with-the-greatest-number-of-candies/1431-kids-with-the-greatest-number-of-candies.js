6b8f18dc30ddffb29462fc8fb0af41d6af66da42
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-05-02 15:48:58 -0400

    Time: 58 ms (75.97%), Space: 42.3 MB (63.23%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+var kidsWithCandies = function(candies, extraCandies) {
+let max = Math.max(...candies);
+    let res=[];
+   for(let i=0;i<candies.length;i++){
+       if(candies[i]+extraCandies >= max){
+           res[i]=true;
+       }else{
+           res[i]=false;
+       }
+   }
+    return res;
+};

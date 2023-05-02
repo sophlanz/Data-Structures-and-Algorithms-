8c5fb4963ada86948eac8892de90100f317eab38
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-05-02 14:22:26 -0400

    Time: 137 ms (5.56%), Space: 46.9 MB (5.23%) - LeetHub

---
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} columnTitle
+ * @return {number}
+ */
+var titleToNumber = function(s) {
+ let total = 0
+  for (let i = 0; i < s.length; i++) {
+      console.log(s.charCodeAt(i))
+      console.log("A".charCodeAt(0) + 1)
+    total = total * 26 + s.charCodeAt(i) - "A".charCodeAt(0) + 1
+  }
+  return total
+};

5c148ff537096dd23ddc368e9c4d2f4bbede7003
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-05-02 17:17:35 -0400

    Time: 85 ms (46.66%), Space: 48.2 MB (56.85%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.js
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.js
@@ -4,7 +4,6 @@
  */
 var validPalindrome = function(s) {
     const isPalindrome = (p1,p2)=>{
- 
         while(p1<p2){
             if(s[p1]!== s[p2])return false;
             p1++
@@ -15,8 +14,6 @@ var validPalindrome = function(s) {
     let l=0,r=s.length-1;
     while(l<r){
         if(s[l]!== s[r]){
-            console.log(isPalindrome(l+1,r))
-            console.log(isPalindrome(l,r-1))
             if(isPalindrome(l+1,r) || isPalindrome(l,r-1))return true
             else return false
         }

b9e801f6176377b7c90db7dbc38b64a453011b0b
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-05-02 14:23:24 -0400

    Time: 72 ms (54.03%), Space: 43.9 MB (50.65%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.js
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.js
@@ -5,8 +5,6 @@
 var titleToNumber = function(s) {
  let total = 0
   for (let i = 0; i < s.length; i++) {
-      console.log(s.charCodeAt(i))
-      console.log("A".charCodeAt(0) + 1)
     total = total * 26 + s.charCodeAt(i) - 64
   }
   return total

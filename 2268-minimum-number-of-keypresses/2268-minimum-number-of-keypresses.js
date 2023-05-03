4df3f235d0a403d934fff885efa0c0e840418743
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-05-03 14:18:24 -0400

    Time: 101 ms (25.00%), Space: 45.4 MB (60.00%) - LeetHub

--- 2268-minimum-number-of-keypresses/2268-minimum-number-of-keypresses.js
+++ 2268-minimum-number-of-keypresses/2268-minimum-number-of-keypresses.js
@@ -3,10 +3,7 @@
  * @return {number}
  */
 var minimumKeypresses = function(s) {
-    //19
-    //first 9 distinct chars press once
-    //next distinct 9 chars press twice, count +=2. if it's already in the map count += 1
-    //next distinct 9 chars press 3 times count += 3. already in map, count +=1
+ //we make a freqMap, so we can leverage mapping the 9 most frequest integers first;
 let count=0;
     let freqMap={};
     for(let i=0;i<s.length;i++){

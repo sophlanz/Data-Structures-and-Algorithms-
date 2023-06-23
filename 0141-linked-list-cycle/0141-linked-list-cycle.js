7b6b6a3de9d474bc28b1efbce82f383c10cab524
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-06-23 09:50:56 -0400

    Time: 69 ms (78.24%), Space: 44.4 MB (96.28%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.js
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -13,7 +13,7 @@
 var hasCycle = function(head) {
     let slow = head;
     let fast = head;
-    while( fast && fast.next && slow.next){
+    while( fast && fast.next){
         fast = fast.next.next;
         slow = slow.next;
         if(slow === fast)return true

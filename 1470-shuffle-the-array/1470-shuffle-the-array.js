56c14cfe6c7a910645ebfbc66489e1113b8aff41
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-07-04 09:13:09 -0400

    Time: 75 ms (26.96%), Space: 44.2 MB (77.22%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.js
+++ 1470-shuffle-the-array/1470-shuffle-the-array.js
@@ -5,9 +5,9 @@
  */
 var shuffle = function(nums, n) {
     let res=[];
-    for(let i=0;i<n;i++) {
+    for(let i=0;i<n;i++){
         res.push(nums[i])
         res.push(nums[i+n])
-    }
+}
     return res;
 };

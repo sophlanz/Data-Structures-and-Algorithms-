8755a48350db7a40b378dcb4b56939e450775155
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-07-04 12:35:07 -0400

    Time: 93 ms (38.04%), Space: 50.2 MB (45.36%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.js
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.js
@@ -12,16 +12,11 @@
  * @return {ListNode}
  */
 var getIntersectionNode = function(headA, headB) {
-    let set = new Set();
-    let curr= headA;
-    while(curr){
-        set.add(curr);
-        curr = curr.next;
+let currA=headA
+let currB=headB;
+    while(currA !== currB){
+        currA = currA ? currA.next : headB  ;
+        currB = currB ? currB.next : headA ; 
     }
-   curr=headB;
-    while(curr){
-        if(set.has(curr)) return curr
-        curr=curr.next
-    }
-    return null;
+    return currA
 };

06131896cd512e4919392783c3d6b869ee28c1ce
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-07-05 10:38:30 -0400

    Time: 64 ms (20.09%), Space: 42.2 MB (9.61%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.js
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.js
@@ -1,20 +1,20 @@
-var Queue = function() {
-    this.queue=[];
+var MyQueue = function(){
+    this.queue =[];
 }
-Queue.prototype.enqueue = function(val) {
-   return this.queue.push(val);
+MyQueue.prototype.enqueue = function(val){
+   return this.queue.push(val)
 }
-Queue.prototype.dequeue = function() {
-    return this.queue.shift();
+MyQueue.prototype.dequeue = function(val){
+    return this.queue.shift()
 }
-Queue.prototype.size = function() {
-    return this.queue.length;
-}
-Queue.prototype.isEmpty = function() {
+MyQueue.prototype.isEmpty = function(){
     return this.queue.length === 0;
 }
+MyQueue.prototype.getSize = function(){
+    return this.queue.length;
+}
 var MyStack = function() {
-    this.stack = new Queue();
+    this.stack = new MyQueue();
 };
 
 /** 
@@ -22,15 +22,16 @@ var MyStack = function() {
  * @return {void}
  */
 MyStack.prototype.push = function(x) {
-    this.stack.enqueue(x)
+    this.stack.enqueue(x);
 };
 
 /**
  * @return {number}
  */
 MyStack.prototype.pop = function() {
-    for(let i=0;i<this.stack.size()-1;i++) {
-       this.stack.enqueue(this.stack.dequeue()) ;
+    let size = this.stack.getSize();
+    for(let i=0;i<size-1;i++){
+       this.stack.enqueue(this.stack.dequeue()) 
     }
     return this.stack.dequeue();
 };
@@ -39,7 +40,7 @@ MyStack.prototype.pop = function() {
  * @return {number}
  */
 MyStack.prototype.top = function() {
-    return this.stack.queue[this.stack.size()-1];
+    return this.stack.queue[this.stack.getSize()-1]
 };
 
 /**

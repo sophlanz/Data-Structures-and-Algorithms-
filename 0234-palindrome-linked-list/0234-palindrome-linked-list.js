4e0cb02abce93bd29f8272b8c15831fcb816020c
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-07-05 08:50:38 -0400

    Time: 136 ms (66.37%), Space: 78.9 MB (43.79%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.js
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.js
@@ -10,13 +10,25 @@
  * @return {boolean}
  */
 var isPalindrome = function(head) {
-    
-    let curr = head;
-    let vals=[];
+    let curr=head;
+    let nums=[];
     while(curr){
-        vals.push(curr.val);
-        curr = curr.next
+       nums.push(curr.val);
+        curr=curr.next;
     }
-   
-    return vals.toString() === vals.reverse().toString();
-};
+ 
+    const isPalindrome = (arr) => {
+        let l = 0;
+        let r = arr.length-1;
+        while(l <= r){
+            if(arr[l] !== arr[r]){
+                 return false;
+            }else{
+                l++
+                r--
+            }
+        }
+        return true;
+    }
+  return isPalindrome(nums)
+}

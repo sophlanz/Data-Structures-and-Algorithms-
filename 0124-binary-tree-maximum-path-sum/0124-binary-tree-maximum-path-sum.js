28f12f665d17e7b8041883277492cc977a72045f
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-07-07 15:29:18 -0400

    Time: 94 ms (11.52%), Space: 51.3 MB (81.50%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.js
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.js
@@ -11,26 +11,17 @@
  * @return {number}
  */
 var maxPathSum = function(root) {
-    	let max = -Infinity;
-
-	const findSums = (node) => {
-		// Base case / hit a null
-		if (!node) return 0;
-
-		let left = findSums(node.left),
-			right = findSums(node.right),
-			allSum = left + right + node.val,
-			leftNodeSum = left + node.val,
-			rightNodeSum = right + node.val;
-
-		// Max is all possible combinations
-		max = Math.max(max, node.val, allSum, leftNodeSum, rightNodeSum);
-		
-		// Return the MAX path, which can be node.val, left + node.val, or right + node.val
-		return Math.max(leftNodeSum, rightNodeSum, node.val);
-	};
-
-	findSums(root);
-
-	return max;
+    let max = -Infinity;
+    const findMax = (node)=>{
+        if(node === null) return 0;
+        let left = findMax(node.left);
+        let right = findMax(node.right);
+        let allSum = left + right + node.val;
+        let leftSum = left + node.val;
+        let rightSum = right+node.val;
+        max = Math.max(max,allSum,leftSum,rightSum, node.val);
+        return Math.max(node.val,leftSum,rightSum)
+    }
+    findMax(root);
+    return max
 };

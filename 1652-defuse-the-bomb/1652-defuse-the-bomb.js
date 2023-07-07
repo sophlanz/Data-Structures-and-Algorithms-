c2786b2a0183bcd96239913d9462c02ac234cb3b
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-07-07 11:01:31 -0400

    Time: 68 ms (40.30%), Space: 41.2 MB (100.00%) - LeetHub

--- 1652-defuse-the-bomb/1652-defuse-the-bomb.js
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.js
@@ -5,24 +5,23 @@
  */
 var decrypt = function(code, k) {
     if(k===0){
-        return Array.from(code).fill(0);
-    };
-    //k>0
-    let start=1;
-    let end = k;
-    if(k<0){
-        start= code.length+k;
-        end=code.length-1
+        return Array(code.length).fill(0);
     }
-    let res=[];
-    let sum=0
+    let start = 1;
+    let end=k;
+    if(k<0){
+        end=code.length-1;
+        start=code.length+k
+    }
+    let sum=0;
     for(let i=start;i<=end;i++){
         sum+= code[i];
     }
-   for(let i=0;i<code.length;i++){
-        res[i]=sum;
-       sum -= code[start++ % code.length]
-       sum += code[++end % code.length]
-   }
-    return res;
+    let res=[];
+    for(let i=0;i<code.length;i++){
+        res.push(sum);
+        sum -= code[start++%code.length]
+        sum += code[++end%code.length]
+    }
+    return res
 };

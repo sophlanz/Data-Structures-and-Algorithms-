31b5eba106288a8dceb0b8ef9a2d4357547b9883
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-07-08 12:31:43 -0400

    Time: 91 ms (18.33%), Space: 45.8 MB (38.25%) - LeetHub

--- 0566-reshape-the-matrix/0566-reshape-the-matrix.js
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.js
@@ -5,18 +5,13 @@
  * @return {number[][]}
  */
 var matrixReshape = function(mat, r, c) {
-    let all = [];
+    let all = mat.flat();
     let rows=mat.length;
     let cols = mat[0].length;
-    for(let i=0;i<rows;i++){
-        for(let j=0;j<cols;j++){
-            all.push(mat[i][j]);
-        }
-    }
+ if(rows * cols !== r* c) return mat
     let res=Array.from({length:r},()=> Array.from({length:c}))
     for(let i=0;i<r;i++){
         for(let j=0;j<c;j++){
-             if(!all.length) return mat;
             let val = all.shift();
             res[i][j] = val
         }

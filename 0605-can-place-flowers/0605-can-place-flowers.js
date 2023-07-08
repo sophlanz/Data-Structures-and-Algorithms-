48d72ae1b5b3bec38a779c021781fc7950f8d183
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-07-08 09:51:07 -0400

    Time: 60 ms (83.72%), Space: 44.6 MB (37.73%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.js
+++ 0605-can-place-flowers/0605-can-place-flowers.js
@@ -4,19 +4,13 @@
  * @return {boolean}
  */
 var canPlaceFlowers = function(flowerbed, n) {
-    let map = {};
-   for(let i=0;i<flowerbed.length;i++){
-       if(flowerbed[i]===1){
-           map[i] = 1;
-       }
-   }
 
     for(let i=0;i<flowerbed.length;i++){
-        if(flowerbed[i]===0){
-            if(!map[i-1] && !map[i+1]){
-                n--
-                map[i] =1;
-            }
+        let prev = flowerbed[i-1];
+        let next = flowerbed[i+1];
+        if(flowerbed[i] !== 1 && prev !== 1 && next !==1 ) {
+            n--
+            flowerbed[i]=1;
         }
     }
     return n<=0

f988dc3896c5692a0a8ce2b782212bd5917ac542
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-07-08 09:11:21 -0400

    Time: 61 ms (50.73%), Space: 41.8 MB (77.15%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.js
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.js
@@ -3,16 +3,17 @@
  * @return {number[]}
  */
 var getRow = function(rowIndex) {
-     if (rowIndex === 0) {
-        return [1]
-    }
-    if (rowIndex > 0) {
-        let a = [1]
-        let p = getRow(rowIndex - 1)
-        for (let i = 1; i < rowIndex; i++) {
-            a[i] = p[i - 1] + p[i]
-        }
-        a.push(1)
-        return a
-    }
+   if(rowIndex===0){
+       return [1]
+   }
+   if(rowIndex>0){
+       let prevRow = getRow(rowIndex-1);
+       let res=[1];
+       for(let i=1;i<rowIndex;i++){
+           res[i]= prevRow[i-1] + prevRow[i];
+       }
+       res.push(1)
+       return res;
+   }
+ 
 };

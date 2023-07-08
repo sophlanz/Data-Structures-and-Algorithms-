45465bc894568c9e89883537380271b1591c8e69
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-07-08 16:27:54 -0400

    Time: 116 ms (29.69%), Space: 50 MB (37.30%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.js
+++ 0056-merge-intervals/0056-merge-intervals.js
@@ -3,22 +3,19 @@
  * @return {number[][]}
  */
 var merge = function(intervals) {
-    intervals.sort((a,b)=> a[0]-b[0]);
+    intervals.sort((a,b)=> a[0]-b[0])
     let start = intervals[0][0];
-    let end=intervals[0][1];
-    let merged=[];
+    let end = intervals[0][1];
+    let res=[];
     for(let i=1;i<intervals.length;i++){
-     const interval=intervals[i];
-        if(interval[0]<= end){
-            end = Math.max(end, interval[1]);
-        }else{
-            merged.push([start,end]);
-            end = interval[1];
-            start = interval[0];
+        let interval = intervals[i];
+        if(interval[0]<=end)end=Math.max(interval[1],end);
+        else{
+            res.push([start,end]);
+            start=interval[0];
+            end=interval[1]
         }
-    } 
-    merged.push([start,end]);
-    return merged;
+    }
+    res.push([start,end]);
+    return res
 };
-//Time Complexity O(nlogn) sorting;
-//Space Complexity O(n )size of intervals array

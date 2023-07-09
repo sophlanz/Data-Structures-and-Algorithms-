8be905c0544baa2cf44f74786d13c43c2b59105e
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-07-09 13:38:17 -0400

    Time: 49 ms (94.53%), Space: 42.3 MB (27.72%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -4,12 +4,12 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
-   if(needle.length >haystack.length) return -1;
-    if(needle === haystack || !needle) return 0;
-    let start=0
+    if(needle.length >haystack.length)return -1;
+    if(haystack === needle) return 0;
+ let start=0;
     for(let end=needle.length;end<=haystack.length;end++) {
-        let string = haystack.slice(start,end);
-        if(string===needle) return start
+        let possible = haystack.slice(start,end);
+        if(possible === needle) return start
         start++
     }
     return -1;

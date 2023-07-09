a6f67bd4e31ae9558febf06144ef3c510dd221f6
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-07-09 12:59:21 -0400

    Time: 72 ms (87.53%), Space: 41.9 MB (98.46%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -5,15 +5,15 @@
  */
 var isAnagram = function(s, t) {
     let freqMap={};
-    let length1=s.length;
-    let length2=t.length
-    let length = Math.max(length1,length2)
+    let length = Math.max(s.length, t.length);
     for(let i=0;i<length;i++){
-        freqMap[s[i]]? freqMap[s[i]]++ : freqMap[s[i]]=1;
-        freqMap[t[i]]? freqMap[t[i]]-- : freqMap[t[i]]=-1;
+        let sChar = s[i];
+        sChar in freqMap ? freqMap[sChar]++ : freqMap[sChar] = 1;
+        let tChar=t[i];
+        tChar in freqMap ? freqMap[tChar]-- : freqMap[tChar]=-1;
     }
-  for(const char in freqMap){
-      if(freqMap[char]>0 || freqMap[char]<0) return false
-  }
+    for(const char in freqMap){
+        if(freqMap[char]!== 0) return false
+    }
     return true;
 };

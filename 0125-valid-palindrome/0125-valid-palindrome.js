1c05cbc5f5586d2ed16c6b42362d9abfc7c09fd6
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-07-09 13:27:37 -0400

    Time: 80 ms (34.45%), Space: 44.8 MB (69.97%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    s=s.replace(/\s+/g,'').replace(/[^a-zA-Z0-9]/g,'').toLowerCase()
+console.log(s)
+    let l=0,r=s.length-1;
+    while(l<r){
+        if(s[l] == s[r]){
+            r--
+            l++
+        }else return false
+    }
+    return true
+};

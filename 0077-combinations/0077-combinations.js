0c73adcf7667fb2c2b415c9cf8a17bd9819860aa
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-07-13 16:05:24 -0400

    Time: 237 ms (5.90%), Space: 61 MB (5.21%) - LeetHub

---
+++ 0077-combinations/0077-combinations.js
@@ -0,0 +1,47 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number[][]}
+ */
+var combine = function(n, k) {
+    let nums=[];
+    while(n > 0){
+    nums.unshift(n)
+    n--
+}
+       let res=[];
+    let memo={}
+    if(k===1) {
+        for(const num of nums){
+            res.push([num])
+        }
+        return res;
+    }
+ 
+    const DFS=(arr,rest)=>{
+        if(arr.length===k && !(arr in memo)){
+            res.push(arr)
+            memo[arr]=1
+        }
+    for(let i=0;i<rest.length;i++){
+        DFS([...arr,rest[i]],[...rest.slice(i+1)])
+    }
+        
+    }
+    DFS([],nums)
+    return res
+    
+};
+//     1 2 3 4
+//      1
+//   1 2.  1 3.  1 4
+//res=[1,2] [2,3][3,4] [2,4]
+//[1,2,3,4]i=2
+//[1,2][2,3,4] 
+//[1,3][3,4] 
+//[2,3,4]
+//[2,3][3,4]         i=2
+//[2,4][]
+//[3,4]
+//[3,4]
+

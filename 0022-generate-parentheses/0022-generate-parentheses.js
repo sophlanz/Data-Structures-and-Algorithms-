2d0dbde1cf32aa05b0b070e85dd36208e392d74c
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-07-14 10:04:56 -0400

    Time: 61 ms (60.63%), Space: 43.4 MB (44.45%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.js
+++ 0022-generate-parentheses/0022-generate-parentheses.js
@@ -8,6 +8,7 @@ var generateParenthesis = function(n) {
       if(s.length === n*2)res.push(s);
       if(o<n) DFS(o+1,c,s+"(")
       if(c < o)DFS(o,c+1,s+")")
+      return;
   }
   DFS(1,0,'(')
     return res

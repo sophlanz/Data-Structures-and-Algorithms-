d02a1cc47d55259c0b967d1aba37d45208f359ac
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-07-17 08:52:48 -0400

    Time: 50 ms (91.80%), Space: 41.9 MB (42.97%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.js
+++ 0278-first-bad-version/0278-first-bad-version.js
@@ -18,16 +18,15 @@ var solution = function(isBadVersion) {
      * @return {integer} The first bad version
      */
     return function(n) {
-        let l=1,
-        r=n;
+        let l=1,r=n;
         while(l<r){
             let mid = Math.floor((l+r)/2);
             if(isBadVersion(mid)){
-                r=mid
+                r=mid;
             }else{
-                l=mid+1
-            }
+            l=mid+1
         }
-        return l
     };
-};
+        return r
+}
+}

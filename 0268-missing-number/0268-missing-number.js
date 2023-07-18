b2db519368cacbb41001da8c05677b305d2770a4
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-07-18 09:44:38 -0400

    Time: 90 ms (28.69%), Space: 44.8 MB (25.52%) - LeetHub

--- 0268-missing-number/0268-missing-number.js
+++ 0268-missing-number/0268-missing-number.js
@@ -5,7 +5,7 @@
 var missingNumber = function(nums) {
     nums.sort((a,b)=>a-b);
     if(nums.length === 1 && nums[0] !== 0)return nums[0]-1
-    else if(nums.length ==1 && nums[0]==0 )return 1
+
     for(let i=0;i<nums.length;i++){
         if(nums[i+1]-nums[i]>1) return nums[i] + 1
     }

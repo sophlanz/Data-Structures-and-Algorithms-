b49f55ced6c57c27e212f60879cfe3a12cde14fa
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-07-19 14:18:03 -0400

    Time: 53 ms (94.63%), Space: 42.8 MB (35.16%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -9,20 +9,17 @@ var isValid = function(s) {
      '}':'{',
      ']':'[' 
  }
- for(const char of s){
-     if(stack.length !==0){
-         //if its closing
-         if(map[char]){
-             let prevChar = stack.pop();
-             if(map[char]!==prevChar) return false;
-         }else{
-             //if its opening 
-             stack.push(char)
-         }
-     } else{
-         stack.push(char) 
-     }
-    
- }
+for(const char of s){
+    if(!stack.length){
+        stack.push(char);
+    }else{
+        if(char in map){
+            let firstChar = stack.pop();
+           if( firstChar !== map[char] )return false;
+        }else{
+            stack.push(char)
+        }
+    }
+}
     return stack.length ===0
 };

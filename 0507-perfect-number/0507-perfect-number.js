b9198624167fe10eca6147bdf850f373b3ed8c51
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-07-19 08:27:57 -0400

    Time: 4444 ms (35.98%), Space: 41.9 MB (65.69%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var checkPerfectNumber = function(num) {
+    let x= 1;
+    let divisors = [];
+    while(x< num){
+        if(num%x === 0)divisors.push(x);
+        x++
+    }
+    let sum =0;
+   for(const num of divisors){
+       sum += num;
+   } 
+    return sum === num
+};

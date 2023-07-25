1552dcc356c97b0a702d10c3ead9520b529bdf27
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-07-25 10:00:03 -0400

    Time: 56 ms (72.56%), Space: 42.3 MB (34.15%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.js
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.js
@@ -11,17 +11,17 @@
  * @return {number[]}
  */
 var inorderTraversal = function(root) {
- let stack=[];
- let res=[];
- let curr=root;
+    let res=[];
+    let stack=[];
+    let curr=root;
     while(curr || stack.length){
         if(curr){
-            stack.push(curr);
-            curr=curr.left; 
+          stack.push(curr);
+          curr=curr.left
         }else{
             let node=stack.pop();
             res.push(node.val);
-            curr=node.right;
+            curr=node.right
         }
     }
     return res;

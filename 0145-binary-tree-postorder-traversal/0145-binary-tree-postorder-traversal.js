9a83f5da256c1429033d03a706936711d570295c
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-07-25 09:55:24 -0400

    Time: 64 ms (27.87%), Space: 42 MB (56.04%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.js
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.js
@@ -11,13 +11,22 @@
  * @return {number[]}
  */
 var postorderTraversal = function(root) {
+    if(!root)return []
     let res=[];
-    const DFS=(node)=>{
-        if(!node)return;
-        DFS(node.left);
-        DFS(node.right);
-        res.push(node.val);
+    let stack1=[root];
+    let stack2=[];
+    while(stack1.length){
+        let node = stack1.pop();
+        stack2.push(node.val);
+        if(node.left){
+            stack1.push(node.left);
+        }
+        if(node.right){
+            stack1.push(node.right);
+        }
     }
-    DFS(root)
+    while(stack2.length){
+        res.push(stack2.pop());
+    }
     return res;
 };

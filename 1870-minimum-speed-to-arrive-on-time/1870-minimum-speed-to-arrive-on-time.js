152162c4eb7eeb08aeb9962767d67347e5e65430
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-07-26 13:24:13 -0400

    Time: 202 ms (38.64%), Space: 54.3 MB (34.09%) - LeetHub

---
+++ 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} dist
+ * @param {number} hour
+ * @return {number}
+ */
+var minSpeedOnTime = function(dist, hour) {
+let l=0,r=10000000;
+let min=Infinity
+if(dist.length-1 >= hour )return -1
+    while(l<r){
+        let mid = Math.floor((l+r)/2);
+         totalHours=0
+        for(let i=0;i<dist.length;i++){
+            if(i===dist.length-1){
+                totalHours += (dist[i]/mid)
+            }else{
+                totalHours += Math.ceil(dist[i]/mid);
+            }
+        }
+        if(totalHours === hour)return mid
+        if(totalHours<hour){
+            r=mid
+        }else{
+            l=mid+1
+        }
+    }
+    return r
+};

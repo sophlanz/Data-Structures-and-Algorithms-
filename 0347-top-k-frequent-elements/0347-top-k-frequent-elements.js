5633fbcbf45ad5673815c93fd9c094aa1970040c
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-07-26 15:00:38 -0400

    Time: 81 ms (45.11%), Space: 45.4 MB (51.80%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -4,13 +4,13 @@
  * @return {number[]}
  */
 var topKFrequent = function(nums, k) {
-
     let freqMap={};
-    for(let i=0;i<nums.length;i++){
-        freqMap[nums[i]]?freqMap[nums[i]]++ : freqMap[nums[i]]=1;
+    for(const num of nums){
+        num in freqMap ? freqMap[num]++ : freqMap[num]=1;
     }
+  let sorted= Object.entries(freqMap).sort((a, b) => b[1] - a[1]).map(([key,value]) => key);
+    console.log(sorted)
+    return sorted.slice(0,k)
 
-    Object.values(freqMap).sort((a,b)=> b-a);
-
-    return Object.keys(freqMap).sort((a,b)=> freqMap[b]-freqMap[a]).slice(0,k)
+    
 };

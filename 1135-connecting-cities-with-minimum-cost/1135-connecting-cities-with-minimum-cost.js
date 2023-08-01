7dafb7e9636e075522e1584a03105c0f73451188
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-08-01 10:24:02 -0400

    Time: 220 ms (25.00%), Space: 86.7 MB (25.00%) - LeetHub

--- 1135-connecting-cities-with-minimum-cost/1135-connecting-cities-with-minimum-cost.js
+++ 1135-connecting-cities-with-minimum-cost/1135-connecting-cities-with-minimum-cost.js
@@ -4,24 +4,32 @@
  * @return {number}
  */
 var minimumCost = function(n, connections) {
-	const parent = new Array(n + 1).fill(0).map((el, i) => i);
-	const find = (child)=>{
-        if(child === parent[child])return child
-        else{
-            parent[child]=find(parent[child]);
-            return parent[child];
+ const graph = new Map();
+    for(let i=1;i<=n;i++){
+        graph.set(i,new Map());
+    }
+    for(let [to,from,dist]of connections){
+        
+        if(graph.get(from).has(to)){
+            dist = Math.min(dist, graph.get(from).get(to));
+        }
+        graph.get(from).set(to,dist);
+        graph.get(to).set(from,dist)
+    }
+   let minHeap = new MinPriorityQueue;
+    minHeap.enqueue(1,0);
+    let cost=0
+    let seen = new Set();
+
+    while(minHeap.size()){
+        const { element, priority } = minHeap.dequeue();
+        if(n===seen.size)return cost   
+        if(seen.has(element))continue
+        cost += priority;
+        seen.add(element)
+        for(const [node,distance] of graph.get(element)){
+                 minHeap.enqueue(node,distance)
         }
     }
-	connections.sort((x, y) => x[2] - y[2]);
-	let cost = 0;
-	let edges = 1;
-	for (const [from, to, dist] of connections) {
-		if (find(from) !== find(to)) {
-			parent[find(from)] = find(to);
-			cost += dist;
-			edges++;
-		}
-	}
-	if (edges !== n) return -1;
-	return cost;
+    return -1
 };

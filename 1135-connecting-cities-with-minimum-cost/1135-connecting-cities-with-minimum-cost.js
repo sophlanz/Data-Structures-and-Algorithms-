0cff715f3dd3f96b9da738946832bdee7adf10c2
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-08-01 09:33:29 -0400

    Time: 133 ms (100.00%), Space: 53.2 MB (25.00%) - LeetHub

---
+++ 1135-connecting-cities-with-minimum-cost/1135-connecting-cities-with-minimum-cost.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} n
+ * @param {number[][]} connections
+ * @return {number}
+ */
+var minimumCost = function(n, connections) {
+	const parent = new Array(n + 1).fill(0).map((el, i) => i);
+    console.log(parent)
+	const find = (t) => (t === parent[t] ? t : (parent[t] = find(parent[t])));
+	connections.sort((x, y) => x[2] - y[2]);
+	let cost = 0;
+	let edges = 1;
+	for (const [from, to, dist] of connections) {
+		if (find(from) !== find(to)) {
+			parent[find(from)] = find(to);
+			cost += dist;
+			edges++;
+		}
+	}
+	if (edges !== n) return -1;
+	return cost;
+};

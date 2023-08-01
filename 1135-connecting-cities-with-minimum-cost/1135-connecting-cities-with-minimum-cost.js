85df3287b2db8c2c7ca6652263e7c7bbf6042969
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-08-01 10:29:37 -0400

    Time: 196 ms (25.00%), Space: 87 MB (25.00%) - LeetHub

--- 1135-connecting-cities-with-minimum-cost/1135-connecting-cities-with-minimum-cost.js
+++ 1135-connecting-cities-with-minimum-cost/1135-connecting-cities-with-minimum-cost.js
@@ -4,31 +4,29 @@
  * @return {number}
  */
 var minimumCost = function(n, connections) {
- const graph = new Map();
+    const graph = new Map();
     for(let i=1;i<=n;i++){
         graph.set(i,new Map());
     }
-    for(let [to,from,dist]of connections){
-        
-        if(graph.get(from).has(to)){
-            dist = Math.min(dist, graph.get(from).get(to));
+    for(let [to,from,dist] of connections){
+        if(graph.get(to).has(from)){
+            dist = Math.min(dist, graph.get(to).get(from));
         }
-        graph.get(from).set(to,dist);
-        graph.get(to).set(from,dist)
+        graph.get(to).set(from,dist);
+        graph.get(from).set(to,dist)
     }
-   let minHeap = new MinPriorityQueue;
+    let minHeap = new MinPriorityQueue();
     minHeap.enqueue(1,0);
-    let cost=0
-    let seen = new Set();
-
+    let cost=0;
+    let seen= new Set();
     while(minHeap.size()){
-        const { element, priority } = minHeap.dequeue();
-        if(n===seen.size)return cost   
-        if(seen.has(element))continue
+        const {element,priority}=minHeap.dequeue();
+        if(seen.size ===n)return cost
+        if(seen.has(element))continue;
         cost += priority;
-        seen.add(element)
+        seen.add(element);
         for(const [node,distance] of graph.get(element)){
-                 minHeap.enqueue(node,distance)
+            minHeap.enqueue(node,distance)
         }
     }
     return -1

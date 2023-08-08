16faf90f860a99dead06b0bf9d72ba284ae45f54
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-08-08 11:04:13 -0400

    Time: 51 ms (87.31%), Space: 41.3 MB (100.00%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.js
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.js
@@ -3,19 +3,26 @@
  * @param {string} order
  * @return {boolean}
  */
-var isAlienSorted = function(words, alphabet) {
+var isAlienSorted = function(words, order) {
   
-let map = {};
-    for(let i=0;i<alphabet.length;i++){
-        map[alphabet[i]] = i+1;
+let letterOrder = new Array(order.length).fill(0);
+    for(let i=0;i<order.length;i++){
+        letterOrder[order.charCodeAt(i)-97] = i
     }
-    for(let i=0;i<words.length-1;i++) {
-        for(let j=0;j<words[i].length;j++) {
-            if(j>= words[i+1].length) return false;
-            if(words[i][j] === words[i+1][j])continue;
-            if(map[words[i][j]] > map[words[i+1][j]]) return false;
-           break;
-        }
+        for(let i=0;i<words.length-1;i++){
+            if(words[i].startsWith(words[i+1]) && words[i].length > words[i+1].length){
+                return false
+            }
+       for(let j=0;j<words[i].length;j++){
+           if(words[i].charAt(j) !== words[i+1].charAt(j)){
+               if(letterOrder[words[i].charCodeAt(j)-97] > letterOrder[words[i+1].charCodeAt(j)-97]){
+                   return false;
+               }else{
+                   break;
+               }
+           }
+       }
     }
-    return true;
+    return true
+
 };

374e4ded4f39cd48995656e77088d89f713556fa
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-08-08 12:34:34 -0400

    Time: 158 ms (88.01%), Space: 69 MB (16.05%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.js
@@ -0,0 +1,55 @@
+/**
+ * @param {string} beginWord
+ * @param {string} endWord
+ * @param {string[]} wordList
+ * @return {number}
+ */
+var ladderLength = function(beginWord, endWord, wordList) {
+    const adjList = new Map();
+    if (!wordList.includes(endWord)) {
+        return 0;
+    }
+    for (const word of wordList) {
+        const wordChars = word.split('');
+        for (let i = 0; i < wordChars.length; i++) {
+            const savedChar = wordChars[i];
+            wordChars[i] = '*';
+            const listOfWords = adjList.get(wordChars.join('')) || [];
+            listOfWords.push(word);
+            adjList.set(wordChars.join(''), listOfWords);
+            wordChars[i] = savedChar;
+        }
+    }
+    const set = new Set();
+    const queue = [];
+    queue.push(beginWord);
+    set.add(beginWord);
+
+    let reVal = 1;
+    while (queue.length > 0) {
+        let levelSize = queue.length;
+        while (levelSize > 0) {
+            const word = queue.shift();
+            const wordChars = word.split('');
+            for (let i = 0; i < wordChars.length; i++) {
+                const savedChar = wordChars[i];
+                wordChars[i] = '*';
+                const words = adjList.get(wordChars.join('')) || [];
+                for (const wordToAdd of words) {
+                    if (wordToAdd === endWord) {
+                        return reVal + 1;
+                    }
+                    if (!set.has(wordToAdd)) {
+                        queue.push(wordToAdd);
+                        set.add(wordToAdd);
+                    }
+                }
+                wordChars[i] = savedChar;
+            }
+            levelSize--;
+        }
+        reVal += 1;
+    }
+    return 0;
+
+};

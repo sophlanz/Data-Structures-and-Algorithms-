6766c4db1f5cdf8709deef5cb6949a2d3f218cdf
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-08-09 10:48:22 -0400

    Time: 250 ms (58.65%), Space: 70.6 MB (13.52%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.js
+++ 0127-word-ladder/0127-word-ladder.js
@@ -5,47 +5,43 @@
  * @return {number}
  */
 var ladderLength = function(beginWord, endWord, wordList) {
-    const adjList = new Map();
-    if(!wordList.includes(endWord)){
-        return 0;
-    }
+const adjList = new Map();
     for(const word of wordList){
         for(let i=0;i<word.length;i++){
-            let mutatedWord=word.split('');
-            mutatedWord[i]='*'
-            let matchList = adjList.get(mutatedWord.join('')) || [];
-            matchList.push(word);
-            adjList.set(mutatedWord.join(''),matchList)
+             let mutatedWord = word.split('');
+            mutatedWord[i]="*";
+            if(!adjList.has(mutatedWord.join(''))){
+                adjList.set(mutatedWord.join(''),[word]);
+            }else{
+                let oldList = adjList.get(mutatedWord.join(''));
+                oldList.push(word);
+                adjList.set(mutatedWord.join(''),oldList)
+            }
         } 
     }
-    let queue=[beginWord]
-    let seen=new Set();
-    seen.add(beginWord);
+    let queue=[beginWord];
     let count=1;
+    let seen=new Set([beginWord]);
     while(queue.length){
-        console.log(queue)
-         let levelSize = queue.length;
+        let levelSize = queue.length;
         for(let i=0;i<levelSize;i++){
             let word = queue.shift();
             for(let j=0;j<word.length;j++){
-                let mutatedWord = word.split('');
+                let mutatedWord=word.split('');
                 mutatedWord[j]='*';
                 if(adjList.get(mutatedWord.join(''))){
-                    for(const wordToCheck of adjList.get(mutatedWord.join(''))){
-                        if(wordToCheck === endWord){
-                            return count+1
-                        }
-                        if(!seen.has(wordToCheck)){
-                            seen.add(wordToCheck);
-                            queue.push(wordToCheck)
-                        }
+                      for(const child of adjList.get(mutatedWord.join(''))){
+                    if(child === endWord)return count+1
+                    if(!seen.has(child)){
+                        queue.push(child);
+                        seen.add(child);
                     }
                 }
+                }
+              
             }
         }
         count++
-            
-      
     }
     return 0
 };

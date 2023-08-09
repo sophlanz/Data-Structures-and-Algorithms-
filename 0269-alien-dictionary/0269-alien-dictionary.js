2b22d7ff6bc42740c28a633262989fe58f1e905d
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-08-09 09:46:41 -0400

    Time: 126 ms (5.19%), Space: 51.1 MB (5.19%) - LeetHub

--- 0269-alien-dictionary/0269-alien-dictionary.js
+++ 0269-alien-dictionary/0269-alien-dictionary.js
@@ -9,45 +9,47 @@ var alienOrder = function(words) {
             if(!adj.get(letter)){
                    adj.set(letter, new Set());
             }
-         
         }
     }
+
     for (let i = 0; i < words.length - 1; i++) {
         if (words[i].startsWith(words[i + 1]) && words[i].length !== words[i + 1].length) {
             return "";
         }
         for (let j = 0; j < words[i].length; j++) {
             if (words[i].charAt(j) !== words[i + 1].charAt(j)) {
-                const children = adj.get(words[i + 1].charAt(j));
-                children.add(words[i].charAt(j));
+                //map its parent
+                const parents = adj.get(words[i + 1].charAt(j));
+                parents.add(words[i].charAt(j));
                 break;
             }
         }
     }
     const processed = new Map();
     const res = [];
-
     for (const child of adj.keys()) {
         if (!topSort(child, adj, processed, res)) {
             return "";
         }
     }
-
     return res.join("");
 }
-function topSort(currentNode, adj, processed, res) {
-    if (processed.has(currentNode)) {
-        return processed.get(currentNode);
-    }
-    processed.set(currentNode, false);
 
+function topSort(currentNode, adj, fullyExplored, res) {
+    console.log({res})
+    console.log({currentNode})
+    if (fullyExplored.has(currentNode)) {
+        return fullyExplored.get(currentNode);
+    }
+    fullyExplored.set(currentNode, false);
     for (const child of adj.get(currentNode)) {
-        if (!topSort(child, adj, processed, res)) {
+        if (!topSort(child, adj, fullyExplored, res)) {
             return false;
         }
     }
 
-    processed.set(currentNode, true);
+    fullyExplored.set(currentNode, true);
     res.push(currentNode);
     return true;
+ 
 };

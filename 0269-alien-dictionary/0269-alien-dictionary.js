b889edeab07cc0d14ddd716fc5f89c9953d6060b
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-08-09 10:17:37 -0400

    Time: 67 ms (83.12%), Space: 45.4 MB (73.38%) - LeetHub

--- 0269-alien-dictionary/0269-alien-dictionary.js
+++ 0269-alien-dictionary/0269-alien-dictionary.js
@@ -3,53 +3,47 @@
  * @return {string}
  */
 var alienOrder = function(words) {
-       const adj = new Map();
-    for (const word of words) {
-        for (const letter of word) {
-            if(!adj.get(letter)){
-                   adj.set(letter, new Set());
+    let res=[];
+  
+    let adjList = new Map();
+    for(const word of words){
+      for(const letter of word){
+          if(!adjList.get(letter)){
+              adjList.set(letter, new Set([]))
+          }
+      }
+    }
+    for(let i=0;i<words.length-1;i++){
+          if(words[i].startsWith(words[i+1]) && words[i].length !== words[i+1].length){
+              return '';
+          }
+        for(let j=0;j<words[i].length;j++){
+            if( words[i].charAt(j) !== words[i+1].charAt(j)){
+                let parents=adjList.get(words[i+1][j]);
+                parents.add(words[i][j]);
+                break
             }
         }
     }
+    let fullyExplored = new Map();
+      const topSort = (child)=>{
+            if(fullyExplored.has(child)){
+                return fullyExplored.get(child)
+            };
+          fullyExplored.set(child,false);
+          for(const parent of adjList.get(child)){
+               if(!topSort(parent))return false;
+          }
+          fullyExplored.set(child,true);
+          res.push(child);
+          return true;
 
-    for (let i = 0; i < words.length - 1; i++) {
-        if (words[i].startsWith(words[i + 1]) && words[i].length !== words[i + 1].length) {
-            return "";
-        }
-        for (let j = 0; j < words[i].length; j++) {
-            if (words[i].charAt(j) !== words[i + 1].charAt(j)) {
-                //map its parent
-                const parents = adj.get(words[i + 1].charAt(j));
-                parents.add(words[i].charAt(j));
-                break;
-            }
-        }
     }
-    const processed = new Map();
-    const res = [];
-    for (const child of adj.keys()) {
-        if (!topSort(child, adj, processed, res)) {
-            return "";
-        }
-    }
-    return res.join("");
-}
+    for(const letter of adjList.keys()){
 
-function topSort(currentNode, adj, fullyExplored, res) {
-    console.log({res})
-    console.log({currentNode})
-    if (fullyExplored.has(currentNode)) {
-        return fullyExplored.get(currentNode);
-    }
-    fullyExplored.set(currentNode, false);
-    for (const child of adj.get(currentNode)) {
-        if (!topSort(child, adj, fullyExplored, res)) {
-            return false;
+        if(!topSort(letter)){
+            return ''
         }
     }
-
-    fullyExplored.set(currentNode, true);
-    res.push(currentNode);
-    return true;
- 
+    return res.join('')
 };

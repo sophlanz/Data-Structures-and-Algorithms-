b5a1e146fad6f9b528f71cbdf46f3b7a9262d6bf
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-08-09 10:35:42 -0400

    Time: 73 ms (10.66%), Space: 45.9 MB (5.33%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.js
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.js
@@ -4,25 +4,23 @@
  * @return {boolean}
  */
 var isAlienSorted = function(words, order) {
-  
-let letterOrder = new Array(order.length).fill(0);
+    let letterOrder = new Array(order.length).fill(0);
     for(let i=0;i<order.length;i++){
-        letterOrder[order.charCodeAt(i)-97] = i
+        letterOrder[order.charCodeAt(i)-97]=i
     }
     console.log(letterOrder)
-        for(let i=0;i<words.length-1;i++){
-            if(words[i].startsWith(words[i+1]) && words[i].length !== words[i+1].length){
-                return false
+    for(let i=0;i<words.length-1;i++){
+        if(words[i].startsWith(words[i+1]) && words[i+1].length !== words[i].length){
+            return false
+        }
+        for(let j=0;j<words[i].length;j++){
+            if(words[i].charAt(j) !== words[i+1].charAt(j)){
+                if(letterOrder[words[i].charCodeAt(j)-97] > letterOrder[words[i+1].charCodeAt(j)-97]){
+                    return false
+                }
+                break;
             }
-       for(let j=0;j<words[i].length;j++){
-           if(words[i].charAt(j) !== words[i+1].charAt(j)){
-               if(letterOrder[words[i].charCodeAt(j)-97] > letterOrder[words[i+1].charCodeAt(j)-97]){
-                   return false;
-               }else{
-                   break;
-               }
-           }
-       }
+        }
     }
-    return true
+    return true;
 };

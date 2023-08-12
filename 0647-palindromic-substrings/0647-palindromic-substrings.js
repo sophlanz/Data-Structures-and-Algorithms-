0d7c5f7fa5a3920a6ec5a86cdde5adc20d7adc67
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-08-12 10:56:57 -0400

    Time: 100 ms (31.36%), Space: 69 MB (16.36%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.js
+++ 0647-palindromic-substrings/0647-palindromic-substrings.js
@@ -4,10 +4,10 @@
  */
 var countSubstrings = function(s) {
     let res = 0;
-    let dp = new Array(s.length).fill(0).map(()=> new Array(s.length).fill(0))
+    let dp = new Array(s.length).fill(false).map(()=> new Array(s.length).fill(false))
     for (let i = s.length - 1; i >= 0; i--) {
         for (let j = i; j < s.length; j++) {
-            dp[i][j] = s.charAt(i) == s.charAt(j) && (j - i  + 1 < 3 || dp[i + 1][j - 1]);
+            dp[i][j] = s.charAt(i) == s.charAt(j) && (j - i  + 1 <= 3 || dp[i + 1][j - 1]);
             if(dp[i][j]) res++;
         }
     }

5b10e76178390e980e288677c76f259dcf6a0118
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-08-14 14:03:55 -0400

    Time: 48 ms (86.79%), Space: 42.1 MB (41.67%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.js
+++ 0213-house-robber-ii/0213-house-robber-ii.js
@@ -3,25 +3,28 @@
  * @return {number}
  */
 var rob = function(nums) {
-    if(nums.length <=3)return Math.max(...nums);
-    if(nums.length===4){
-        return Math.max(nums[0]+nums[2],nums[1]+nums[3])
-    }
+     if(nums.length === 1) return nums[0]
+    if(nums.length === 2) return Math.max(...nums)
+    if(nums.length === 0) return 0
+    
+    let first1 = nums[0]
+    let last1 = Math.max(nums[0], nums[1])
+    
+    let first2 = nums[1]
+    let last2 = Math.max(nums[1], nums[2])
 
-    const getRob = (house)=>{
-        console.log(house)
-           let dp=new Array(house.length).fill(0);
-           dp[0]=house[0];
-            dp[1]=house[1]
-            dp[2]=house[2]+house[0]
-            console.log(dp[2])
-            for(let i=3;i<house.length;i++) {
-            dp[i]=Math.max(house[i]+ dp[i-2],house[i]+dp[i-3]);
-    }
-        return Math.max(...dp)
+    
+    for(let i = 2; i < nums.length-1; i++){
+        let temp1 = Math.max(first1 + nums[i], last1)
+        first1 = last1
+        last1 = temp1
         
+        let temp2 = Math.max(first2 + nums[i+1], last2)
+        first2 = last2
+        last2 = temp2
     }
-  return Math.max((getRob(nums.slice(1))), getRob(nums.slice(0,nums.length-1)))
+    
+    return Math.max(last1, last2)
  
 };
 

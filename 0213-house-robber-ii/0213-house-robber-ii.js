beac799cb5f3225a3c5a7d8be9b4c7003a5587d1
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-08-15 10:56:04 -0400

    Time: 57 ms (49.74%), Space: 41.9 MB (58.61%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.js
+++ 0213-house-robber-ii/0213-house-robber-ii.js
@@ -3,28 +3,21 @@
  * @return {number}
  */
 var rob = function(nums) {
-     if(nums.length === 1) return nums[0]
-    if(nums.length === 2) return Math.max(...nums)
-    if(nums.length === 0) return 0
-    
-    let first1 = nums[0]
-    let last1 = Math.max(nums[0], nums[1])
-    
-    let first2 = nums[1]
-    let last2 = Math.max(nums[1], nums[2])
-
-    
-    for(let i = 2; i < nums.length-1; i++){
-        let temp1 = Math.max(first1 + nums[i], last1)
-        first1 = last1
-        last1 = temp1
-        
-        let temp2 = Math.max(first2 + nums[i+1], last2)
-        first2 = last2
-        last2 = temp2
+    if (nums.length == 1) return nums[0]
+    let prev1 = 0, cur1 = 0
+    let prev2 = 0, cur2 = 0
+    let temp
+    for (let i = 1; i < nums.length; i++) {
+        const n1 = nums[i-1]
+        const n2 = nums[i]
+        temp=cur1
+        cur1 = Math.max(n1 + prev1, cur1)
+        prev1=temp
+        temp=cur2
+        cur2 = Math.max(n2 + prev2, cur2)
+        prev2=temp
     }
-    
-    return Math.max(last1, last2)
+    return Math.max(cur1, cur2)
  
 };
 

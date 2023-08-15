f4c75ca59e6d0d26586c70685928b166db9b607e
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-08-15 10:05:28 -0400

    Time: 196 ms (71.09%), Space: 118.6 MB (19.41%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.js
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.js
@@ -3,31 +3,27 @@
  * @return {boolean}
  */
 var canPartition = function(nums) {
- const getSum = ()=>{
-        let sum=0;
-        for(const num of nums){
-            sum+= num;
-        }
-        return sum;
+let sum=0;
+    for(const num of nums){
+        sum += num;
     }
-let sum=getSum();
-if(sum % 2 !== 0)return false;
-let half=sum/2;
-let dp = new Array(nums.length).fill(false).map(()=> new Array(half+1).fill(false));
+    if(sum % 2 !== 0)return false;
+    let half=sum/2;
+    let dp=new Array(nums.length).fill(false).map(()=>new Array(half+1).fill(false));
     for(let row=0;row<nums.length;row++){
-        dp[row][0] = true;
-    }
+        dp[row][0]=true;
+    };
     for(let col=0;col<=half;col++){
         dp[0][col] = nums[0] === col
-    }
+    };
     for(let i=1;i<nums.length;i++){
         for(let j=1;j<=half;j++){
             if(dp[i-1][j]){
-                dp[i][j]=true;
+                dp[i][j]=true
             }else{
-               if(j>=nums[i]) {
-                 dp[i][j]=dp[i-1][j-nums[i]]
-               }
+                if(j > nums[i]) {
+                    dp[i][j]= dp[i-1][j-nums[i]]
+                }
             }
         }
     }

a9857db1d67969ca2c622574196908682c0ab7ff
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-08-15 10:00:01 -0400

    Time: 193 ms (71.39%), Space: 118.6 MB (19.41%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.js
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.js
@@ -3,35 +3,35 @@
  * @return {boolean}
  */
 var canPartition = function(nums) {
-    //parameters sum and index
-    //base can sum===0 or index>nums.length
-    let sum=0;
-    for(let i=0;i<nums.length;i++){
-        sum += nums[i];
+ const getSum = ()=>{
+        let sum=0;
+        for(const num of nums){
+            sum+= num;
+        }
+        return sum;
     }
-    if(sum % 2 !== 0)return false
-    let half=(sum/2)
-    let dp = new Array(nums.length).fill(false).map(()=> new Array(half+1).fill(false))
-    for(let i=0;i<nums.length;i++){
-        dp[i][0]=true;
+let sum=getSum();
+if(sum % 2 !== 0)return false;
+let half=sum/2;
+let dp = new Array(nums.length).fill(false).map(()=> new Array(half+1).fill(false));
+    for(let row=0;row<nums.length;row++){
+        dp[row][0] = true;
     }
-    for(let j=1;j<=half;j++){
-        dp[0][j]= nums[0] === j
+    for(let col=0;col<=half;col++){
+        dp[0][col] = nums[0] === col
     }
- 
     for(let i=1;i<nums.length;i++){
         for(let j=1;j<=half;j++){
-          if(dp[i-1][j]){
-              dp[i][j]= true;
-          }else{
-              if(j >= nums[i]){
-                  dp[i][j]=dp[i-1][j-nums[i]]
-              }
-          }
-              
+            if(dp[i-1][j]){
+                dp[i][j]=true;
+            }else{
+               if(j>=nums[i]) {
+                 dp[i][j]=dp[i-1][j-nums[i]]
+               }
             }
         }
-    return dp[nums.length-1][half];
+    }
+    return dp[nums.length-1][half]
 };      
 
 

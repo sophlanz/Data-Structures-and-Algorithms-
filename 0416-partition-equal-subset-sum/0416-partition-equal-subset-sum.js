fbfd5f51d6112bad18bf779cbeea7199d2e83814
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-08-15 10:21:38 -0400

    Time: 365 ms (39.85%), Space: 100.4 MB (38.25%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.js
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.js
@@ -9,25 +9,22 @@ let sum=0;
     }
     if(sum % 2 !== 0)return false;
     let half=sum/2;
-    let memo ={};
+    let memo =[];
     const DFS = (index,total)=>{
-        if(`${index}+${total}`in memo){
-            return memo[`${index}+${total}`];
+        memo[index] = memo[index] || [];
+        if(memo[index][total]!== undefined){
+            return memo[index][total];
         }
         if(total ===0)return true;
         if(index>nums.length)return false;
-  
              if(total-nums[index]>=0 ){
               if(DFS(index+1,total-nums[index])){
-                   memo[`${index}+${total}`]=true
+                   memo[index][total]=true
                   return true;
               }
          }
-            memo[`${index}+${total}`] = DFS(index+1,total)
-     
-       
-     return memo[`${index}+${total}`] 
-        
+            memo[index][total]= DFS(index+1,total)
+     return memo[index][total]   
     }
    return DFS(0,half)
 };      

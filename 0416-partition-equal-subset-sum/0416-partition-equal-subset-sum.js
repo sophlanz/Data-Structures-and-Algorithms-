d0db63d99702c5c04ab81e89aff9dba0b1198bbb
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-08-16 10:51:28 -0400

    Time: 288 ms (51.34%), Space: 100.5 MB (38.39%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.js
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.js
@@ -3,33 +3,29 @@
  * @return {boolean}
  */
 var canPartition = function(nums) {
-let sum=0;
+    let total=0;
     for(const num of nums){
-        sum += num;
+        total += num;
     }
-    if(sum % 2 !== 0)return false;
-    let half=sum/2;
-    let memo =[];
-    const DFS = (index,total)=>{
-        memo[index] = memo[index] || [];
-        if(memo[index][total]!== undefined){
-            return memo[index][total];
+    if(total % 2 !== 0)return false;
+    let half=total/2
+    let memo=[];
+    const DFS = (index,sum)=>{
+        memo[index]=memo[index]||[];
+        if(memo[index][sum] !== undefined)return memo[index][sum]
+        if(sum === 0)return true;
+        if(index>= nums.length)return false;
+        if(sum - nums[index]>=0){
+            if(DFS(index+1, sum-nums[index])){
+                memo[index][sum] = true;
+                return true;
+            }
         }
-        if(total ===0)return true;
-        if(index>nums.length)return false;
-             if(total-nums[index]>=0 ){
-              if(DFS(index+1,total-nums[index])){
-                   memo[index][total]=true
-                  return true;
-              }
-         }
-            memo[index][total]= DFS(index+1,total)
-     return memo[index][total]   
+      return  memo[index][sum] = DFS(index+1,sum)
+
+         
     }
-   return DFS(0,half)
-};      
-
-
-
-
+       return DFS(0,half)
+};
+    
 

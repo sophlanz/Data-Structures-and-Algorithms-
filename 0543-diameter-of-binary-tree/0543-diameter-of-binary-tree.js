cfe2b298c58c8dc689e4c84c2a5b5c69dc4b77d2
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-08-16 12:36:09 -0400

    Time: 68 ms (57.67%), Space: 45.2 MB (85.85%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.js
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.js
@@ -11,16 +11,15 @@
  * @return {number}
  */
 var diameterOfBinaryTree = function(root) {
-     let max = 0
-     
-     function maxDepth(root) {
-         if (root === null) return 0 
-         let left = maxDepth(root.left) 
-         let right = maxDepth(root.right) 
-         max = Math.max(max, left + right) 
-         return Math.max(left, right) + 1 
-     }
-  
-     maxDepth(root)
-     return max
+    //calculating the max path for each node
+    let max=0
+  const DFS = (node)=>{
+      if(!node)return 0;
+      let left=DFS(node.left);              //# of edges to left
+      let right=DFS(node.right);            //# of edges to right
+       max = Math.max(max, left + right)    // path size
+     return Math.max(left, right) + 1       //# of edges between current node and bottom of tree
+  }
+DFS(root);
+return max
 };

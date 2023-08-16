31d9c98d882f50c728aeb395e2afe6eca3c4e640
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-08-16 09:26:34 -0400

    Time: 73 ms (93.15%), Space: 49.7 MB (85.19%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.js
+++ 0143-reorder-list/0143-reorder-list.js
@@ -9,35 +9,39 @@
  * @param {ListNode} head
  * @return {void} Do not return anything, modify head in-place instead.
  */
+
 var reorderList = function(head) {
-    let fast=head;
-    let slow=head;
-    while(fast && fast.next){
-        fast=fast.next.next;
-        slow=slow.next;
-    }
-    let mid=slow;
-    let reverseList = (node)=>{
-        let prev=null;
-        let curr=node;
-        while(curr){
-            let temp = curr.next;
-            curr.next=prev;
-            prev=curr;
-            curr=temp;
-        }
-        return prev;
-    }
-    let secondHalf=reverseList(mid)
-   let firstHalf=head;
-    while(secondHalf.next){
-       let temp1=firstHalf.next;
-        let temp2=secondHalf.next;
-        firstHalf.next=secondHalf;
-        secondHalf.next=temp1;
-        secondHalf=temp2;
-        firstHalf=temp1
-        
-    }
-    return head
+    if (!head) return;
+    let curr = head, middle = findMiddle(head);
+    let rev = reverseList(middle.next);
+   while(rev){
+       let temp = curr.next;
+       curr.next = rev;
+       rev=rev.next;
+       curr.next.next=temp
+       curr=temp
+   }
+    middle.next = null;
 };
+
+var findMiddle = function(head) {
+    if (!head) return;
+    let middle = head;
+    while (head && head.next) {
+        head = head.next.next;
+        middle = middle.next;
+    }
+    return middle;
+}
+
+var reverseList = function(head) {
+    if (!head) return;
+    let prev = null;
+    while (head) {
+        const saved = head.next;
+        head.next = prev;
+        prev = head;
+        head = saved;
+    }
+    return prev;
+}

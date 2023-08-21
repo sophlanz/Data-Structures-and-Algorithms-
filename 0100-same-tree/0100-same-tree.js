5ab63b40ed9a6c8e2e94e694b10442618f68c038
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-08-21 14:22:22 -0400

    Time: 50 ms (80.64%), Space: 41.9 MB (77.36%) - LeetHub

--- 0100-same-tree/0100-same-tree.js
+++ 0100-same-tree/0100-same-tree.js
@@ -13,8 +13,7 @@
  */
 var isSameTree = function(p, q) {
     if(!p && !q)return true;
-    if(!p || ! q)return false;
+    if(!p || !q)return false;
     if(p.val !== q.val)return false;
-    return isSameTree(p.left,q.left) && isSameTree(p.right,q.right)
-    
+    return isSameTree(p.left, q.left) && isSameTree(p.right,q.right);
 };

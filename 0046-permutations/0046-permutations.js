86aa21598d38edb514c9d3df78127bb540616108
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-10-03 11:38:49 -0400

    Time: 63 ms (55.88%), Space: 44.5 MB (76.94%) - LeetHub

--- 0046-permutations/0046-permutations.js
+++ 0046-permutations/0046-permutations.js
@@ -4,14 +4,18 @@
  */
 var permute = function(nums) {
     let res=[];
-    const DFS = (curr, rest) => {
-        if(curr.length === nums.length) {
-            res.push(curr)
-        }
-        for(let i=0;i<rest.length;i++) {
-            DFS([...curr,rest[i]], [...rest.slice(0,i) , ...rest.slice(i+1)])
-        }
-    }
-    DFS([],nums)
-    return res
+  const DFS = (arr,rest)=>{
+      if(arr.length===nums.length){
+          res.push(arr)
+          return;
+      }
+      for(let i=0;i<rest.length;i++){
+          const add = rest[i];
+          const newArray = [...arr,add]
+          const newRest = [...rest.slice(0,i), ...rest.slice(i+1)]
+          DFS(newArray,newRest);
+      }
+  }
+DFS([],nums)
+return res
 };

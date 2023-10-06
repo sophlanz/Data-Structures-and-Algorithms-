f9fac40e6af69098410baf35bbbb3eaae756e4cf
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-10-06 14:57:15 -0400

    Time: 59 ms (91.91%), Space: 41.4 MB (99.40%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
@@ -4,20 +4,17 @@
  * @return {number}
  */
 var characterReplacement = function(s, k) {
-let start=0,
-    map={},
-    maxLen=0,
-    mostFreqChar=0;
-for(let end=0;end<s.length;end++) {
-    let rChar=s[end];
-    map[rChar] = map[rChar]+1 || 1;
-    mostFreqChar=Math.max(mostFreqChar,map[rChar])
-    if((end-start+1-mostFreqChar)>k) {
-        let lChar=s[start];
-        map[lChar]--
-        start++
+let count = new Array(26).fill(0);
+    let start=0,end=0;
+    let maxLen=0;
+    let maxCount=0;
+    for(let end=0;end<s.length;end++){
+        maxCount = Math.max(maxCount, ++count[s.charCodeAt(end)-65]);
+        if(end-start+1-maxCount>k){
+            count[s.charCodeAt(start)-65]--;
+            start++
+        }
+        maxLen = Math.max(maxLen,end-start+1)
     }
-    maxLen=Math.max(maxLen,end-start+1)
-}
-    return maxLen;
+    return maxLen
 };

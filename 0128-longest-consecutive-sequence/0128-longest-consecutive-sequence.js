6964d1384afee9bb362f135259c6a1862cd0cea4
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-10-07 11:59:33 -0400

    Time: 94 ms (82.05%), Space: 64.3 MB (31.84%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
@@ -3,22 +3,19 @@
  * @return {number}
  */
 var longestConsecutive = function(nums) {
-    let len=0;
+    let set = new Set(nums);
     let maxLen=0;
-   nums = new Set(nums)
-    for(const num of nums){
-          len=1
-        if(!(nums.has(num-1))){
-            let checkNum=num+1
-            while(nums.has(checkNum)){
-          
+    for(const num of set){
+        let len=1;
+        if(set.has(num-1) && !(set.has(num+1))){
+            let numToFind = num-1
+            while(set.has(numToFind)){
+                numToFind--
                 len++
-                checkNum++
             }
-             maxLen=Math.max(maxLen,checkNum-num)
         }
-       maxLen=Math.max(maxLen,len)
-      
+        maxLen=Math.max(maxLen,len);
     }
     return maxLen
 };
+

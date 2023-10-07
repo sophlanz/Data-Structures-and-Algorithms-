e43dd04f41219e6698782db78a78a3fdfd1c1a9f
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-10-07 11:51:34 -0400

    Time: 72 ms (97.59%), Space: 54.9 MB (40.64%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -3,18 +3,16 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-    let res=new Array(nums.length).fill(0);
-    let product =1;
+    let productArray = new Array(nums.length).fill(0);
+    let product=1;
     for(let i=0;i<nums.length;i++){
-        res[i]=product;
+        productArray[i]=product;
+        product *=nums[i];
+    }
+    product=1;
+    for(let i=nums.length-1;i>=0;i--){
+        productArray[i] = productArray[i]*product;
         product *= nums[i];
     }
-    product =1;
-    for(let i=nums.length-1;i>=0;i--){
-        res[i]*=product;
-        product *= nums[i]
-    }
-    return res;
+    return productArray
 };
-
- 

005c1d012bbd58e40c10bcb7e567762ec89ab99f
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-10-10 17:46:59 -0400

    Time: 48 ms (75.50%), Space: 41.6 MB (88.51%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
@@ -4,14 +4,14 @@
  */
 var findMin = function(nums) {
     let l=0,r=nums.length-1;
-    let best=nums[0];
+    let best = nums[0];
     while(l<r){
         let mid = Math.floor((l+r)/2);
-        if(nums[r]<nums[mid]){
+        if(nums[r] < nums[mid]){
             best = Math.min(best,nums[r]);
             l=mid+1;
         }else{
-            best=Math.min(best,nums[l]);
+            best = Math.min(best,nums[l]);
             r=mid;
         }
     }

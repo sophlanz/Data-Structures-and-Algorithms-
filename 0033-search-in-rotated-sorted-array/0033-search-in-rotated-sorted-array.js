68c03e60d6f76992b68bdf9cf92621eda10beb6d
Sophia Lanzendorf <75180391+sophlanz@users.noreply.github.com>
2023-10-16 17:15:02 -0400

    Time: 68 ms (5.77%), Space: 44.4 MB (5.41%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -4,18 +4,19 @@
  * @return {number}
  */
 var search = function(nums, target) {
-let l=0,r=nums.length-1;
+    let l=0,r=nums.length-1;
     while(l<=r){
         let mid = Math.floor((l+r)/2);
-        if(nums[mid]===target)return mid
-        if(nums[l]<=nums[mid]){
-            if(target<nums[l] || target>nums[mid]){
-                l=mid+1
+        console.log(mid)
+        if(nums[mid]===target)return mid;
+        if(nums[mid]>=nums[l]){
+            if(target > nums[mid] || target<nums[l]){
+                l=mid+1;
             }else{
                 r=mid-1
             }
         }else{
-            if(target>nums[r] || target<nums[mid]){
+            if(target<nums[mid] || target>nums[r]){
                 r=mid-1
             }else{
                 l=mid+1
